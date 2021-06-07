174ee572dc5211f340b15ba94d29168bc9d8155c
Chingiz <chyngyz.i@p2p.org>
2021-06-07 19:34:11 +0600

    - Klint fix

--- app/src/main/java/com/p2p/wallet/main/ui/main/MainFragment.kt
+++ app/src/main/java/com/p2p/wallet/main/ui/main/MainFragment.kt
@@ -25,7 +25,6 @@ import com.p2p.wallet.utils.attachAdapter
 import com.p2p.wallet.utils.replaceFragment
 import com.p2p.wallet.utils.viewbinding.viewBinding
 import org.koin.android.ext.android.inject
-import timber.log.Timber
 import java.math.BigDecimal
 
 class MainFragment :
