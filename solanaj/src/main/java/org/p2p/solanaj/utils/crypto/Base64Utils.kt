13381b6e2db19494a6faf85d24673b30bd21df09
Chingiz <chyngyz.i@p2p.org>
2021-09-20 22:31:50 +0600

    - gitignore, base64 fix

--- solanaj/src/main/java/org/p2p/solanaj/utils/crypto/Base64Utils.kt
+++ solanaj/src/main/java/org/p2p/solanaj/utils/crypto/Base64Utils.kt
@@ -5,7 +5,7 @@ import android.util.Base64
 object Base64Utils {
 
     fun encode(bytes: ByteArray): String =
-        Base64.encodeToString(bytes, Base64.DEFAULT)
+        Base64.encodeToString(bytes, Base64.NO_WRAP)
 
     fun decode(stringToDecode: String): ByteArray =
         Base64.decode(stringToDecode, Base64.DEFAULT)
