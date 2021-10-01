dd284434d49ed940caee1ce11a1a702c539e04f2
Eugene Rogov <evgeniy.r@p2p.org>
2021-10-01 15:52:32 +0300

    started adding UsernameRepository

--- app/src/main/java/com/p2p/wallet/auth/AuthModule.kt
+++ app/src/main/java/com/p2p/wallet/auth/AuthModule.kt
@@ -1,11 +1,17 @@
 package com.p2p.wallet.auth
 
+import android.content.Context
 import androidx.biometric.BiometricManager
+import com.google.gson.Gson
+import com.p2p.wallet.BuildConfig
+import com.p2p.wallet.R
 import com.p2p.wallet.auth.api.UsernameApi
 import com.p2p.wallet.auth.interactor.AuthInteractor
 import com.p2p.wallet.auth.interactor.ReservingUsernameInteractor
 import com.p2p.wallet.auth.repository.AuthRemoteRepository
 import com.p2p.wallet.auth.repository.AuthRepository
+import com.p2p.wallet.auth.repository.UsernameRemoteRepository
+import com.p2p.wallet.auth.repository.UsernameRepository
 import com.p2p.wallet.auth.ui.biometric.BiometricContract
 import com.p2p.wallet.auth.ui.biometric.BiometricPresenter
 import com.p2p.wallet.auth.ui.pin.create.CreatePinContract
@@ -16,10 +22,13 @@ import com.p2p.wallet.auth.ui.security.SecurityKeyContract
 import com.p2p.wallet.auth.ui.security.SecurityKeyPresenter
 import com.p2p.wallet.auth.ui.username.ReservingUsernameContract
 import com.p2p.wallet.auth.ui.username.ReservingUsernamePresenter
+import com.p2p.wallet.user.UserModule.createLoggingInterceptor
+import okhttp3.OkHttpClient
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 object AuthModule {
 
@@ -37,6 +46,20 @@ object AuthModule {
         factory { CreatePinPresenter(get()) } bind CreatePinContract.Presenter::class
         factory { SignInPinPresenter(get()) } bind SignInPinContract.Presenter::class
         factory { ReservingUsernamePresenter(get()) } bind ReservingUsernameContract.Presenter::class
+
+        single {
+            val client = OkHttpClient.Builder()
+                .apply { if (BuildConfig.DEBUG) addInterceptor(createLoggingInterceptor("Blockstream")) }
+                .build()
+            val api = Retrofit.Builder()
+                .baseUrl(get<Context>().getString(R.string.solanaUsername))
+                .addConverterFactory(GsonConverterFactory.create(get<Gson>()))
+                .client(client)
+                .build()
+                .create(UsernameApi::class.java)
+
+            UsernameRemoteRepository(api)
+        } bind UsernameRepository::class
 
         factory { get<Retrofit>(named(RESERVING_USERNAME_QUALIFIER)).create(UsernameApi::class.java) }
     }
