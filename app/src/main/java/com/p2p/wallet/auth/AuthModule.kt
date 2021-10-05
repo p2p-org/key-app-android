bdfb5926b83fce9bee81838fcd018dd954ec8936
Chingiz <chyngyz.i@p2p.org>
2021-10-05 15:41:59 +0600

    - StringQualifier for feeRelayer retrofit object

--- app/src/main/java/com/p2p/wallet/auth/AuthModule.kt
+++ app/src/main/java/com/p2p/wallet/auth/AuthModule.kt
@@ -1,10 +1,6 @@
 package com.p2p.wallet.auth
 
-import android.content.Context
 import androidx.biometric.BiometricManager
-import com.google.gson.Gson
-import com.p2p.wallet.BuildConfig
-import com.p2p.wallet.R
 import com.p2p.wallet.auth.api.UsernameApi
 import com.p2p.wallet.auth.interactor.AuthInteractor
 import com.p2p.wallet.auth.interactor.ReservingUsernameInteractor
@@ -22,17 +18,14 @@ import com.p2p.wallet.auth.ui.security.SecurityKeyContract
 import com.p2p.wallet.auth.ui.security.SecurityKeyPresenter
 import com.p2p.wallet.auth.ui.username.ReservingUsernameContract
 import com.p2p.wallet.auth.ui.username.ReservingUsernamePresenter
-import com.p2p.wallet.user.UserModule.createLoggingInterceptor
-import okhttp3.OkHttpClient
 import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 object AuthModule {
 
-    private const val RESERVING_USERNAME_QUALIFIER = "https://fee-relayer.solana.p2p.org"
+    const val RESERVING_USERNAME_QUALIFIER = "https://fee-relayer.solana.p2p.org"
 
     fun create() = module {
         single { BiometricManager.from(get()) }
@@ -47,17 +40,9 @@ object AuthModule {
         // reserving username
         factory { ReservingUsernameInteractor(get()) }
         factory { ReservingUsernamePresenter(get()) } bind ReservingUsernameContract.Presenter::class
-        factory { get<Retrofit>(named(RESERVING_USERNAME_QUALIFIER)).create(UsernameApi::class.java) }
         single {
-            val client = OkHttpClient.Builder()
-                .apply { if (BuildConfig.DEBUG) addInterceptor(createLoggingInterceptor("solanaUsername")) }
-                .build()
-            val api = Retrofit.Builder()
-                .baseUrl(get<Context>().getString(R.string.feeRelayerBaseUrl))
-                .addConverterFactory(GsonConverterFactory.create(get<Gson>()))
-                .client(client)
-                .build()
-                .create(UsernameApi::class.java)
+            val retrofit = get<Retrofit>(named(RESERVING_USERNAME_QUALIFIER))
+            val api = retrofit.create(UsernameApi::class.java)
             UsernameRemoteRepository(api)
         } bind UsernameRepository::class
     }
