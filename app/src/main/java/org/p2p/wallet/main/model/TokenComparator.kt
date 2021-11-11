ca2c74d7375802efd718c6fc63ce97cf24960691
Chingiz <chingiz.isakov>
2021-11-11 18:42:41 +0600

    [P2PW-1062] - Minor corrections

--- app/src/main/java/org/p2p/wallet/main/model/TokenComparator.kt
+++ app/src/main/java/org/p2p/wallet/main/model/TokenComparator.kt
@@ -10,28 +10,28 @@ import org.p2p.wallet.utils.isMoreThan
  * */
 class TokenComparator : Comparator<Token> {
 
-    override fun compare(o1: Token, o2: Token): Int {
-        if (o1.isSOL) return -1
-        if (o2.isSOL) return 1
+    override fun compare(o1: Token, o2: Token): Int = when {
+        o1.isSOL -> -1
+        o2.isSOL -> 1
+        o1 is Token.Active && o2 is Token.Active -> compareActiveTokens(o1, o2)
+        o1 is Token.Active -> -1
+        o2 is Token.Active -> 1
+        else -> 0
+    }
 
-        if (o1 is Token.Active && o2 is Token.Active) {
-            return when {
-                o1.totalInUsd != null && o2.totalInUsd != null ->
-                    if (o1.totalInUsd.isMoreThan(o2.totalInUsd)) -1 else 1
-                o1.totalInUsd == null && o2.totalInUsd == null ->
-                    if (o1.total.isMoreThan(o2.total)) -1 else 1
-                o1.totalInUsd != null && o2.totalInUsd == null ->
-                    -1
-                o1.totalInUsd == null && o2.totalInUsd != null ->
-                    1
-                else ->
-                    if (o1.total.isMoreThan(o2.total)) -1 else 1
-            }
-        }
-
-        if (o1 is Token.Active) return -1
-        if (o2 is Token.Active) return 1
-
-        return 0
+    private fun compareActiveTokens(
+        o1: Token.Active,
+        o2: Token.Active
+    ): Int = when {
+        o1.totalInUsd != null && o2.totalInUsd != null ->
+            if (o1.totalInUsd.isMoreThan(o2.totalInUsd)) -1 else 1
+        o1.totalInUsd == null && o2.totalInUsd == null ->
+            if (o1.total.isMoreThan(o2.total)) -1 else 1
+        o1.totalInUsd != null && o2.totalInUsd == null ->
+            -1
+        o1.totalInUsd == null && o2.totalInUsd != null ->
+            1
+        else ->
+            if (o1.total.isMoreThan(o2.total)) -1 else 1
     }
 }
