88d66fb57e2187862de8ea7b8b757dc9c38f7a19
Chingiz <chingiz.isakov>
2021-11-18 21:35:27 +0600

    [P2PW-987] - Added icon to git

--- app/src/main/java/org/p2p/wallet/auth/api/CheckUsernameResponse.kt
+++ app/src/main/java/org/p2p/wallet/auth/api/CheckUsernameResponse.kt
@@ -3,7 +3,10 @@ package org.p2p.wallet.auth.api
 import com.google.gson.annotations.SerializedName
 
 data class CheckUsernameResponse(
-    @SerializedName("parent_name") val parentName: String,
-    @SerializedName("owner") val owner: String,
-    @SerializedName("class") val clazz: String,
+    @SerializedName("parent_name")
+    val parentName: String,
+    @SerializedName("owner")
+    val owner: String,
+    @SerializedName("class")
+    val clazz: String,
 )
--- app/src/main/java/org/p2p/wallet/auth/api/RegisterUsernameResponse.kt
+++ app/src/main/java/org/p2p/wallet/auth/api/RegisterUsernameResponse.kt
@@ -3,5 +3,6 @@ package org.p2p.wallet.auth.api
 import com.google.gson.annotations.SerializedName
 
 data class RegisterUsernameResponse(
-    @SerializedName("signature") val signature: String,
+    @SerializedName("signature")
+    val signature: String
 )
