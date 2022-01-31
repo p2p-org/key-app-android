497e80030585d31aef0c7989718ecf3e9ceda17c
Davran <khasanovdavran10@gmail.com>
2022-01-31 19:35:58 +0600

    start token list

--- app/src/main/java/org/p2p/wallet/main/ui/receive/list/TokenListFragment.kt
+++ app/src/main/java/org/p2p/wallet/main/ui/receive/list/TokenListFragment.kt
@@ -1,10 +1,27 @@
 package org.p2p.wallet.main.ui.receive.list
 
+import android.os.Bundle
+import android.view.View
 import org.koin.android.ext.android.inject
 import org.p2p.wallet.R
 import org.p2p.wallet.common.mvp.BaseMvpFragment
+import org.p2p.wallet.databinding.FragmentReceiveListBinding
+import org.p2p.wallet.utils.viewbinding.viewBinding
 
 class TokenListFragment :
     BaseMvpFragment<TokenListContract.View, TokenListContract.Presenter>(R.layout.fragment_receive_list) {
+
+    companion object {
+        fun create() = TokenListFragment()
+    }
+
     override val presenter: TokenListContract.Presenter by inject()
+    private val binding: FragmentReceiveListBinding by viewBinding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+
+        }
+    }
 }
