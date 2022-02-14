101846efaf674e6b9980a361495521c70c14037f
Chingiz <chyngyz.i@p2p.org>
2022-02-15 03:18:34 +0600

    [NO-TICKET] - Added constant&todo

--- app/src/main/java/org/p2p/wallet/infrastructure/network/environment/EnvironmentManager.kt
+++ app/src/main/java/org/p2p/wallet/infrastructure/network/environment/EnvironmentManager.kt
@@ -16,6 +16,10 @@ class EnvironmentManager(
     private val sharedPreferences: SharedPreferences
 ) {
 
+    companion object {
+        private const val FAKE_ETH_ADDRESS = "0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae"
+    }
+
     private var onChanged: ((Environment) -> Unit)? = null
 
     fun isDevnet(): Boolean = loadEnvironment() == Environment.DEVNET
@@ -39,7 +43,8 @@ class EnvironmentManager(
             .appendQueryParameter("baseCurrencyAmount", amount)
             .appendQueryParameter("baseCurrencyCode", USD_READABLE_SYMBOL.lowercase())
             .appendQueryParameter("lockAmount", "false")
-            .appendQueryParameter("walletAddress", "0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae")
+            // FIXME: fake address will be replaced by real token address, this is for testing
+            .appendQueryParameter("walletAddress", FAKE_ETH_ADDRESS)
             .build()
             .toString()
     }
