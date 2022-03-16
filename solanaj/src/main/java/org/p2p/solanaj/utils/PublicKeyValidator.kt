39e2be596c43edb2564f104fcfce87668704f985
Roman <roman.kuznetsov@p2p.org>
2022-03-16 18:11:01 +0300

    [P2PW-1478] - Use Base58Utils in PublicKeyValidator.kt

--- solanaj/src/main/java/org/p2p/solanaj/utils/PublicKeyValidator.kt
+++ solanaj/src/main/java/org/p2p/solanaj/utils/PublicKeyValidator.kt
@@ -1,21 +1,21 @@
 package org.p2p.solanaj.utils
 
-import org.bitcoinj.core.Base58
 import org.p2p.solanaj.core.PublicKey
+import org.p2p.solanaj.utils.crypto.Base58Utils
 
 object PublicKeyValidator {
     private const val REGEX = "[1-9A-HJ-NP-Za-km-z]{32,44}"
 
     @Throws(IllegalArgumentException::class)
     fun validate(publicKey: String) = require(
-        Regex(REGEX).matches(publicKey) && Base58.decode(publicKey).size == PublicKey.PUBLIC_KEY_LENGTH
+        Regex(REGEX).matches(publicKey) && Base58Utils.decode(publicKey).size == PublicKey.PUBLIC_KEY_LENGTH
     ) {
         { "Invalid public key input[String]" }
     }
 
     @Throws(IllegalArgumentException::class)
     fun validate(publicKey: ByteArray) = require(
-        Regex(REGEX).matches(Base58.encode(publicKey)) && publicKey.size == PublicKey.PUBLIC_KEY_LENGTH
+        Regex(REGEX).matches(Base58Utils.encode(publicKey)) && publicKey.size == PublicKey.PUBLIC_KEY_LENGTH
     ) {
         { "Invalid public key input[ByteArray]" }
     }
