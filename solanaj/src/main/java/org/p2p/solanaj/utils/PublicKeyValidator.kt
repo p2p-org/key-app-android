8a8326cd70b55a2b78fc4368da200e2bf6372ad7
Roman <roman.kuznetsov@p2p.org>
2022-03-17 15:53:52 +0300

    [P2PW-1478] - Fix function formatting in PublicKeyValidator.kt

--- solanaj/src/main/java/org/p2p/solanaj/utils/PublicKeyValidator.kt
+++ solanaj/src/main/java/org/p2p/solanaj/utils/PublicKeyValidator.kt
@@ -7,16 +7,16 @@ object PublicKeyValidator {
     private const val REGEX = "[1-9A-HJ-NP-Za-km-z]{32,44}"
 
     @Throws(IllegalArgumentException::class)
-    fun validate(publicKey: String) = require(
-        Regex(REGEX).matches(publicKey) && Base58Utils.decode(publicKey).size == PublicKey.PUBLIC_KEY_LENGTH
-    ) {
-        { "Invalid public key input[String]" }
+    fun validate(publicKey: String) {
+        require(Regex(REGEX).matches(publicKey) && Base58Utils.decode(publicKey).size == PublicKey.PUBLIC_KEY_LENGTH) {
+            { "Invalid public key input[String]" }
+        }
     }
 
     @Throws(IllegalArgumentException::class)
-    fun validate(publicKey: ByteArray) = require(
-        Regex(REGEX).matches(Base58Utils.encode(publicKey)) && publicKey.size == PublicKey.PUBLIC_KEY_LENGTH
-    ) {
-        { "Invalid public key input[ByteArray]" }
+    fun validate(publicKey: ByteArray) {
+        require(Regex(REGEX).matches(Base58Utils.encode(publicKey)) && publicKey.size == PublicKey.PUBLIC_KEY_LENGTH) {
+            { "Invalid public key input[ByteArray]" }
+        }
     }
 }
