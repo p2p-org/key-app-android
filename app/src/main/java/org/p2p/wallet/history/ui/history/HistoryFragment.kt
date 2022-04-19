6834b90100e83f25964a8afd7c397aa325e839eb
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-04-19 11:04:27 +0300

    PWN-3251 - reformat code

--- app/src/main/java/org/p2p/wallet/history/ui/history/HistoryFragment.kt
+++ app/src/main/java/org/p2p/wallet/history/ui/history/HistoryFragment.kt
@@ -12,9 +12,7 @@ import org.p2p.wallet.common.ui.recycler.PagingState
 import org.p2p.wallet.databinding.FragmentHistoryBinding
 import org.p2p.wallet.history.model.HistoryTransaction
 import org.p2p.wallet.history.model.TransactionDetailsLaunchState
-import org.p2p.wallet.history.ui.details.TransactionDetailsFragment
 import org.p2p.wallet.history.ui.token.adapter.HistoryAdapter
-import org.p2p.wallet.utils.replaceFragment
 import org.p2p.wallet.utils.unsafeLazy
 import org.p2p.wallet.utils.viewbinding.viewBinding
 import timber.log.Timber
