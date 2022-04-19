7cb18b90a665e3fc0036f36e735b2d86d2d7272c
Roman Kuznetsov <100695872+kuznetsov@users.noreply.github.com>
2022-04-19 15:24:42 +0300

    PWN-3114 - Device token send retry (#424)
    * PWN-3114 - Retry device token send in case of ServerException
    
    * PWN-3114 - Code style changes
    
    * PWN-3114 - Improve recursion in updateDeviceToken()
    
    * PWN-3114 - Naming changes

--- app/src/main/java/org/p2p/wallet/push_notifications/ineractor/PushNotificationsInteractor.kt
+++ app/src/main/java/org/p2p/wallet/push_notifications/ineractor/PushNotificationsInteractor.kt
@@ -3,22 +3,29 @@ package org.p2p.wallet.push_notifications.ineractor
 import android.content.SharedPreferences
 import android.os.Build
 import androidx.core.content.edit
+import kotlinx.coroutines.delay
+import org.p2p.wallet.infrastructure.network.data.ServerException
 import org.p2p.wallet.infrastructure.network.provider.TokenKeyProvider
 import org.p2p.wallet.push_notifications.model.DeviceInfo
 import org.p2p.wallet.push_notifications.model.DeviceToken
 import org.p2p.wallet.push_notifications.repository.DeviceTokenRepository
 import org.p2p.wallet.push_notifications.repository.PushTokenRepository
+import timber.log.Timber
 
 private const val KEY_DEVICE_TOKEN = "KEY_DEVICE_TOKEN"
+private const val TOKEN_SEND_RETRY_DELAY_MS = 60000L
+private const val DEFAULT_RETRIES_NUMBER = 1
 
 class PushNotificationsInteractor(
     private val deviceTokenRepository: DeviceTokenRepository,
     private val pushTokenRepository: PushTokenRepository,
     private val tokenKeyProvider: TokenKeyProvider,
-    private val sharedPreferences: SharedPreferences,
+    private val sharedPreferences: SharedPreferences
 ) {
 
-    suspend fun updateDeviceToken(): DeviceToken {
+    suspend fun updateDeviceToken(retries: Int = DEFAULT_RETRIES_NUMBER) {
+        if (retries < 0) return
+
         val token = pushTokenRepository.getPushToken().value
 
         sharedPreferences.edit { putString(KEY_DEVICE_TOKEN, token) }
@@ -35,7 +42,13 @@ class PushNotificationsInteractor(
             clientId = tokenKeyProvider.publicKey
         )
 
-        return deviceTokenRepository.sendDeviceToken(deviceToken)
+        try {
+            deviceTokenRepository.sendDeviceToken(deviceToken)
+        } catch (e: ServerException) {
+            Timber.e(e, "Error sending device token $token to server")
+            delay(TOKEN_SEND_RETRY_DELAY_MS)
+            updateDeviceToken(retries - 1)
+        }
     }
 
     suspend fun deleteDeviceToken() {
