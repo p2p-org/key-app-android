2be9a25abfa937cb1d2201a98840d7023569aec5
“khasanov.davran” <khasanov.davran@p2p.org>
2022-04-21 16:38:19 +0600

    PWN-3219 - Fix link to transaction details in progress bottom sheet

--- app/src/main/java/org/p2p/wallet/transaction/ui/ProgressBottomSheet.kt
+++ app/src/main/java/org/p2p/wallet/transaction/ui/ProgressBottomSheet.kt
@@ -74,11 +74,6 @@ class ProgressBottomSheet : NonDraggableBottomSheetDialogFragment() {
             subTitleTextView.text = data.subTitle
             transactionIdGroup.isVisible = data.transactionId.isNotEmpty()
 
-            transactionLabelTextView.setOnClickListener {
-                val url = getString(R.string.solanaExplorer, data.transactionId)
-                showUrlInCustomTabs(url)
-            }
-
             transactionIdTextView.text = data.transactionId
 
             arrowImageView.setOnClickListener {
@@ -128,11 +123,16 @@ class ProgressBottomSheet : NonDraggableBottomSheetDialogFragment() {
 
     private fun setSuccessState(message: String, signature: String) {
         with(binding) {
+
             titleTextView.text = message
             progressBar.isVisible = false
 
             transactionIdGroup.isVisible = true
             transactionIdTextView.text = signature
+            transactionLabelTextView.setOnClickListener {
+                val url = getString(R.string.solanaExplorer, signature)
+                showUrlInCustomTabs(url)
+            }
 
             transactionImageView.setImageResource(R.drawable.ic_success)
             lineView.isVisible = true
