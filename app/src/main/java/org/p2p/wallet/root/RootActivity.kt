9839e668a64ac2fb0eafb33de441cbd1ab01a17c
Chingiz <chingiz.isakov>
2022-05-05 11:45:45 +0600

    NO-TICKET - Debug view visible persistence

--- app/src/main/java/org/p2p/wallet/root/RootActivity.kt
+++ app/src/main/java/org/p2p/wallet/root/RootActivity.kt
@@ -5,9 +5,11 @@ import androidx.core.view.isVisible
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.TextView
+import androidx.core.content.edit
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import org.koin.android.ext.android.inject
@@ -28,6 +30,8 @@ import org.p2p.wallet.utils.replaceFragment
 import org.p2p.wallet.utils.toast
 import timber.log.Timber
 
+private const val KEY_DEBUG_HIDDEN = "KEY_DEBUG_HIDDEN"
+
 class RootActivity : BaseMvpActivity<RootContract.View, RootContract.Presenter>(), RootContract.View {
 
     companion object {
@@ -40,6 +44,7 @@ class RootActivity : BaseMvpActivity<RootContract.View, RootContract.Presenter>(
     override val presenter: RootContract.Presenter by inject()
     private val adminAnalytics: AdminAnalytics by inject()
     private val analyticsInteractor: ScreensAnalyticsInteractor by inject()
+    private val sharedPreferences: SharedPreferences by inject()
     private val binding: ActivityRootBinding by lazy {
         ActivityRootBinding.inflate(LayoutInflater.from(this))
     }
@@ -105,19 +110,24 @@ class RootActivity : BaseMvpActivity<RootContract.View, RootContract.Presenter>(
     @SuppressLint("SetTextI18n")
     private fun initializeDebugDrawer() {
         val devView = findViewById<TextView>(R.id.developTextView)
+        val isDebugHidden = sharedPreferences.getBoolean(KEY_DEBUG_HIDDEN, false)
 
         if (BuildConfig.DEBUG) {
             val drawer = DebugDrawer.install(this)
 
             devView.text = "${BuildConfig.BUILD_TYPE}-${BuildConfig.VERSION_NAME}"
             devView.isVisible = true
+
+            devView.alpha = if (isDebugHidden) 0f else 1f
             devView.setOnClickListener { drawer.openDrawer() }
             devView.setOnLongClickListener {
                 val currentAlpha = devView.alpha
                 if (currentAlpha == 0f) {
                     devView.alpha = 1f
+                    sharedPreferences.edit { putBoolean(KEY_DEBUG_HIDDEN, false) }
                 } else {
                     devView.alpha = 0f
+                    sharedPreferences.edit { putBoolean(KEY_DEBUG_HIDDEN, true) }
                 }
                 return@setOnLongClickListener true
             }
