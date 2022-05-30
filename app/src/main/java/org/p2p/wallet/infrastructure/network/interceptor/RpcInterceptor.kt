592073a83e61478ad0c4639c50a336f5c52173a9
khasanov.davran <khasanov.davran@p2p.org>
2022-05-30 15:28:38 +0600

    PWN-2819 - Fix ren btc sessions errors

--- app/src/main/java/org/p2p/wallet/infrastructure/network/interceptor/RpcInterceptor.kt
+++ app/src/main/java/org/p2p/wallet/infrastructure/network/interceptor/RpcInterceptor.kt
@@ -52,7 +52,7 @@ open class RpcInterceptor(
         val json = getRequestJson(request)
 
         val url = request.url
-
+        Timber.tag("____________").d(json.toString())
         val key = RpcConstants.REQUEST_METHOD_KEY
         val historyEndpoint = RpcConstants.REQUEST_METHOD_VALUE_GET_CONFIRMED_TRANSACTIONS
         val signaturesEndpoint = RpcConstants.REQUEST_METHOD_VALUE_GET_CONFIRMED_SIGNATURES
