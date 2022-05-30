516b7b085b7ea37435507f0a0f14e3503a459765
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-05-30 15:45:55 +0300

    PWN-3206 - ssl cert added and used in app

--- app/src/main/java/org/p2p/wallet/infrastructure/network/NetworkModule.kt
+++ app/src/main/java/org/p2p/wallet/infrastructure/network/NetworkModule.kt
@@ -1,5 +1,6 @@
 package org.p2p.wallet.infrastructure.network
 
+import android.content.res.Resources
 import com.google.gson.GsonBuilder
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -25,13 +26,25 @@ import org.p2p.wallet.rpc.RpcModule.RPC_RETROFIT_QUALIFIER
 import org.p2p.wallet.updates.ConnectionStateProvider
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
+import java.io.InputStream
 import java.math.BigDecimal
+import java.security.GeneralSecurityException
+import java.security.KeyStore
+import java.security.cert.Certificate
+import java.security.cert.CertificateFactory
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
 
 object NetworkModule : InjectionModule {
 
     const val DEFAULT_CONNECT_TIMEOUT_SECONDS = 60L
     const val DEFAULT_READ_TIMEOUT_SECONDS = 60L
+
+    private const val SSL_CERT_TAG = "SSL_CERT"
 
     override fun create() = module {
         single { EnvironmentManager(get(), get()) }
@@ -50,30 +63,47 @@ object NetworkModule : InjectionModule {
 
         single(named(MOONPAY_QUALIFIER)) {
             val moonPayApiUrl = androidContext().getString(R.string.moonpayBaseUrl)
-            getRetrofit(moonPayApiUrl, "Moonpay", MoonpayErrorInterceptor(get()))
+            getRetrofit(
+                baseUrl = moonPayApiUrl,
+                tag = "Moonpay",
+                resources = get(),
+                interceptor = MoonpayErrorInterceptor(get())
+            )
         }
 
         single(named(RPC_RETROFIT_QUALIFIER)) {
             val environment = get<EnvironmentManager>().loadEnvironment()
             val rpcApiUrl = environment.endpoint
-            getRetrofit(rpcApiUrl, "Rpc", RpcInterceptor(get(), get()))
+            getRetrofit(
+                baseUrl = rpcApiUrl,
+                tag = "Rpc",
+                resources = get(),
+                interceptor = RpcInterceptor(get(), get())
+            )
         }
 
         single(named(NOTIFICATION_SERVICE_RETROFIT_QUALIFIER)) {
             val endpoint = androidContext().getString(R.string.notification_service_url)
-            getRetrofit(endpoint, "NotificationService", null)
+            getRetrofit(
+                baseUrl = endpoint,
+                tag = "NotificationService",
+                resources = get(),
+                interceptor = null
+            )
         }
     }
 
     fun Scope.getRetrofit(
         baseUrl: String,
         tag: String = "OkHttpClient",
+        resources: Resources,
         interceptor: Interceptor?
     ): Retrofit {
+
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create(get()))
-            .client(getClient(tag, interceptor))
+            .client(getClient(tag, resources, interceptor))
             .build()
     }
 
@@ -83,11 +113,25 @@ object NetworkModule : InjectionModule {
         }
     }
 
-    private fun Scope.getClient(tag: String, interceptor: Interceptor? = null): OkHttpClient {
+    private fun Scope.getClient(tag: String, resources: Resources, interceptor: Interceptor? = null): OkHttpClient {
         return OkHttpClient.Builder()
             .readTimeout(DEFAULT_CONNECT_TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .connectTimeout(DEFAULT_READ_TIMEOUT_SECONDS, TimeUnit.SECONDS)
             .apply {
+                var sslContext: SSLContext? = null
+                try {
+                    sslContext = createCertificate(resources.openRawResource(R.raw.cert))
+                } catch (e: Exception) {
+                    if (!BuildConfig.DEBUG) {
+                        Timber.tag(SSL_CERT_TAG).e(e, "Error on opening SSL cert")
+                    }
+                }
+                if (sslContext != null) {
+                    systemDefaultTrustManager()?.let { trustManager ->
+                        Timber.tag(SSL_CERT_TAG).i("SslSocketFactory successfully added with cert")
+                        sslSocketFactory(sslContext.socketFactory, trustManager)
+                    }
+                }
                 if (BuildConfig.CRASHLYTICS_ENABLED) {
                     addInterceptor(CrashHttpLoggingInterceptor())
                 }
@@ -102,5 +146,44 @@ object NetworkModule : InjectionModule {
             }
             .addNetworkInterceptor(ContentTypeInterceptor())
             .build()
+    }
+
+    private fun createCertificate(trustedCertificateIS: InputStream): SSLContext? {
+        val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
+        val ca: Certificate = trustedCertificateIS.use { trustedCertificateIS ->
+            cf.generateCertificate(trustedCertificateIS)
+        }
+
+        // creating a KeyStore containing our trusted CAs
+        val keyStoreType: String = KeyStore.getDefaultType()
+        val keyStore: KeyStore = KeyStore.getInstance(keyStoreType)
+        keyStore.load(null, null)
+        keyStore.setCertificateEntry("ca", ca)
+
+        // creating a TrustManager that trusts the CAs in our KeyStore
+        val tmfAlgorithm: String = TrustManagerFactory.getDefaultAlgorithm()
+        val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm)
+        tmf.init(keyStore)
+
+        // creating an SSLSocketFactory that uses our TrustManager
+        val sslContext: SSLContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, tmf.trustManagers, null)
+        return sslContext
+    }
+
+    private fun systemDefaultTrustManager(): X509TrustManager? {
+        return try {
+            val trustManagerFactory: TrustManagerFactory =
+                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+            trustManagerFactory.init(null as KeyStore?)
+            val trustManagers: Array<TrustManager> = trustManagerFactory.trustManagers
+            check(!(trustManagers.size != 1 || trustManagers[0] !is X509TrustManager)) {
+                "Unexpected default trust managers:" + trustManagers.contentToString()
+            }
+            trustManagers[0] as X509TrustManager
+        } catch (e: GeneralSecurityException) {
+            // The system has no TLS. Just give up.
+            throw AssertionError()
+        }
     }
 }
