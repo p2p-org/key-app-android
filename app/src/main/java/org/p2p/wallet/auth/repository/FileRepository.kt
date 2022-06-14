719b34faa231dc5ca7723c17b9c71bb3caa1ba24
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-06-14 11:15:28 +0300

    PWN-3779 - changes according to comments

--- app/src/main/java/org/p2p/wallet/auth/repository/FileRepository.kt
+++ app/src/main/java/org/p2p/wallet/auth/repository/FileRepository.kt
@@ -1,6 +1,5 @@
 package org.p2p.wallet.auth.repository
 
-import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.media.MediaScannerConnection
@@ -8,6 +7,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
+import androidx.core.content.contentValuesOf
 import kotlinx.coroutines.withContext
 import org.p2p.wallet.R
 import org.p2p.wallet.infrastructure.dispatchers.CoroutineDispatchers
@@ -18,6 +18,9 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStream
 import java.util.Date
+
+private const val PENDING = 1
+private const val NOT_PENDING = 0
 
 class FileRepository(
     private val context: Context,
@@ -92,25 +95,23 @@ class FileRepository(
                 val resolver = context.contentResolver
                 saveBitmapToFile(mainDir, fileName, bitmap).also { savedFile ->
                     if (toPublic) {
-                        resolver.openInputStream(Uri.fromFile(savedFile)).use { input ->
-                            val contentValues = ContentValues().apply {
-                                put(MediaStore.Images.Media.DISPLAY_NAME, "$fileName.png")
-                                put(MediaStore.Images.Media.MIME_TYPE, mimeType)
-                                put(MediaStore.Images.Media.RELATIVE_PATH, "${Environment.DIRECTORY_PICTURES}/$appName")
-                                put(MediaStore.Images.Media.IS_PENDING, 1)
-                            }
+                        resolver.openInputStream(Uri.fromFile(savedFile))?.use { input ->
+                            val contentValues = contentValuesOf(
+                                MediaStore.Images.Media.DISPLAY_NAME to "$fileName.png",
+                                MediaStore.Images.Media.MIME_TYPE to mimeType,
+                                MediaStore.Images.Media.RELATIVE_PATH to "${Environment.DIRECTORY_PICTURES}/$appName",
+                                MediaStore.Images.Media.IS_PENDING to PENDING,
+                            )
 
                             val collection = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
                             val uri = resolver.insert(collection, contentValues)
                                 ?: throw IOException("Unable to get Uri: $contentValues")
-                            resolver.openOutputStream(uri).use { out ->
-                                out?.let { output ->
-                                    input?.copyTo(output)
-                                }
+                            resolver.openOutputStream(uri)?.use { out ->
+                                input.copyTo(out)
                             }
 
                             contentValues.clear()
-                            contentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+                            contentValues.put(MediaStore.Images.Media.IS_PENDING, NOT_PENDING)
                             resolver.update(uri, contentValues, null, null)
                         }
                     }
