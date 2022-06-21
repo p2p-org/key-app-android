9795279b00cdd3d00cbf114c2075c222db39b6a2
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-06-21 11:14:20 +0300

    PWN-3867 - migrate moonpay on chromeTabs

--- app/src/main/java/org/p2p/wallet/utils/TabExtensions.kt
+++ app/src/main/java/org/p2p/wallet/utils/TabExtensions.kt
@@ -3,14 +3,31 @@ package org.p2p.wallet.utils
 import android.content.Context
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.fragment.app.Fragment
+import org.p2p.wallet.R
 
 fun Context.showUrlInCustomTabs(url: String) {
     val uri = Uri.parse(url)
-    val customTabsIntent = CustomTabsIntent.Builder()
-        .setShowTitle(true)
-        .build()
+    val context = this
+    val customTabsIntent = CustomTabsIntent.Builder().apply {
+        setShowTitle(true)
+        val mainBackgroundColor = context.getColor(R.color.backgroundPrimary)
+        val defaultColors = CustomTabColorSchemeParams.Builder()
+            .setToolbarColor(mainBackgroundColor)
+            .setNavigationBarColor(mainBackgroundColor)
+            .setNavigationBarDividerColor(mainBackgroundColor)
+            .setSecondaryToolbarColor(context.getColor(R.color.backgroundButtonPrimary))
+            .build()
+        setDefaultColorSchemeParams(defaultColors)
+        val backBitmap = BitmapUtils.fromVectorDrawable(context, R.drawable.ic_back)
+        if (backBitmap != null) {
+            setCloseButtonIcon(backBitmap)
+        }
+        setStartAnimations(context, R.anim.nav_enter, R.anim.nav_exit)
+        setShareState(CustomTabsIntent.SHARE_STATE_OFF)
+    }.build()
     customTabsIntent.launchUrl(this, uri)
 }
 
