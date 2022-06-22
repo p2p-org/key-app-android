c6ececddbab0a1b17c0078b358a6f9d31dd78da5
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-06-22 12:30:20 +0300

    PWN-3877 - splash screen prepared for integration

---
+++ app/src/main/java/org/p2p/wallet/splash/SplashContract.kt
@@ -0,0 +1,13 @@
+package org.p2p.wallet.splash
+
+import org.p2p.wallet.common.mvp.MvpPresenter
+import org.p2p.wallet.common.mvp.MvpView
+
+interface SplashContract {
+    interface View : MvpView {
+        fun navigateToOnboarding()
+        fun navigateToSignIn()
+    }
+
+    interface Presenter : MvpPresenter<View>
+}
---
+++ app/src/main/java/org/p2p/wallet/splash/SplashFragment.kt
@@ -0,0 +1,27 @@
+package org.p2p.wallet.splash
+
+import org.koin.android.ext.android.inject
+import org.p2p.wallet.R
+import org.p2p.wallet.auth.ui.onboarding.OnboardingFragment
+import org.p2p.wallet.auth.ui.pin.signin.SignInPinFragment
+import org.p2p.wallet.common.mvp.BaseMvpFragment
+import org.p2p.wallet.utils.replaceFragment
+
+class SplashFragment :
+    BaseMvpFragment<SplashContract.View, SplashContract.Presenter>(R.layout.fragment_splash),
+    SplashContract.View {
+
+    companion object {
+        fun create() = SplashFragment()
+    }
+
+    override val presenter: SplashContract.Presenter by inject()
+
+    override fun navigateToOnboarding() {
+        replaceFragment(OnboardingFragment())
+    }
+
+    override fun navigateToSignIn() {
+        replaceFragment(SignInPinFragment.create())
+    }
+}
