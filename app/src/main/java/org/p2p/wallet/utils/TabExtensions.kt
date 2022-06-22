26214afa8d48ff9162114cde572d3820a5dd7031
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-06-22 09:22:44 +0300

    PWN-3867 - changes according to comments

--- app/src/main/java/org/p2p/wallet/utils/TabExtensions.kt
+++ app/src/main/java/org/p2p/wallet/utils/TabExtensions.kt
@@ -13,22 +13,31 @@ fun Context.showUrlInCustomTabs(url: String) {
     val context = this
     val customTabsIntent = CustomTabsIntent.Builder().apply {
         setShowTitle(true)
-        val mainBackgroundColor = context.getColor(R.color.backgroundPrimary)
-        val defaultColors = CustomTabColorSchemeParams.Builder()
-            .setToolbarColor(mainBackgroundColor)
-            .setNavigationBarColor(mainBackgroundColor)
-            .setNavigationBarDividerColor(mainBackgroundColor)
-            .setSecondaryToolbarColor(context.getColor(R.color.backgroundButtonPrimary))
-            .build()
-        setDefaultColorSchemeParams(defaultColors)
-        val backBitmap = BitmapUtils.fromVectorDrawable(context, R.drawable.ic_back)
-        if (backBitmap != null) {
-            setCloseButtonIcon(backBitmap)
-        }
+        setColors(context)
+        setCloseIcon(context)
         setStartAnimations(context, R.anim.nav_enter, R.anim.nav_exit)
         setShareState(CustomTabsIntent.SHARE_STATE_OFF)
-    }.build()
+    }
+        .build()
     customTabsIntent.launchUrl(this, uri)
+}
+
+private fun CustomTabsIntent.Builder.setColors(context: Context) {
+    val mainBackgroundColor = context.getColor(R.color.backgroundPrimary)
+    val defaultColors = CustomTabColorSchemeParams.Builder()
+        .setToolbarColor(mainBackgroundColor)
+        .setNavigationBarColor(mainBackgroundColor)
+        .setNavigationBarDividerColor(mainBackgroundColor)
+        .setSecondaryToolbarColor(context.getColor(R.color.backgroundButtonPrimary))
+        .build()
+    setDefaultColorSchemeParams(defaultColors)
+}
+
+private fun CustomTabsIntent.Builder.setCloseIcon(context: Context) {
+    val backBitmap = BitmapUtils.fromVectorDrawable(context, R.drawable.ic_back)
+    if (backBitmap != null) {
+        setCloseButtonIcon(backBitmap)
+    }
 }
 
 fun Fragment.showUrlInCustomTabs(url: String) {
