616e7d0bfd95fb8180fa0231b577b3a702cafd7f
Roman <roman.kuznetsov@p2p.org>
2022-06-24 13:45:48 +0300

    PWN-3948 - Add SectionHeader view

---
+++ ui-kit/src/main/kotlin/org/p2p/uikit/organisms/SectionHeader.kt
@@ -0,0 +1,52 @@
+package org.p2p.uikit.organisms
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.core.view.isVisible
+import org.p2p.uikit.R
+import org.p2p.uikit.databinding.WidgetSectionHeaderBinding
+
+class SectionHeader @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val binding = WidgetSectionHeaderBinding.inflate(LayoutInflater.from(context), this)
+
+    init {
+        context.obtainStyledAttributes(attrs, R.styleable.SectionHeader).use { typedArray ->
+            val showIcon = typedArray.getBoolean(R.styleable.SectionHeader_showIcon, true)
+            val defaultColor = context.getColor(R.color.text_mountain)
+            val tint = typedArray.getColor(R.styleable.SectionHeader_textIconTint, defaultColor)
+            val text = typedArray.getString(R.styleable.SectionHeader_sectionText)
+            val isCaps = typedArray.getBoolean(R.styleable.SectionHeader_isCaps, false)
+
+            binding.apply {
+                chevronImageView.isVisible = showIcon
+                titleTextView.isAllCaps = isCaps
+                titleTextView.text = text
+                titleTextView.setTextColor(tint)
+                chevronImageView.setColorFilter(tint)
+            }
+        }
+    }
+
+    fun setText(text: String) {
+        binding.titleTextView.text = text
+    }
+
+    fun showIcon(isVisible: Boolean) {
+        binding.chevronImageView.isVisible = isVisible
+    }
+
+    fun setHidden(isHidden: Boolean) {
+        val rotationValue = if (isHidden) 0f else 180f
+        binding.chevronImageView
+            .animate()
+            .rotation(rotationValue)
+            .start()
+    }
+}
