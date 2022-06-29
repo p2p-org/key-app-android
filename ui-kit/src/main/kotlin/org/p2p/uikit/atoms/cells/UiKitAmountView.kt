5adc635ecd3901f449dc8d42acaed94863af2f74
Roman <roman.kuznetsov@p2p.org>
2022-06-29 15:54:22 +0300

    PWN-3891 - Add icon to UiKitAmountView

--- ui-kit/src/main/kotlin/org/p2p/uikit/atoms/cells/UiKitAmountView.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/atoms/cells/UiKitAmountView.kt
@@ -2,10 +2,12 @@ package org.p2p.uikit.atoms.cells
 
 import android.content.Context
 import android.util.AttributeSet
+import androidx.annotation.DrawableRes
 import androidx.constraintlayout.widget.ConstraintLayout
 import org.p2p.uikit.R
 import org.p2p.uikit.databinding.WidgetAmountViewBinding
 import org.p2p.uikit.utils.inflateViewBinding
+import org.p2p.uikit.utils.withImageOrGone
 import org.p2p.uikit.utils.withTextOrGone
 
 class UiKitAmountView @JvmOverloads constructor(
@@ -26,10 +28,19 @@ class UiKitAmountView @JvmOverloads constructor(
             field = value
         }
 
+    @DrawableRes
+    var icon: Int? = null
+        set(value) {
+            binding.actionImageView.withImageOrGone(value)
+            field = value
+        }
+
     private val binding = inflateViewBinding<WidgetAmountViewBinding>()
 
     init {
         context.obtainStyledAttributes(attrs, R.styleable.UiKitAmountView).use { typedArray ->
+            val iconRes = typedArray.getResourceId(R.styleable.UiKitAmountView_icon, -1)
+            icon = if (iconRes != -1) iconRes else null
             usdAmount = typedArray.getString(R.styleable.UiKitAmountView_usdAmount)
             tokenAmount = typedArray.getString(R.styleable.UiKitAmountView_tokenAmount)
         }
