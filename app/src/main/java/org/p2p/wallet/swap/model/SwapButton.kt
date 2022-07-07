d17863e94746ea7d79e767d21ada474b45d43569
Roman <roman.kuznetsov@p2p.org>
2022-07-07 16:09:03 +0300

    PWN-4120 - Check destination validation before empty pairs

--- app/src/main/java/org/p2p/wallet/swap/model/SwapButton.kt
+++ app/src/main/java/org/p2p/wallet/swap/model/SwapButton.kt
@@ -43,10 +43,10 @@ class SwapButton(
 
             return when {
                 !isValidAmount -> State.Disabled(R.string.main_enter_the_amount)
+                !isValidDestination -> State.Disabled(R.string.swap_choose_the_destination)
                 isPairEmpty -> State.Disabled(R.string.swap_cannot_swap_these_tokens)
                 !isEnoughBalance -> State.Disabled(R.string.swap_funds_not_enough)
                 !isEnoughToCoverExpenses -> State.Disabled(R.string.swap_insufficient_funds)
-                !isValidDestination -> State.Disabled(R.string.swap_choose_the_destination)
                 else -> {
                     State.Enabled(
                         textRes = R.string.swap_format,
