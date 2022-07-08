552fce73c6673b8f2ac4c94781b3404c11992513
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2022-07-08 11:21:13 +0300

    PWN-3966 - migrate on new url -> and remove not needed values (#579)
    * PWN-3966 - migrate on new url -> and remove not needed values
    
    * PWN-3966 - tests fixes

--- app/src/main/java/org/p2p/wallet/swap/model/orca/OrcaConfigs.kt
+++ app/src/main/java/org/p2p/wallet/swap/model/orca/OrcaConfigs.kt
@@ -2,7 +2,6 @@ package org.p2p.wallet.swap.model.orca
 
 class OrcaConfigs(
     val tokens: OrcaTokens,
-    val aquafarms: OrcaAquafarms,
     val pools: OrcaPools,
     val programId: OrcaProgramId,
 )
