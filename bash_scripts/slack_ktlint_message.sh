28d5d4e3dbaa05e0d091c9f5b2f8f184b379ea44
khasanov.davran <khasanov.davran@p2p.org>
2022-07-11 20:56:54 +0600

    PWN-4195 - Fix CI workflow

---
+++ bash_scripts/slack_ktlint_message.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+slack_ktlint_message="Запущен процесс новой сборки (25%):
+1 Ktlint :white_check_mark:
+3 Update gradle version :man-running:
+4 Upload to Firebase :man-running:
+5 Upload to Slack :hourglass:
+Сборка почти готова, ожидаем апрува от разработчика чтоб доставить вам новый билд ))"
+
+curl \
+-F token="$1" \
+-F channel="$2" \
+-F ts="$3" \
+-F text="${slack_ktlint_message}" \
+https://slack.com/api/chat.update
