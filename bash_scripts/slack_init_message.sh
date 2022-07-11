12b47e4d58964ebbb7b22c3fec0dbb6670768ff3
khasanov.davran <khasanov.davran@p2p.org>
2022-07-11 15:18:19 +0600

    PWN-4195 - Update Slack Integration

---
+++ bash_scripts/slack_init_message.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+slack_ktlint_message="Запущен процесс новой сборки (0%):
+1 Ktlint :man-running:
+2 Unit Tests :man-running:
+4 Upload to Firebase :hourglass:
+5 Upload to Slack :hourglass: "
+
+SLACK_SEND_MESSAGE_RESPONSE=$(curl \
+    -F token="${SLACK_CHANNEL_ACCESS_TOKEN}" \
+    -F channel="${SLACK_CHANNEL_ID}" \
+    -F text="$slack_ktlint_message" \
+    https://slack.com/api/chat.postMessage
+    )
+echo "$SLACK_SEND_MESSAGE_RESPONSE" > timestamp.json
+echo $(sed -n 's|.*"ts":"\([^"]*\)".*|\1|p' timestamp.json ) > timestamp.txt
