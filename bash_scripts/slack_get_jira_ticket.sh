9ca028fa4bc254134b6ed2819d46678db6608730
khasanov.davran <khasanov.davran@p2p.org>
2022-07-12 17:41:39 +0600

    PWN-4195 - Add additional statement for checking branch name

--- bash_scripts/slack_get_jira_ticket.sh
+++ bash_scripts/slack_get_jira_ticket.sh
@@ -1,11 +1,12 @@
 branch_name="$GITHUB_HEAD_REF"
-if [ -z "$branch_name" ]; then
-  branch_name="$GITHUB_REF"
-fi
 cut_branch_name=$( echo ${branch_name##*/} )
 IFS='-'
 read -r -a array <<< "$cut_branch_name"
-jira_ticket_message="https://p2pvalidator.atlassian.net/browse/${array[0]}-${array[1]}"
+jira_ticket_name="${array[0]}-${array[1]}"
+if [ -z "$jira_ticket_name" ]; then
+  jira_ticket_name="$GITHUB_REF"
+fi
+jira_ticket_message="https://p2pvalidator.atlassian.net/browse/$jira_ticket_name"
 
 curl \
 -F token="$1" \
