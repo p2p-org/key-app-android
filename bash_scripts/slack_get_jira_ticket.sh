9698752fcc8f0accf1ca83520f62ad1887ce95af
davran1001 <87668832+davran1001@users.noreply.github.com>
2022-07-12 18:03:52 +0600

    PWN-4195 - Update Github Actions BUG (#587)
    * PWN-3819 - Fix showing keyboard when user click on key frame
    
    * PWN-3819 - Fix bug when keyboard dissapear after hide and show app
    
    * PWN-3819 - Fix ktlint
    
    * PWN-4195 - Update Slack Integration
    
    * PWN-4195 - Update CI workflow
    
    * PWN-4195 - Update CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow variables
    
    * PWN-4195 - Fix CI workflow variables
    
    * PWN-4195 - Fix CI workflow variables, and refactor
    
    * PWN-4195 - Fix CI workflow fix branch variable
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Update CI, add last step message, add jira ticket to build message
    
    * PWN-4195 - Translate all russian text to english
    
    * PWN-4195 - Fix yaml file
    
    * PWN-4195 - Resolve PR comments, finish integration
    
    * PWN-4195 - Fix last message sending
    
    * PWN-4195 - Fix branch name on push event
    
    * PWN-4195 - Add additional statement for checking branch name
    
    Co-authored-by: khasanov.davran <khasanov.davran@p2p.org>

--- bash_scripts/slack_get_jira_ticket.sh
+++ bash_scripts/slack_get_jira_ticket.sh
@@ -2,7 +2,11 @@ branch_name="$GITHUB_HEAD_REF"
 cut_branch_name=$( echo ${branch_name##*/} )
 IFS='-'
 read -r -a array <<< "$cut_branch_name"
-jira_ticket_message="https://p2pvalidator.atlassian.net/browse/${array[0]}-${array[1]}"
+jira_ticket_name="${array[0]}-${array[1]}"
+if [ -z "$jira_ticket_name" ]; then
+  jira_ticket_name="$GITHUB_REF"
+fi
+jira_ticket_message="https://p2pvalidator.atlassian.net/browse/$jira_ticket_name"
 
 curl \
 -F token="$1" \
