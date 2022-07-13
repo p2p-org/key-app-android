3d1b28d61021e0c303bea73699a8116e25e5d79f
davran1001 <87668832+davran1001@users.noreply.github.com>
2022-07-13 22:59:25 +0600

    PWN-4195 - Update CI workflow (#590)
    * PWN-3819 - Fix showing keyboard when user click on key frame
    
    * PWN-3819 - Fix bug when keyboard dissapear after hide and show app
    
    * PWN-3819 - Fix ktlint
    
    * PWN-4195 - Update Slack Integration
    
    * PWN-4195 - Update CI workflow
    
    * PWN-4195 - Update CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow variables
    
    * PWN-4195 - Fix CI workflow variables
    
    * PWN-4195 - Fix CI workflow variables, and refactor
    
    * PWN-4195 - Fix CI workflow fix branch variable
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Fix CI workflow
    
    * PWN-4195 - Update CI, add last step message, add jira ticket to build message
    
    * PWN-4195 - Translate all russian text to english
    
    * PWN-4195 - Fix yaml file
    
    * PWN-4195 - Resolve PR comments, finish integration
    
    * PWN-4195 - Fix last message sending
    
    * PWN-4195 - Fix branch name on push event
    
    * PWN-4195 - Add additional statement for checking branch name
    
    * PWN-4195 - Update feature workflow
    
    * PWN-4195 - Remove needs for start CI workflow
    
    * Update deploy progress message CI
    
    Co-authored-by: khasanov.davran <khasanov.davran@p2p.org>

--- bash_scripts/slack_deploy_finish_message.sh
+++ bash_scripts/slack_deploy_finish_message.sh
@@ -6,14 +6,10 @@ IFS='-'
 read -r -a array <<< "$cut_branch_name"
 jira_ticket_message="https://p2pvalidator.atlassian.net/browse/${array[0]}-${array[1]}"
 
-slack_ktlint_message="The process of creating a new assembly is started (25%):
-Jira Ticket - $jira_ticket_message
-1 Ktlint :white_check_mark:
-2 Unit Tests :man-running:
-4 Upload to Firebase :hourglass:
-5 Upload to Slack :hourglass: "
-
-
+slack_ktlint_message="*The process of creating a new assembly is finished (100%)*:
+• *Jira Ticket* - $jira_ticket_message
+• *Upload to Firebase* -:white_check_mark:
+• *Upload to Slack* - :white_check_mark:"
 curl \
 -F token="$1" \
 -F channel="$2" \
--- bash_scripts/slack_deploy_start_message.sh
+++ bash_scripts/slack_deploy_start_message.sh
@@ -6,18 +6,15 @@ IFS='-'
 read -r -a array <<< "$cut_branch_name"
 jira_ticket_message="https://p2pvalidator.atlassian.net/browse/${array[0]}-${array[1]}"
 
-slack_ktlint_message="The process of creating a new assembly is started (0%):
-Jira Ticket - $jira_ticket_message
-1 Ktlint :man-running:
-2 Unit Tests :man-running:
-4 Upload to Firebase :hourglass:
-5 Upload to Slack :hourglass: "
-
+slack_ktlint_message="*The process of creating a new assembly is started (0%)*:
+• *Jira Ticket* - $jira_ticket_message
+• *Upload to Firebase* -::athletic_shoe:
+• *Upload to Slack* - :athletic_shoe:"
 SLACK_SEND_MESSAGE_RESPONSE=$(curl \
     -F token="$1" \
     -F channel="$2"\
     -F text="$slack_ktlint_message" \
     https://slack.com/api/chat.postMessage
-    )
+)
 echo "$SLACK_SEND_MESSAGE_RESPONSE" > timestamp.json
 echo $(sed -n 's|.*"ts":"\([^"]*\)".*|\1|p' timestamp.json ) > timestamp.txt
