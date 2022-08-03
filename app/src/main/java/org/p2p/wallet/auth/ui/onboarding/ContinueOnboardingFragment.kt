8523e6241b5175523c7ca5cbba1640911f5a5db2
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-08-03 19:49:53 +0300

    PWN-4268 - navigate to phone screen from continue

--- app/src/main/java/org/p2p/wallet/auth/ui/onboarding/ContinueOnboardingFragment.kt
+++ app/src/main/java/org/p2p/wallet/auth/ui/onboarding/ContinueOnboardingFragment.kt
@@ -2,12 +2,13 @@ package org.p2p.wallet.auth.ui.onboarding
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.activity.addCallback
 import org.p2p.wallet.R
+import org.p2p.wallet.auth.ui.phone.PhoneNumberEnterFragment
 import org.p2p.wallet.common.mvp.BaseFragment
 import org.p2p.wallet.databinding.FragmentContinueOnboardingBinding
 import org.p2p.wallet.utils.popBackStack
+import org.p2p.wallet.utils.replaceFragment
 import org.p2p.wallet.utils.viewbinding.viewBinding
 
 class ContinueOnboardingFragment : BaseFragment(R.layout.fragment_continue_onboarding) {
@@ -24,8 +25,7 @@ class ContinueOnboardingFragment : BaseFragment(R.layout.fragment_continue_onboa
         with(binding) {
             onboardingContinueText.text = getString(R.string.onboarding_continue_subtitle, "test****@gmail.com")
             onboardingContinueButton.setOnClickListener {
-                // TODO PWN-4268 make real implementation and move user to phone number screen
-                Toast.makeText(context, "In development right now", Toast.LENGTH_SHORT).show()
+                replaceFragment(PhoneNumberEnterFragment.create())
             }
         }
 
