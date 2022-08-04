269863267abbcd9a5ec86bbe22c8621b133bd07c
khasanov.davran <khasanov.davran@p2p.org>
2022-08-04 21:49:03 +0600

    PWN-4397 - Resolve PR comments

--- ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitBottomNavigationView.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitBottomNavigationView.kt
@@ -3,8 +3,8 @@ package org.p2p.uikit.components
 import android.content.Context
 import android.util.AttributeSet
 import android.view.Menu
-import android.view.MenuItem
 import androidx.constraintlayout.widget.ConstraintLayout
+import org.p2p.uikit.R
 import org.p2p.uikit.databinding.WidgetBottomNavigationViewBinding
 import org.p2p.uikit.utils.inflateViewBinding
 
@@ -14,24 +14,41 @@ class UiKitBottomNavigationView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    enum class ScreenTabs {
-        HOME_SCREEN, HISTORY_SCREEN, SEND_SCREEN, FEEDBACK_SCREEN, SETTINGS_SCREEN
-    }
-
     val binding = inflateViewBinding<WidgetBottomNavigationViewBinding>()
 
-    var selectedItemId: Int = binding.bottoNavigationView.selectedItemId
+    init {
+        binding.bottomNavigationView.inflateMenu(R.menu.menu_ui_kit_bottom_navigation)
+    }
+
+    var selectedItemId: ScreenTab = getScreenTabByMenuItemId(binding.bottomNavigationView.selectedItemId)
 
     val menu: Menu
-        get() = binding.bottoNavigationView.menu
+        get() = binding.bottomNavigationView.menu
 
-    fun setOnItemSelectedListener(block: (MenuItem) -> Boolean) {
-        binding.bottoNavigationView.setOnItemSelectedListener {
-            return@setOnItemSelectedListener block(it)
+    fun setOnItemSelectedListener(block: (ScreenTab) -> Boolean) {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            val screenTabItem = getScreenTabByMenuItemId(it.itemId)
+            block(screenTabItem)
+            return@setOnItemSelectedListener true
         }
     }
 
-    fun inflateMenu(resId: Int) {
-        binding.bottoNavigationView.inflateMenu(resId)
+    fun setOnCenterActionButtonListener(block: () -> Unit) {
+        binding.buttonCenterAction.setOnClickListener { block.invoke() }
+    }
+
+    private fun getScreenTabByMenuItemId(menuItemId: Int): ScreenTab {
+        return when (menuItemId) {
+            R.id.homeItem -> ScreenTab.HOME_SCREEN
+            R.id.historyItem -> ScreenTab.HISTORY_SCREEN
+            R.id.sendItem -> ScreenTab.SEND_SCREEN
+            R.id.feedbackItem -> ScreenTab.FEEDBACK_SCREEN
+            R.id.settingsItem -> ScreenTab.SETTINGS_SCREEN
+            else -> throw IllegalStateException("No screen tab found for menu item = $menuItemId")
+        }
+    }
+
+    enum class ScreenTab {
+        HOME_SCREEN, HISTORY_SCREEN, SEND_SCREEN, FEEDBACK_SCREEN, SETTINGS_SCREEN
     }
 }
