4d05627feaed698d1ec14f747b7cbfc542dcbe3a
Roman <roman.kuznetsov@p2p.org>
2022-08-17 12:34:27 +0300

    PWN-4447 - Temporary open NewCreatePinFragment from splash

--- app/src/main/java/org/p2p/wallet/splash/SplashFragment.kt
+++ app/src/main/java/org/p2p/wallet/splash/SplashFragment.kt
@@ -2,7 +2,7 @@ package org.p2p.wallet.splash
 
 import org.koin.android.ext.android.inject
 import org.p2p.wallet.R
-import org.p2p.wallet.auth.ui.onboarding.root.OnboardingRootFragment
+import org.p2p.wallet.auth.ui.pin.newcreate.NewCreatePinFragment
 import org.p2p.wallet.auth.ui.pin.signin.SignInPinFragment
 import org.p2p.wallet.common.mvp.BaseMvpFragment
 import org.p2p.wallet.utils.replaceFragment
@@ -18,7 +18,8 @@ class SplashFragment :
     override val presenter: SplashContract.Presenter by inject()
 
     override fun navigateToOnboarding() {
-        replaceFragment(OnboardingRootFragment.create())
+        // TODO: Replace back with OnboardingRootFragment after QA
+        replaceFragment(NewCreatePinFragment.create())
     }
 
     override fun navigateToSignIn() {
