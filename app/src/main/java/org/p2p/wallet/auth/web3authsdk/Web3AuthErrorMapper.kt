ad50c89d9d843541e46acf86e36316a65dabac6d
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2022-08-17 13:45:29 +0300

    NO-TICKET - fix communication channel with Web3Auth

---
+++ app/src/main/java/org/p2p/wallet/auth/web3authsdk/Web3AuthErrorMapper.kt
@@ -0,0 +1,26 @@
+package org.p2p.wallet.auth.web3authsdk
+
+import com.google.gson.Gson
+import org.p2p.wallet.auth.model.Web3AuthSignUpResponse
+import org.p2p.wallet.utils.fromJsonReified
+import timber.log.Timber
+
+class Web3AuthErrorMapper(private val gson: Gson) {
+    fun fromNetworkSignUp(responseJson: String): Web3AuthSignUpResponse? {
+        return gson.fromJsonReified<Web3AuthSignUpResponse>(responseJson)
+    }
+
+    fun fromNetworkError(errorResponseJson: String): Web3AuthErrorResponse {
+        return gson.fromJsonReified<Web3AuthErrorResponse>(errorResponseJson)
+            ?.let { createFilledResponseWithErrorType(it) }
+            ?: error("Response mapping failed from: $errorResponseJson")
+    }
+
+    private fun createFilledResponseWithErrorType(response: Web3AuthErrorResponse): Web3AuthErrorResponse {
+        val errorType = Web3AuthErrorResponse.ErrorType.findByCode(response.errorCode)
+        if (errorType == Web3AuthErrorResponse.ErrorType.UNDEFINED) {
+            Timber.tag("Web3AuthErrorMapper").e("No code found for response: ${response.errorCode}")
+        }
+        return response.copy(errorType = errorType)
+    }
+}
--- app/src/main/java/org/p2p/wallet/auth/web3authsdk/Web3AuthErrorResponse.kt
+++ app/src/main/java/org/p2p/wallet/auth/web3authsdk/Web3AuthErrorResponse.kt
@@ -1,11 +1,39 @@
 package org.p2p.wallet.auth.web3authsdk
 
 import com.google.gson.annotations.SerializedName
-import java.lang.Error
 
 data class Web3AuthErrorResponse(
     @SerializedName("name") val errorName: String,
     @SerializedName("code") val errorCode: Int,
     @SerializedName("message") val errorMessage: String,
     @SerializedName("stack") var stack: String?,
-) : Error(errorMessage)
+
+    // changes during the mapping
+    @Transient val errorType: ErrorType = ErrorType.UNDEFINED
+) : Error(errorMessage) {
+    // https://github.com/p2p-org/web3-auth-sdk-new/blob/main/packages/sdk/src/errors/typeMap.ts
+    enum class ErrorType(val code: Int) {
+        UNDEFINED(code = -1),
+        WRONG_SHARE_TYPE(code = 1001),
+        NO_TKEY_INSTANCE(code = 1002),
+        INVALID_MNEMONIC(code = 1003),
+        NO_TOKEN_PROVIDED(code = 1004),
+        NO_DEVICE_SHARE(code = 1005),
+        CUSTOM_SHARE_EXIST(code = 1006),
+        BAD_MAC(code = 1007),
+        HEX_TO_BIN_CONVERSION(code = 1008),
+        CANNOT_RECONSTRUCT(code = 1009),
+        CANNOT_READ_WEB_STORAGE(code = 1011),
+        CANNOT_INITIALIZE_TKEY(code = 1012),
+        DUPLICATE_TOKEN(code = 1013),
+        READ_VERSION_ERROR(code = 1014),
+        NO_CUSTOM_SHARE(code = 1015),
+        USER_NOT_REGISTERED(code = 1016),
+        NO_CUSTOM_METADATA(code = 1017),
+        INVALID_INIT_ARGS(code = 1018);
+
+        companion object {
+            fun findByCode(code: Int): ErrorType = values().find { it.code == code } ?: UNDEFINED
+        }
+    }
+}
