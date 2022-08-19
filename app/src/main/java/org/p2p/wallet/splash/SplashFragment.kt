4fbce0e2c73ad89f23a5ee1416cde11109696cc6
Roman <roman.kuznetsov@p2p.org>
2022-08-19 12:55:38 +0300

    NO-TICKET - Remove mock screen navigation

--- app/src/main/java/org/p2p/wallet/splash/SplashFragment.kt
+++ app/src/main/java/org/p2p/wallet/splash/SplashFragment.kt
@@ -2,7 +2,7 @@ package org.p2p.wallet.splash
 
 import org.koin.android.ext.android.inject
 import org.p2p.wallet.R
-import org.p2p.wallet.auth.ui.pin.newcreate.NewCreatePinFragment
+import org.p2p.wallet.auth.ui.onboarding.root.OnboardingRootFragment
 import org.p2p.wallet.auth.ui.pin.signin.SignInPinFragment
 import org.p2p.wallet.common.mvp.BaseMvpFragment
 import org.p2p.wallet.utils.replaceFragment
@@ -18,8 +18,7 @@ class SplashFragment :
     override val presenter: SplashContract.Presenter by inject()
 
     override fun navigateToOnboarding() {
-        // TODO: Replace back with OnboardingRootFragment after QA
-        replaceFragment(NewCreatePinFragment.create())
+        replaceFragment(OnboardingRootFragment.create())
     }
 
     override fun navigateToSignIn() {
