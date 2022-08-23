0554efa17f1900ce70cebb143e7e1071ac2a175b
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2022-08-23 10:43:35 +0300

    PWN-4385 - review fixes

--- app/src/main/java/org/p2p/wallet/auth/model/Web3AuthSignInResponse.kt
+++ app/src/main/java/org/p2p/wallet/auth/model/Web3AuthSignInResponse.kt
@@ -3,7 +3,9 @@ package org.p2p.wallet.auth.model
 import com.google.gson.annotations.SerializedName
 
 data class Web3AuthSignInResponse(
-    @SerializedName("ethPublic") val ethereumPublicKey: String,
+    @SerializedName("ethPublic")
+    val ethereumPublicKey: String,
     // bip39 mnemonic
-    @SerializedName("privateSOL") val mnemonicPhrase: String,
+    @SerializedName("privateSOL")
+    val mnemonicPhrase: String,
 )
