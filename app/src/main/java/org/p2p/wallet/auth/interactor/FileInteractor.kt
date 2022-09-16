86c3218c73b83ede729175b844d2ab7ac1b39cff
Roman <roman.kuznetsov@p2p.org>
2022-09-16 10:55:39 +0300

    PWN-4346 - Changes by comments

--- app/src/main/java/org/p2p/wallet/auth/interactor/FileInteractor.kt
+++ app/src/main/java/org/p2p/wallet/auth/interactor/FileInteractor.kt
@@ -4,17 +4,15 @@ import android.content.Context
 import org.p2p.wallet.auth.repository.FileRepository
 import java.io.File
 
+private const val TERMS_OF_USE_PDF = "p2p_terms_of_service.pdf"
+private const val PRIVACY_POLICY_PDF = "p2p_privacy_policy.pdf"
+private const val TERMS_OF_USE = "p2p_terms_of_service"
+private const val PRIVACY_POLICY = "p2p_privacy_policy"
+
 class FileInteractor(
     private val context: Context,
     private val fileRepository: FileRepository
 ) {
-
-    companion object {
-        private const val TERMS_OF_USE_PDF = "p2p_terms_of_service.pdf"
-        private const val PRIVACY_POLICY_PDF = "p2p_privacy_policy.pdf"
-        private const val TERMS_OF_USE = "p2p_terms_of_service"
-        private const val PRIVACY_POLICY = "p2p_privacy_policy"
-    }
 
     fun getTermsOfUseFile(): File {
         return if (fileRepository.isFileExists(TERMS_OF_USE_PDF)) {
