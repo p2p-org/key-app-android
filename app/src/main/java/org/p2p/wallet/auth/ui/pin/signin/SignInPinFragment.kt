ebf2d3444d1c64a7f23e41ae1ca157c07bbcd797
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2022-09-19 14:57:36 +0300

    PWN-5173 - do not show progress on pin-code sign in (#760)

--- app/src/main/java/org/p2p/wallet/auth/ui/pin/signin/SignInPinFragment.kt
+++ app/src/main/java/org/p2p/wallet/auth/ui/pin/signin/SignInPinFragment.kt
@@ -60,10 +60,6 @@ class SignInPinFragment :
         }
     }
 
-    override fun showLoading(isLoading: Boolean) {
-        binding.pinView.showLoading(isLoading)
-    }
-
     override fun onStart() {
         super.onStart()
         presenter.checkIfBiometricAvailable()
@@ -74,6 +70,7 @@ class SignInPinFragment :
     }
 
     override fun onSignInSuccess() {
+        binding.pinView.onSuccessPin()
         popAndReplaceFragment(MainFragment.create(), inclusive = true)
     }
 
