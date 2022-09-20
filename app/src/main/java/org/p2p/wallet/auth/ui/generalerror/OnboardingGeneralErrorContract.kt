16b2089d97da022fa52fd52f939825bf3531c4b6
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-09-20 11:34:41 +0300

    PWN-5192 - remove not needed code related to terms

--- app/src/main/java/org/p2p/wallet/auth/ui/generalerror/OnboardingGeneralErrorContract.kt
+++ app/src/main/java/org/p2p/wallet/auth/ui/generalerror/OnboardingGeneralErrorContract.kt
@@ -2,17 +2,12 @@ package org.p2p.wallet.auth.ui.generalerror
 
 import org.p2p.wallet.common.mvp.MvpPresenter
 import org.p2p.wallet.common.mvp.MvpView
-import java.io.File
 
 interface OnboardingGeneralErrorContract {
     interface View : MvpView {
         fun updateText(title: String, message: String)
         fun setViewState(errorState: GeneralErrorScreenError)
-        fun showFile(file: File)
     }
 
-    interface Presenter : MvpPresenter<View> {
-        fun onTermsClick()
-        fun onPolicyClick()
-    }
+    interface Presenter : MvpPresenter<View>
 }
