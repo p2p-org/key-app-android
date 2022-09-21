da4e2905ccac01bb3022d9326f78566816439607
Chingiz <chyngyz.i@p2p.org>
2022-09-21 10:56:19 -0500

    PWN-4817 - ktlint

--- app/src/main/java/org/p2p/wallet/home/analytics/HomeAnalytics.kt
+++ app/src/main/java/org/p2p/wallet/home/analytics/HomeAnalytics.kt
@@ -3,7 +3,6 @@ package org.p2p.wallet.home.analytics
 import org.p2p.wallet.common.analytics.Analytics
 import org.p2p.wallet.common.analytics.constants.EventNames.HOME_USER_AGGREGATE_BALANCE
 import org.p2p.wallet.common.analytics.constants.EventNames.HOME_USER_HAS_POSITIVE_BALANCE
-import java.math.BigDecimal
 
 class HomeAnalytics(private val tracker: Analytics) {
 
