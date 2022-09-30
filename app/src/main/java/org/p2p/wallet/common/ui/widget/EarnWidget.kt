a7c8b2ec1dfa2a316a6daaeebc100b7f3e8ba0fd
Konstantin Abramov <konstantin.abramov@p2p.org>
2022-09-30 15:13:35 +0300

    PWN-5316 - widget created and implemented all states PWN-5318 - amount ticker created and connected to widget

---
+++ app/src/main/java/org/p2p/wallet/common/ui/widget/EarnWidget.kt
@@ -0,0 +1,131 @@
+package org.p2p.wallet.common.ui.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.annotation.ColorRes
+import androidx.annotation.StringRes
+import androidx.core.view.isVisible
+import com.robinhood.ticker.TickerUtils
+import com.robinhood.ticker.TickerView
+import org.p2p.uikit.utils.getColor
+import org.p2p.uikit.utils.toPx
+import org.p2p.wallet.R
+import org.p2p.wallet.databinding.WidgetEardViewBinding
+import org.p2p.wallet.utils.DecimalFormatter
+import org.p2p.wallet.utils.isZero
+import java.math.BigDecimal
+
+private const val DEFAULT_BUTTON_TEXT_PADDING_DP = 20
+private const val SMALL_BUTTON_TEXT_PADDING_DP = 16
+
+private const val TICKER_DECIMALS = 12
+
+class EarnWidget @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private val binding = WidgetEardViewBinding.inflate(LayoutInflater.from(context), this, true)
+
+    var currentState: EarnWidgetState? = null
+
+    init {
+        binding.tickerViewAmount.apply {
+            setCharacterLists(TickerUtils.provideNumberList())
+            setPreferredScrollingDirection(TickerView.ScrollingDirection.DOWN)
+        }
+    }
+
+    fun setOnButtonClickListener(buttonClickListener: OnClickListener) {
+        binding.buttonEarn.setOnClickListener(buttonClickListener)
+    }
+
+    fun setWidgetState(state: EarnWidgetState) = with(binding) {
+        currentState = state
+        viewEarnContent.setBackgroundColor(getColor(state.backgroundColor))
+        val balanceState = state is EarnWidgetState.Balance
+        val depositingState = state is EarnWidgetState.Depositing
+        textViewEarnTitle.isVisible = !depositingState
+        tickerViewAmount.isVisible = balanceState
+        textViewEarnMessage.isVisible = !balanceState
+        buttonEarn.isEnabled = !depositingState
+        when (state) {
+            is EarnWidgetState.Balance -> {
+                makeAlignStartContent()
+                tickerViewAmount.text = "$${state.amount.formatTicker()}"
+                textViewEarnTitle.setText(R.string.earn_widget_balance_title)
+                buttonEarn.setText(R.string.earn_widget_balance_button)
+            }
+            is EarnWidgetState.LearnMore -> {
+                makeAlignCenterContent()
+                textViewEarnTitle.setText(R.string.earn_widget_learn_more_title)
+                textViewEarnMessage.setText(R.string.earn_widget_learn_more_message)
+                buttonEarn.setText(R.string.earn_widget_learn_more_button)
+            }
+            is EarnWidgetState.Depositing -> {
+                makeAlignStartContent()
+                tickerViewAmount.isVisible = false
+                textViewEarnTitle.isVisible = false
+                textViewEarnMessage.isVisible = false
+                buttonEarn.apply {
+                    isEnabled = true // false
+                    setText(state.buttonTextRes)
+                }
+            }
+            is EarnWidgetState.DepositFoundsFailed -> {
+                makeAlignCenterContent()
+                textViewEarnTitle.setText(R.string.earn_widget_deposit_failed_title)
+                textViewEarnMessage.setText(R.string.earn_widget_deposit_failed_message)
+                buttonEarn.setText(R.string.earn_widget_deposit_failed_button)
+            }
+            is EarnWidgetState.Error -> {
+                makeAlignCenterContent()
+                textViewEarnTitle.setText(R.string.earn_widget_error_title)
+                textViewEarnMessage.setText(state.messageTextRes)
+                buttonEarn.setText(state.buttonTextRes)
+            }
+        }
+    }
+
+    private fun makeAlignStartContent() = with(binding) {
+        textViewEarnTitle.apply {
+            textAlignment = TEXT_ALIGNMENT_TEXT_START
+            setTextAppearance(R.style.UiKit_TextAppearance_Regular_Text3)
+        }
+        val horizontalPadding = SMALL_BUTTON_TEXT_PADDING_DP.toPx()
+        buttonEarn.apply {
+            textAlignment = TEXT_ALIGNMENT_TEXT_START
+            setPadding(horizontalPadding, 0, horizontalPadding, 0)
+        }
+    }
+
+    private fun makeAlignCenterContent() = with(binding) {
+        textViewEarnTitle.apply {
+            textAlignment = TEXT_ALIGNMENT_CENTER
+            setTextAppearance(R.style.UiKit_TextAppearance_SemiBold_Text1)
+        }
+        val horizontalPadding = DEFAULT_BUTTON_TEXT_PADDING_DP.toPx()
+        buttonEarn.apply {
+            textAlignment = TEXT_ALIGNMENT_CENTER
+            setPadding(horizontalPadding, 0, horizontalPadding, 0)
+        }
+    }
+}
+
+fun BigDecimal.formatTicker(): String = this.run {
+    if (isZero()) this.toString() else DecimalFormatter.format(this, TICKER_DECIMALS)
+}
+
+sealed class EarnWidgetState(@ColorRes val backgroundColor: Int = R.color.bg_rain) {
+    object LearnMore : EarnWidgetState(backgroundColor = R.color.bg_lime)
+    data class Depositing(@StringRes val buttonTextRes: Int) : EarnWidgetState()
+    object DepositFoundsFailed : EarnWidgetState()
+    data class Balance(val amount: BigDecimal) : EarnWidgetState()
+    data class Error(
+        @StringRes val messageTextRes: Int,
+        @StringRes val buttonTextRes: Int
+    ) : EarnWidgetState()
+}
--- app/src/main/java/org/p2p/wallet/solend/ui/SolendEarnFragment.kt
+++ app/src/main/java/org/p2p/wallet/solend/ui/SolendEarnFragment.kt
@@ -2,11 +2,20 @@ package org.p2p.wallet.solend.ui
 
 import android.os.Bundle
 import android.view.View
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import org.p2p.wallet.R
 import org.p2p.wallet.common.mvp.BaseMvpFragment
+import org.p2p.wallet.common.ui.widget.EarnWidgetState
 import org.p2p.wallet.databinding.FragmentSolendEarnBinding
 import org.p2p.wallet.utils.viewbinding.viewBinding
+import java.math.BigDecimal
 
 class SolendEarnFragment :
     BaseMvpFragment<SolendEarnContract.View, SolendEarnContract.Presenter>(R.layout.fragment_solend_earn),
@@ -20,10 +29,64 @@ class SolendEarnFragment :
 
     private val binding: FragmentSolendEarnBinding by viewBinding()
 
+    private var timerJob: Job? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         with(binding) {
+            // Update after real integration
+            viewEarnWidget.setWidgetState(EarnWidgetState.LearnMore)
+            viewEarnWidget.setOnButtonClickListener {
+                timerJob?.cancel()
+                when (val state = viewEarnWidget.currentState) {
+                    is EarnWidgetState.Balance -> {
+                        viewEarnWidget.setWidgetState(
+                            EarnWidgetState.Depositing(R.string.earn_widget_deposit_withdrawing)
+                        )
+                    }
+                    is EarnWidgetState.Depositing -> {
+                        if (state.buttonTextRes == R.string.earn_widget_deposit_withdrawing) {
+                            viewEarnWidget.setWidgetState(EarnWidgetState.DepositFoundsFailed)
+                        } else {
+                            val timer = (1..Int.MAX_VALUE)
+                                .asSequence()
+                                .asFlow()
+                                .onEach { delay(1_000) }
 
+                            val job = Job()
+                            val uiScope = CoroutineScope(Dispatchers.Main + job)
+                            timerJob = uiScope.launch() {
+                                var balance = 89.5708912487
+                                timer.collect {
+                                    balance += (it.toFloat() / 100000000000) // + Random.nextInt(1, 9)
+                                    viewEarnWidget.setWidgetState(EarnWidgetState.Balance(BigDecimal(balance)))
+                                }
+                            }
+                        }
+                    }
+                    EarnWidgetState.LearnMore -> {
+                        viewEarnWidget.setWidgetState(
+                            EarnWidgetState.Depositing(R.string.earn_widget_deposit_sending)
+                        )
+                    }
+                    EarnWidgetState.DepositFoundsFailed -> {
+                        viewEarnWidget.setWidgetState(
+                            EarnWidgetState.Error(
+                                R.string.earn_widget_error_message_withdrawal,
+                                R.string.earn_widget_error_button_ok
+                            )
+                        )
+                    }
+                    is EarnWidgetState.Error -> {
+                        viewEarnWidget.setWidgetState(EarnWidgetState.LearnMore)
+                    }
+                }
+            }
         }
+    }
+
+    override fun onDestroyView() {
+        timerJob?.cancel()
+        super.onDestroyView()
     }
 }
