71fafa50981daf4cc7859a5c18da654b04744b95
Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>
2022-10-04 09:57:07 +0300

    PWN-5032 - Solend. Add feature toggle (#817)
    * PWN-5032 - Solend. add feature toggle
    * PWN-5032 - Solend. review fixes
    
    Co-authored-by: kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>

--- app/src/main/java/org/p2p/wallet/solend/SolendModule.kt
+++ app/src/main/java/org/p2p/wallet/solend/SolendModule.kt
@@ -1,9 +1,14 @@
 package org.p2p.wallet.solend
 
+import org.koin.core.module.Module
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import org.p2p.wallet.common.di.InjectionModule
+import org.p2p.wallet.solend.data.SolendConfigurationLocalRepository
+import org.p2p.wallet.solend.data.SolendConfigurationRepository
+import org.p2p.wallet.solend.data.mapper.SolendConfigurationRepositoryMapper
 import org.p2p.wallet.solend.ui.earn.SolendEarnContract
 import org.p2p.wallet.solend.ui.earn.SolendEarnPresenter
 
@@ -11,5 +16,12 @@ object SolendModule : InjectionModule {
 
     override fun create() = module {
         factoryOf(::SolendEarnPresenter) bind SolendEarnContract.Presenter::class
+
+        initDataLayer()
+    }
+
+    private fun Module.initDataLayer() {
+        factoryOf(::SolendConfigurationRepositoryMapper)
+        singleOf(::SolendConfigurationLocalRepository) bind SolendConfigurationRepository::class
     }
 }
