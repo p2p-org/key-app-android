f43162c7047c193f83503ebd5d28244fd43d54cf
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2022-10-07 10:23:16 +0400

    NO-TICKET - remove device share encoding

--- app/src/main/java/org/p2p/wallet/infrastructure/account/AccountStorage.kt
+++ app/src/main/java/org/p2p/wallet/infrastructure/account/AccountStorage.kt
@@ -1,15 +1,13 @@
 package org.p2p.wallet.infrastructure.account
 
-import androidx.core.content.edit
 import android.content.Context
+import androidx.core.content.edit
 import com.google.gson.Gson
-import org.p2p.wallet.common.crypto.keystore.KeyStoreWrapper
 import org.p2p.wallet.infrastructure.account.AccountStorageContract.Key
 import kotlin.reflect.KClass
 
 class AccountStorage(
     context: Context,
-    private val keyStoreWrapper: KeyStoreWrapper,
     private val gson: Gson
 ) : AccountStorageContract {
 
@@ -18,38 +16,30 @@ class AccountStorage(
 
     override fun <T> saveObject(key: Key, data: T) {
         val objectAsJson = gson.toJson(data)
-        val encodedData = keyStoreWrapper.encode(key.prefsValue, objectAsJson)
-        sharedPreferences.edit { putString(key.prefsValue, encodedData) }
+        sharedPreferences.edit { putString(key.prefsValue, objectAsJson) }
     }
 
     @Throws(IllegalArgumentException::class)
     override fun <T : Any> getObject(key: Key, type: KClass<T>): T? {
         return sharedPreferences.getString(key.prefsValue, null)
-            ?.let { keyStoreWrapper.decode(key.prefsValue, it) }
             ?.let { gson.fromJson(it, type.java) }
     }
 
     override fun saveString(key: Key, data: String) {
-        val encodedData = keyStoreWrapper.encode(key.prefsValue, data)
-        sharedPreferences.edit { putString(key.prefsValue, encodedData) }
+        sharedPreferences.edit { putString(key.prefsValue, data) }
     }
 
     override fun getString(key: Key): String? {
-        val encodedData = sharedPreferences.getString(key.prefsValue, null)
-        return encodedData?.let { keyStoreWrapper.decode(key.prefsValue, it) }
+        return sharedPreferences.getString(key.prefsValue, null)
     }
 
     override fun contains(key: Key): Boolean = sharedPreferences.contains(key.prefsValue)
 
     override fun remove(key: Key) {
         sharedPreferences.edit { remove(key.prefsValue) }
-        keyStoreWrapper.delete(key.prefsValue)
     }
 
     override fun removeAll() {
-        sharedPreferences.all.forEach { (key, _) ->
-            keyStoreWrapper.delete(key)
-        }
-        sharedPreferences.edit().clear()
+        sharedPreferences.edit { clear() }
     }
 }
