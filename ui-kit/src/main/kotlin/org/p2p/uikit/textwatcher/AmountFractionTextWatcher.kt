e55d5ba2e65399eac5d07487e54c0a896bf89abb
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2022-10-28 19:07:59 +0300

    PWN-5103 - validation fix (#928)

--- ui-kit/src/main/kotlin/org/p2p/uikit/textwatcher/AmountFractionTextWatcher.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/textwatcher/AmountFractionTextWatcher.kt
@@ -1,14 +1,11 @@
-package org.p2p.wallet.common.ui.textwatcher
+package org.p2p.uikit.textwatcher
 
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
-import org.p2p.wallet.R
-import org.p2p.wallet.utils.Constants.SYMBOL_ZERO
-import org.p2p.wallet.utils.DecimalFormatter
-import org.p2p.wallet.utils.NoOp
-import org.p2p.wallet.utils.emptyString
-import org.p2p.wallet.utils.orZero
+import org.p2p.uikit.R
+import org.p2p.uikit.utils.emptyString
+import org.p2p.uikit.utils.orZero
 import java.lang.ref.WeakReference
 import kotlin.properties.Delegates
 
@@ -17,16 +14,15 @@ import kotlin.properties.Delegates
  * We prohibit entering the value after the dot if it's exceeded the max allowed length
  * For example: 10000.1234456789123123 -> 10 000.123456789
  * */
-
-private const val SYMBOL_DOT = "."
-private const val MAX_INT_LENGTH = 12
 private const val MAX_FRACTION_LENGTH = 9
 
 class AmountFractionTextWatcher(
     editText: EditText,
-    private val maxLengthAllowed: Int,
+    maxLengthAllowed: Int,
     private val onValueChanged: (String) -> Unit
 ) : TextWatcher {
+
+    private val amountFractionFormatter = AmountFractionFormatter(maxLengthAllowed)
 
     companion object {
         fun installOn(
@@ -54,7 +50,7 @@ class AmountFractionTextWatcher(
 
     private var cursorPosition: Int = 0
 
-    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = NoOp
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
 
     override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
         val value = s.toString()
@@ -64,17 +60,7 @@ class AmountFractionTextWatcher(
         // Move cursor one char left if user tries to remove space
         if (spaceWasRemoved) symbolsAfterCursor++
 
-        valueText = when {
-            value.isEmpty() -> value
-            // Remove whole string if dot before zero was removed
-            value == SYMBOL_ZERO && before == 1 -> emptyString()
-            value == "$SYMBOL_ZERO$SYMBOL_ZERO" && start == 1 -> SYMBOL_ZERO
-            value.startsWith(SYMBOL_DOT) -> "$SYMBOL_ZERO$value"
-            value.endsWith(SYMBOL_DOT) -> handleEndWithDotCase(value)
-            value.contains(SYMBOL_DOT) -> handleValueWithDotCase(value)
-            else -> handleGeneralCase(value)
-        }
-
+        valueText = amountFractionFormatter.formatAmountFraction(value, before, start)
         // Keep cursor in same place from the end
         cursorPosition = if (valueText.length < symbolsAfterCursor) 0 else valueText.length - symbolsAfterCursor
     }
@@ -90,34 +76,7 @@ class AmountFractionTextWatcher(
         }
     }
 
-    private fun handleEndWithDotCase(value: String): String = value.dropLast(1)
-        .dropSpaces()
-        .formatDecimal(maxLengthAllowed) + SYMBOL_DOT
-
-    private fun handleGeneralCase(value: String): String = value.dropSpaces()
-        .take(MAX_INT_LENGTH)
-        .formatDecimal(maxLengthAllowed)
-
-    private fun handleValueWithDotCase(value: String): String {
-        val dotPosition = value.indexOf(SYMBOL_DOT)
-        val intPart = value.substring(0, dotPosition)
-            .dropSpaces()
-            .take(MAX_INT_LENGTH)
-
-        // Remove extra dots and shorten fractional part to maxLengthAllowed symbols
-        val fractionalPart = value.substring(dotPosition + 1)
-            .dropSpaces()
-            .replace(SYMBOL_DOT, emptyString())
-            .take(maxLengthAllowed)
-
-        return intPart.formatDecimal(maxLengthAllowed) + SYMBOL_DOT + fractionalPart
-    }
-
     private fun String.dropSpaces(): String {
         return replace(" ", emptyString())
-    }
-
-    private fun String.formatDecimal(fractionLength: Int): String {
-        return DecimalFormatter.format(this.toBigDecimal(), fractionLength)
     }
 }
