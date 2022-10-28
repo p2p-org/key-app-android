202a6c1f311f275c36474902851c4ed673b97751
davran1001 <87668832+davran1001@users.noreply.github.com>
2022-10-28 19:08:17 +0600

    NO-TICKET - Fix all transaction history bugs (#925)
    * PWN-5732 - Change copy address logic (#919)
    
    * increment version for release 1.14.0 -> 1.15.0
    
    * subVerifiers added for better checking
    
    * use subVerifier for prod store
    
    * NO-TICKET - fix swapping test and release urls
    
    * PWN-5732 - change home screen copy address logic
    
    Co-authored-by: kabramovp2p <konstantin.abramov@p2p.org>
    
    * PWN-5729- Restore username on app create (#918)
    
    * increment version for release 1.14.0 -> 1.15.0
    
    * subVerifiers added for better checking
    
    * use subVerifier for prod store
    
    * NO-TICKET - fix swapping test and release urls
    
    * PWN-5729 - restore username on each app laucnh
    
    * PWN-5729 - revert val -> var for openedFromSource
    
    * PWN-5729 - remove legacy on restore username
    
    Co-authored-by: kabramovp2p <konstantin.abramov@p2p.org>
    
    * NO-TICKET - fix history parsing, fix error with skipping unknown transactions, also fix bug with displaying swap with same source and destionation addresses
    
    * NO-TICKET - fix dispaying error transactions
    
    * PWN-5710 - Fix amount on new transactions
    
    Co-authored-by: Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>
    Co-authored-by: kabramovp2p <konstantin.abramov@p2p.org>
    Co-authored-by: khasanov.davran <khasanov.davran@p2p.org>

--- app/src/main/java/org/p2p/wallet/history/repository/remote/TransactionDetailsRpcRepository.kt
+++ app/src/main/java/org/p2p/wallet/history/repository/remote/TransactionDetailsRpcRepository.kt
@@ -8,6 +8,7 @@ import org.p2p.wallet.history.strategy.ParsingResult
 import org.p2p.wallet.history.strategy.TransactionParsingContext
 import org.p2p.wallet.rpc.RpcConstants
 import org.p2p.wallet.rpc.api.RpcHistoryApi
+import timber.log.Timber
 
 class TransactionDetailsRpcRepository(
     private val rpcApi: RpcHistoryApi,
@@ -18,6 +19,7 @@ class TransactionDetailsRpcRepository(
         userPublicKey: String,
         transactionSignatures: List<HistoryStreamItem>
     ): List<TransactionDetails> {
+
         val encoding = buildMap {
             this[RpcConstants.REQUEST_PARAMETER_KEY_ENCODING] =
                 RpcConstants.REQUEST_PARAMETER_VALUE_JSON_PARSED
@@ -25,18 +27,27 @@ class TransactionDetailsRpcRepository(
                 RpcConstants.REQUEST_PARAMETER_VALUE_CONFIRMED
         }
         val requestsBatch = transactionSignatures.map { streamItem ->
-            val signature = streamItem.streamSource?.signature ?: return emptyList()
+
+            val signature = streamItem.streamSource
+                ?.signature
+                ?: return emptyList()
+
             val params = listOf(signature, encoding)
-            RpcRequest(method = RpcConstants.REQUEST_METHOD_VALUE_GET_CONFIRMED_TRANSACTIONS, params = params)
+
+            RpcRequest(
+                method = RpcConstants.REQUEST_METHOD_VALUE_GET_CONFIRMED_TRANSACTIONS,
+                params = params
+            )
         }
 
-        val transactions = rpcApi.getConfirmedTransactions(requestsBatch).map { it.result }
+        val transactions = rpcApi.getConfirmedTransactions(requestsBatch)
+            .map { it.result }
 
         return fromNetworkToDomain(transactions).onEach { transactionDetails ->
             val signatureItem =
-                transactionSignatures.first { it.streamSource?.signature == transactionDetails.signature }
-            transactionDetails.status = signatureItem.streamSource?.status
-            transactionDetails.account = signatureItem.account
+                transactionSignatures.firstOrNull { it.streamSource?.signature == transactionDetails.signature }
+            transactionDetails.status = signatureItem?.streamSource?.status
+            transactionDetails.account = signatureItem?.account
         }
     }
 
@@ -45,15 +56,25 @@ class TransactionDetailsRpcRepository(
     ): List<TransactionDetails> {
         val transactionDetails = mutableListOf<TransactionDetails>()
         transactions.forEach { transaction ->
-
-            transactionDetails.forEach {
-                it.error = transaction.meta.error?.instructionError
-            }
-            val parsingResult = transactionParsingContext.parseTransaction(transaction)
-            if (parsingResult is ParsingResult.Transaction) {
-                transactionDetails.addAll(parsingResult.details)
+            when (val parsingResult = transactionParsingContext.parseTransaction(transaction)) {
+                is ParsingResult.Transaction -> {
+                    transactionDetails.addAll(parsingResult.details)
+                }
+                is ParsingResult.Error -> {
+                    Timber.i("Error on parsing transaction ${parsingResult.error.message}")
+                }
             }
         }
-        return transactionDetails
+        val result = transactionDetails.distinctBy { it.signature }
+        val errorTransactions = transactions.filter { it.meta?.error != null }
+        errorTransactions.forEach { root ->
+            val foundTransaction = result.firstOrNull { it.signature in root.transaction?.signatures.orEmpty() }
+            val indexOfTransaction = result.indexOf(foundTransaction)
+            if (indexOfTransaction != -1) {
+                result.getOrNull(indexOfTransaction)
+                    ?.let { it.error = root.meta?.error }
+            }
+        }
+        return result
     }
 }
