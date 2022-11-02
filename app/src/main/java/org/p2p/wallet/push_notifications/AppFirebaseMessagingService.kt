165a4578c6323dc28f08c646a9e624a70694b0f8
Chingiz <chyngyz.i@p2p.org>
2022-11-02 18:30:16 +0600

    PWN-5766 - Changed theme colors according to our new design (#936)
    * PWN-5766 - Changed theme colors according to our new design
    
    * PWN-5766 - ColorAccent is black temporary
    
    * PWN-5766 - ColorAccent is black temporary, minor color changes
    
    * PWN-5766 - Comment fixes
    
    * PWN-5796 - Fixes for username found state
    
    Co-authored-by: Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>

--- app/src/main/java/org/p2p/wallet/push_notifications/AppFirebaseMessagingService.kt
+++ app/src/main/java/org/p2p/wallet/push_notifications/AppFirebaseMessagingService.kt
@@ -4,7 +4,7 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.p2p.wallet.appsfly.AppsFlyerService
+import org.p2p.wallet.appsflyer.AppsFlyerService
 import org.p2p.wallet.deeplinks.AppDeeplinksManager
 import org.p2p.wallet.notification.AppNotificationManager
 import org.p2p.wallet.notification.FcmPushNotificationData
