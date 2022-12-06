23a8b04bed5b2edd38e33746f89dd9df78cc023d
kabramovp2p <konstantin.abramov@p2p.org>
2022-12-06 16:50:30 +0200

    PWN-6238 - fix layout and make some preparations

--- app/src/main/java/org/p2p/wallet/newsend/NewSendFragment.kt
+++ app/src/main/java/org/p2p/wallet/newsend/NewSendFragment.kt
@@ -3,10 +3,13 @@ package org.p2p.wallet.newsend
 import android.os.Bundle
 import android.view.View
 import org.koin.android.ext.android.inject
+import org.p2p.uikit.glide.GlideManager
 import org.p2p.wallet.R
 import org.p2p.wallet.common.mvp.BaseMvpFragment
 import org.p2p.wallet.databinding.FragmentSendNewBinding
+import org.p2p.wallet.home.model.Token
 import org.p2p.wallet.utils.Base58String
+import org.p2p.wallet.utils.Constants
 import org.p2p.wallet.utils.args
 import org.p2p.wallet.utils.viewbinding.viewBinding
 import org.p2p.wallet.utils.withArgs
@@ -33,10 +36,29 @@ class NewSendFragment :
     private val binding: FragmentSendNewBinding by viewBinding()
 
     override val presenter: NewSendContract.Presenter by inject()
+    private val glideManager: GlideManager by inject()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbar.title = recipientUsername ?: recipientAddress
-        binding.sliderSend.setActionText("Enter Amount")
+        // TODO PWN-6090 make logic of setting token!
+        binding.widgetSendDetails.apply {
+            setSendLabel(getString(R.string.send_label))
+            setSwitchLabel(
+                getString(R.string.send_switch_to_token, Constants.SOL_SYMBOL)
+            )
+        }
+        // TODO PWN-6090 make button
+        binding.sliderSend.setActionText(R.string.send_enter_amount)
+    }
+
+    // TODO PWN-6090 connect this method to token selection
+    fun showSourceToken(token: Token.Active) {
+        with(binding.widgetSendDetails) {
+            glideManager.load(imageViewTokenIcon, token.iconUrl)
+            textViewTokenName.text = token.tokenSymbol
+            textViewTokenTotal.text = token.getFormattedTotal(includeSymbol = true)
+            textViewTokenAmountInUsd.text = token.getFormattedUsdTotal()
+        }
     }
 }
--- ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitSendDetailsWidget.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitSendDetailsWidget.kt
@@ -3,7 +3,8 @@ package org.p2p.uikit.components
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
-import org.p2p.uikit.databinding.WidgetSendDetailsBinding
+import org.p2p.uikit.databinding.WidgetSendDetailsInputBinding
+import org.p2p.uikit.textwatcher.AmountFractionTextWatcher
 import org.p2p.uikit.utils.inflateViewBinding
 
 class UiKitSendDetailsWidget @JvmOverloads constructor(
@@ -11,5 +12,53 @@ class UiKitSendDetailsWidget @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
-    private val binding: WidgetSendDetailsBinding = inflateViewBinding()
+
+    private val binding: WidgetSendDetailsInputBinding = inflateViewBinding()
+
+    var amountListener: ((input: String) -> Unit)? = null
+    val switchListener: (() -> Unit)? = null
+    val tokenClickListener: (() -> Unit)? = null
+
+    val imageViewTokenIcon
+        get() = binding.imageViewTokenIcon
+
+    val textViewTokenName
+        get() = binding.textViewTokenName
+
+    val textViewTokenTotal
+        get() = binding.textViewTokenTotal
+
+    val textViewTokenAmountInUsd
+        get() = binding.textViewTokenAmountInUsd
+
+    init {
+        with(binding) {
+            containerToken.setOnClickListener {
+                tokenClickListener?.invoke()
+            }
+            imageViewSwitchTo.setOnClickListener {
+                switchListener?.invoke()
+            }
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        AmountFractionTextWatcher.installOn(binding.editTextAmount) {
+            amountListener?.invoke(it)
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        AmountFractionTextWatcher.uninstallFrom(binding.editTextAmount)
+        super.onDetachedFromWindow()
+    }
+
+    fun setSwitchLabel(text: String) {
+        binding.textViewTokenSwitchLabel.text = text
+    }
+
+    fun setSendLabel(text: String) {
+        binding.textViewSendLabel.text = text
+    }
 }
