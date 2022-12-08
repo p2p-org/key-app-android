9ceabed0145abfd7e2193da032e6fa4de3769ee0
kabramovp2p <konstantin.abramov@p2p.org>
2022-12-08 09:59:26 +0200

    PWN-6238 - set token in UiKitSendDetailsWidget.kt

--- ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitSendDetailsWidget.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitSendDetailsWidget.kt
@@ -1,9 +1,13 @@
 package org.p2p.uikit.components
 
-import androidx.constraintlayout.widget.ConstraintLayout
 import android.content.Context
 import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.p2p.core.glide.GlideManager
 import org.p2p.core.textwatcher.AmountFractionTextWatcher
+import org.p2p.core.token.Token
 import org.p2p.uikit.databinding.WidgetSendDetailsInputBinding
 import org.p2p.uikit.utils.inflateViewBinding
 
@@ -11,25 +15,15 @@ class UiKitSendDetailsWidget @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr), KoinComponent {
 
     private val binding: WidgetSendDetailsInputBinding = inflateViewBinding()
+
+    private val glideManager: GlideManager by inject()
 
     var amountListener: ((input: String) -> Unit)? = null
     var switchListener: (() -> Unit)? = null
     var tokenClickListener: (() -> Unit)? = null
-
-    val imageViewTokenIcon
-        get() = binding.imageViewTokenIcon
-
-    val textViewTokenName
-        get() = binding.textViewTokenName
-
-    val textViewTokenTotal
-        get() = binding.textViewTokenTotal
-
-    val textViewTokenAmountInUsd
-        get() = binding.textViewTokenAmountInUsd
 
     init {
         with(binding) {
@@ -52,6 +46,15 @@ class UiKitSendDetailsWidget @JvmOverloads constructor(
     override fun onDetachedFromWindow() {
         AmountFractionTextWatcher.uninstallFrom(binding.editTextAmount)
         super.onDetachedFromWindow()
+    }
+
+    fun setToken(token: Token.Active) {
+        with(binding) {
+            glideManager.load(imageViewTokenIcon, token.iconUrl)
+            textViewTokenName.text = token.tokenSymbol
+            textViewTokenTotal.text = token.getFormattedTotal(includeSymbol = true)
+            textViewTokenAmountInUsd.text = token.getFormattedUsdTotal()
+        }
     }
 
     fun setSwitchLabel(text: String) {
