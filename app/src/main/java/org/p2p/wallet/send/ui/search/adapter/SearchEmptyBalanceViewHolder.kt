5f7652d87d66f2bddee0d1ffdc36dd1bed261517
kabramovp2p <konstantin.abramov@p2p.org>
2022-12-20 16:51:26 +0200

    PWN-6391 - check own address and own token direct address

--- app/src/main/java/org/p2p/wallet/send/ui/search/adapter/SearchEmptyBalanceViewHolder.kt
+++ app/src/main/java/org/p2p/wallet/send/ui/search/adapter/SearchEmptyBalanceViewHolder.kt
@@ -4,7 +4,8 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.p2p.wallet.databinding.ItemSearchEmptyBalanceBinding
 import org.p2p.wallet.send.model.SearchResult
-import org.p2p.wallet.utils.cutEnd
+import org.p2p.wallet.utils.CUT_SEVEN_SYMBOLS
+import org.p2p.wallet.utils.cutMiddle
 import org.p2p.wallet.utils.viewbinding.inflateViewBinding
 
 class SearchEmptyBalanceViewHolder(
@@ -15,7 +16,7 @@ class SearchEmptyBalanceViewHolder(
 
     fun onBind(item: SearchResult.EmptyBalance) {
         with(binding) {
-            textViewAddress.text = item.addressState.address.cutEnd()
+            textViewAddress.text = item.addressState.address.cutMiddle(CUT_SEVEN_SYMBOLS)
         }
 
         itemView.setOnClickListener { onItemClicked(item) }
