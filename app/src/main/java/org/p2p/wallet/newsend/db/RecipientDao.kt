27c7bb864d46095bd7d80c863db80f56bfa33762
kabramovp2p <konstantin.abramov@p2p.org>
2022-12-21 14:11:44 +0200

    PWN-6396 - remove not needed update methods

--- app/src/main/java/org/p2p/wallet/newsend/db/RecipientDao.kt
+++ app/src/main/java/org/p2p/wallet/newsend/db/RecipientDao.kt
@@ -4,45 +4,15 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Transaction
 
 @Dao
 interface RecipientDao {
-
-    @Transaction
-    suspend fun insertOrUpdate(entities: List<RecipientEntry>) {
-        entities.forEach { entity ->
-            val found = findByAddress(entity.address)
-            if (found != null) {
-                update(
-                    entity.address,
-                    entity.nickname,
-                    entity.dateTimestamp
-                )
-            } else {
-                insertOrReplace(entity)
-            }
-        }
-    }
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrReplace(entities: List<RecipientEntry>)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrReplace(entity: RecipientEntry)
-
-    @Query(
-        """
-            UPDATE recipient_table
-            SET nickname = :nickname, date_timestamp = :dateTimestamp
-            WHERE address = :address 
-        """
-    )
-    suspend fun update(
-        address: String,
-        nickname: String?,
-        dateTimestamp: Long
-    )
 
     @Query("SELECT * FROM recipient_table WHERE address = :address")
     suspend fun findByAddress(address: String): RecipientEntry?
