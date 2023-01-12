c944c89a9e4c3a6f3be33c774dd22e8b723da062
Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>
2023-01-12 17:27:29 +0800

    PWN-6723 - Debug feature-toggles redesign and save (#1093)
    * PWN-6723 - change design of debug feature-toggles
    
    * PWN-6723 - add saving debug feature toggles
    
    * PWN-6723 - refactor and simplify

--- app/src/main/java/org/p2p/wallet/common/feature_toggles/remote_config/LocalFirebaseRemoteConfig.kt
+++ app/src/main/java/org/p2p/wallet/common/feature_toggles/remote_config/LocalFirebaseRemoteConfig.kt
@@ -1,16 +1,22 @@
 package org.p2p.wallet.common.feature_toggles.remote_config
 
-class LocalFirebaseRemoteConfig(appFirebaseRemoteConfig: AppFirebaseRemoteConfig) : RemoteConfigValuesProvider {
+class LocalFirebaseRemoteConfig(
+    appFirebaseRemoteConfig: AppFirebaseRemoteConfig,
+    private val storage: LocalFeatureToggleStorage
+) : RemoteConfigValuesProvider {
 
-    private val toggleKeyToValue: MutableMap<String, String> =
-        appFirebaseRemoteConfig.allFeatureTogglesRaw().toMutableMap()
-
-    fun changeFeatureToggle(toggleKey: String, newValue: String) {
-        toggleKeyToValue[toggleKey] = newValue
+    init {
+        appFirebaseRemoteConfig.allFeatureTogglesRaw()
+            .filterKeys { it !in storage } // do not rewrite already written keys
+            .forEach { storage.putFeatureToggle(it.key, it.value) }
     }
 
-    override fun getBoolean(toggleKey: String): Boolean = toggleKeyToValue[toggleKey].toBoolean()
-    override fun getString(toggleKey: String): String? = toggleKeyToValue[toggleKey]
-    override fun getInt(toggleKey: String): Int? = toggleKeyToValue[toggleKey]?.toIntOrNull()
-    override fun getFloat(toggleKey: String): Float? = toggleKeyToValue[toggleKey]?.toFloatOrNull()
+    fun changeFeatureToggle(toggleKey: String, newValue: String) {
+        storage.putFeatureToggle(toggleKey, newValue)
+    }
+
+    override fun getBoolean(toggleKey: String): Boolean = storage[toggleKey].toBoolean()
+    override fun getString(toggleKey: String): String? = storage[toggleKey]
+    override fun getInt(toggleKey: String): Int? = storage[toggleKey]?.toIntOrNull()
+    override fun getFloat(toggleKey: String): Float? = storage[toggleKey]?.toFloatOrNull()
 }
