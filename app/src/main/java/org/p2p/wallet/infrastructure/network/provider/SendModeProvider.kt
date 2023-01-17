0bc8ff08592a76cfef3dea9d027bebec68ceb66e
kabramovp2p <konstantin.abramov@p2p.org>
2023-01-17 09:36:10 +0200

    PWN-6518 - changes according to designs

--- app/src/main/java/org/p2p/wallet/infrastructure/network/provider/SendModeProvider.kt
+++ app/src/main/java/org/p2p/wallet/infrastructure/network/provider/SendModeProvider.kt
@@ -1,19 +1,19 @@
 package org.p2p.wallet.infrastructure.network.provider
 
-import com.google.gson.annotations.SerializedName
+import android.content.SharedPreferences
+import androidx.core.content.edit
 import org.p2p.core.utils.Constants.USD_READABLE_SYMBOL
-import org.p2p.wallet.infrastructure.security.SecureStorageContract
-import org.p2p.wallet.infrastructure.security.SecureStorageContract.Key
 import org.p2p.wallet.send.model.CurrencyMode
 import org.p2p.wallet.utils.emptyString
 import timber.log.Timber
 
 private const val TAG = "SendModeProvider"
+private const val KEY_SEND_MODE = "SEND_MODE"
 
 private val EMPTY_TOKEN = CurrencyMode.Token(emptyString(), 2)
 
 class SendModeProvider(
-    private val secureStorage: SecureStorageContract
+    private val sharedPreferences: SharedPreferences
 ) {
 
     var sendMode: CurrencyMode = CurrencyMode.Usd
@@ -21,33 +21,36 @@ class SendModeProvider(
         set(value) {
             field = value
             saveSendModeToStorage(value)
-            Timber.tag(TAG).i("updating user seed phrase: $value")
         }
 
     private fun getSendModeFromStorage(): CurrencyMode =
         try {
-            val sendMode: SendMode = secureStorage.getObject(Key.KEY_SEND_MODE, SendMode::class)
-                ?: SendMode(USD_READABLE_SYMBOL)
-            if (sendMode.mode == USD_READABLE_SYMBOL) CurrencyMode.Usd
-            else EMPTY_TOKEN
+            val sendMode: String = sharedPreferences.getString(KEY_SEND_MODE, USD_READABLE_SYMBOL)
+                ?: USD_READABLE_SYMBOL
+            if (sendMode == USD_READABLE_SYMBOL) {
+                CurrencyMode.Usd
+            } else {
+                EMPTY_TOKEN
+            }
         } catch (e: Throwable) {
             Timber.tag(TAG).e(e)
             throw e
         }
 
     private fun saveSendModeToStorage(value: CurrencyMode) {
-        secureStorage.saveObject(
-            Key.KEY_SEND_MODE,
-            SendMode(if (value == CurrencyMode.Usd) USD_READABLE_SYMBOL else emptyString())
-        )
+        sharedPreferences.edit {
+            putString(
+                KEY_SEND_MODE,
+                if (value == CurrencyMode.Usd) {
+                    USD_READABLE_SYMBOL
+                } else {
+                    emptyString()
+                }
+            )
+        }
     }
 
     fun clear() {
         sendMode = CurrencyMode.Usd
     }
-
-    data class SendMode(
-        @SerializedName("mode")
-        val mode: String
-    )
 }
