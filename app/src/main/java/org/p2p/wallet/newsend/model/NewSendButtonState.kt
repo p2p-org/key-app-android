0e206a6544ed8a34e4b9ff5981dc93e731089126
kabramovp2p <konstantin.abramov@p2p.org>
2023-01-19 17:15:07 +0200

    PWN-6902 - check on isMinRequiredBalanceLeft in case if !isAmountValidForSender

--- app/src/main/java/org/p2p/wallet/newsend/model/NewSendButtonState.kt
+++ app/src/main/java/org/p2p/wallet/newsend/model/NewSendButtonState.kt
@@ -69,11 +69,7 @@ class NewSendButtonState(
                     val textContainer = TextContainer.Raw(text)
                     State.Disabled(textContainer, R.color.text_rose)
                 }
-                !isMinRequiredBalanceLeft -> {
-                    val textContainer = TextContainer.Res(R.string.error_insufficient_funds)
-                    State.Disabled(textContainer, R.color.text_rose)
-                }
-                !isAmountValidForSender -> {
+                !isAmountValidForSender && isMinRequiredBalanceLeft -> {
                     val maxSolAmountAllowed = sourceToken.totalInLamports - minRentExemption
                     val format = resources.getString(
                         R.string.send_max_warning_text_format,
@@ -84,6 +80,10 @@ class NewSendButtonState(
                         textContainer = TextContainer.Raw(format),
                         totalAmountTextColor = R.color.text_rose
                     )
+                }
+                !isMinRequiredBalanceLeft -> {
+                    val textContainer = TextContainer.Res(R.string.error_insufficient_funds)
+                    State.Disabled(textContainer, R.color.text_rose)
                 }
                 !isEnoughToCoverExpenses -> {
                     val textContainer = TextContainer.Res(R.string.send_insufficient_funds)
