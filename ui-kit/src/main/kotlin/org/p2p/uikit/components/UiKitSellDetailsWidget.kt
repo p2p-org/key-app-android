bff96759e1d45dc5e8e35bf3bb30450a5819e681
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2023-01-19 15:31:28 +0800

    PWN-6868 - Off-ramp. New widget integration

--- ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitSellDetailsWidget.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/components/UiKitSellDetailsWidget.kt
@@ -1,17 +1,33 @@
 package org.p2p.uikit.components
 
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.widget.doAfterTextChanged
 import android.content.Context
 import android.util.AttributeSet
 import org.koin.core.component.KoinComponent
 import org.p2p.core.model.CurrencyMode
+import org.p2p.core.textwatcher.AmountFractionTextWatcher
+import org.p2p.core.utils.MOONPAY_DECIMAL
 import org.p2p.core.utils.formatFiat
-import org.p2p.core.utils.toBigDecimalOrZero
+import org.p2p.core.utils.formatTokenForMoonpay
+import org.p2p.uikit.R
 import org.p2p.uikit.databinding.WidgetSellInputAmountBinding
+import org.p2p.uikit.utils.focusAndShowKeyboard
 import org.p2p.uikit.utils.getString
 import org.p2p.uikit.utils.inflateViewBinding
 import java.math.BigDecimal
+
+data class SellWidgetViewState(
+    val tokenSymbol: String,
+    val fiatName: String,
+    val currencyMode: CurrencyMode,
+    val currencyModeToSwitch: CurrencyMode,
+    val availableTokenAmount: BigDecimal,
+    val fiatEarningAmount: BigDecimal,
+    val feeInFiat: BigDecimal,
+    val feeInToken: BigDecimal,
+    val inputAmount: String,
+    val sellQuoteInFiat: BigDecimal
+)
 
 class UiKitSellDetailsWidget @JvmOverloads constructor(
     context: Context,
@@ -21,71 +37,82 @@ class UiKitSellDetailsWidget @JvmOverloads constructor(
 
     private val binding = inflateViewBinding<WidgetSellInputAmountBinding>()
 
-    var onAmountChanged: (newAmount: BigDecimal) -> Unit = {}
+    var onAmountChanged: (newAmount: String) -> Unit = {}
     var onCurrencyModeSwitchClicked: () -> Unit = {}
     var onMaxAmountButtonClicked: () -> Unit = {}
-
-    class ViewState(
-        val tokenSymbol: String,
-        val fiatName: String,
-        val currencyMode: CurrencyMode,
-        val availableTokenAmount: BigDecimal,
-        val fiatEarningAmount: BigDecimal,
-        val tokenSellAmount: BigDecimal,
-        val feeInFiat: BigDecimal,
-        val feeInToken: BigDecimal,
-        val sellQuoteInFiat: BigDecimal
-    )
+    var onInputFocusChanged: OnFocusChangeListener? = null
+        set(value) {
+            binding.editTextAmount.onFocusChangeListener = value
+            field = value
+        }
 
     init {
         binding.textViewAvailableAmountValue.setOnClickListener { onMaxAmountButtonClicked.invoke() }
         binding.textViewSwitchCurrency.setOnClickListener { onCurrencyModeSwitchClicked.invoke() }
-        binding.editTextAmount.doAfterTextChanged { editable ->
-            onAmountChanged.invoke(parseStringToBigDecimal(editable.toString()))
-        }
+        AmountFractionTextWatcher.installOn(
+            editText = binding.editTextAmount,
+            maxDecimalsAllowed = MOONPAY_DECIMAL,
+            maxIntLength = Int.MAX_VALUE,
+            onValueChanged = { onAmountChanged.invoke(it) }
+        )
     }
 
-    private fun parseStringToBigDecimal(value: String): BigDecimal = value.toBigDecimalOrZero()
-
-    fun render(viewState: ViewState) {
+    fun render(viewState: SellWidgetViewState) {
         with(binding) {
             textViewQuoteValue.text = getString(
-                org.p2p.uikit.R.string.sell_quote_in_fiat,
+                R.string.sell_quote_in_fiat,
                 viewState.tokenSymbol.uppercase(),
                 viewState.sellQuoteInFiat.formatFiat(),
                 viewState.fiatName
             )
 
+            val switchModeSymbol = when (val mode = viewState.currencyModeToSwitch) {
+                is CurrencyMode.Token -> mode.symbol
+                is CurrencyMode.Fiat -> mode.fiatAbbreviation
+            }
+
             textViewSwitchCurrency.text = getString(
-                org.p2p.uikit.R.string.sell_switch_currency_mode_title,
-                viewState.fiatName
+                R.string.sell_switch_currency_mode_title,
+                switchModeSymbol
             )
 
             textViewAvailableAmountValue.text = getString(
-                org.p2p.uikit.R.string.sell_available_tokens,
-                viewState.availableTokenAmount,
+                R.string.sell_available_tokens,
+                viewState.availableTokenAmount.formatTokenForMoonpay(),
                 viewState.tokenSymbol
             )
 
+            val activeModeSymbol = when (val mode = viewState.currencyMode) {
+                is CurrencyMode.Token -> mode.symbol
+                is CurrencyMode.Fiat -> mode.fiatAbbreviation
+            }
+            textViewAmountName.text = activeModeSymbol
+
+            editTextAmount.setText(viewState.inputAmount)
+
             textViewFiatEarningTitle.text = getString(
-                org.p2p.uikit.R.string.sell_fiat_earning_title,
+                R.string.sell_fiat_earning_title,
                 viewState.tokenSymbol,
                 viewState.fiatName
             )
 
             textViewFiatEarningValue.text = getString(
-                org.p2p.uikit.R.string.sell_fiat_earning_value,
+                R.string.sell_fiat_earning_value,
                 viewState.fiatEarningAmount.formatFiat(),
                 viewState.fiatName
             )
 
             textViewFeesInfo.text = getString(
-                org.p2p.uikit.R.string.sell_fees_information,
-                viewState.feeInToken,
+                R.string.sell_fees_information,
+                viewState.feeInToken.formatTokenForMoonpay(),
                 viewState.tokenSymbol,
-                viewState.feeInFiat,
+                viewState.feeInFiat.formatFiat(),
                 viewState.fiatName
             )
         }
+    }
+
+    fun focusInputAndShowKeyboard() {
+        binding.editTextAmount.focusAndShowKeyboard()
     }
 }
