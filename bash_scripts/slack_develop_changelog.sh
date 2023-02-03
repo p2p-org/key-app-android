929a220ec475094b27525404a1f1422f2d67f48f
Chingiz <chyngyz.i@p2p.org>
2023-02-03 15:48:46 +0600

    PWN-6722 - Jira&CI/CD manipulations (#1181)
    * PWN-6722 - Jira manipulations
    * PWN-7977 - Removing Review status update. Fixed RFT status

--- bash_scripts/slack_develop_changelog.sh
+++ bash_scripts/slack_develop_changelog.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 git fetch
-# since we are building the app after the pull request is merged in develop branch
-# we are getting the develop's last commit description
+
 printf "$(git log -1 --pretty=format:'%b' develop)" > changelog.txt
 
 curl \
