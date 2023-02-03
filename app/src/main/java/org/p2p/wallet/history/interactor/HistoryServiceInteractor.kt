e8c79e8cf4998f2f72a69f7729f8e10e827e7464
Davran331122 <khasanov.davran@p2p.org>
2023-02-03 17:16:50 +0600

    PWN-6884 - Fix private signature

--- app/src/main/java/org/p2p/wallet/history/interactor/HistoryServiceInteractor.kt
+++ app/src/main/java/org/p2p/wallet/history/interactor/HistoryServiceInteractor.kt
@@ -29,7 +29,7 @@ class HistoryServiceInteractor(
         BorshSerializable {
 
         override fun serializeSelf(): ByteArray = getBorshBuffer().write(
-            pubkey.toBase58Instance(),
+            pubkey.contentToString(),
             offset,
             limit,
             0
