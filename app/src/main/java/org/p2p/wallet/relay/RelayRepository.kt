6190886d3b254d308904af718163df40f62af339
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2023-02-03 18:57:17 +0800

    PWN-7026 PWN-7024 - Jupiter API. Create and send swap transaction

--- app/src/main/java/org/p2p/wallet/relay/RelayRepository.kt
+++ app/src/main/java/org/p2p/wallet/relay/RelayRepository.kt
@@ -1,5 +1,7 @@
 package org.p2p.wallet.relay
 
+import org.p2p.solanaj.model.types.RecentBlockhash
+
 interface RelayRepository {
-    suspend fun signTransaction(transaction: String, keypair: String, blockhash: String): String
+    suspend fun signTransaction(transaction: String, keypair: String, blockhash: RecentBlockhash): String
 }
--- app/src/main/java/org/p2p/wallet/sdk/facade/model/solend/KeyAppSdkMethodResultException.kt
+++ app/src/main/java/org/p2p/wallet/sdk/facade/model/solend/KeyAppSdkMethodResultException.kt
@@ -2,7 +2,7 @@ package org.p2p.wallet.sdk.facade.model.solend
 
 import com.google.gson.annotations.SerializedName
 
-data class SolendMethodResultException(
+data class KeyAppSdkMethodResultException(
     @SerializedName("error")
     val error: String,
 ) : Throwable(message = "Solend method failed: $error")
