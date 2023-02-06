656a14f2a8c1ece69bd9a8115f5a912cd13ab934
NikolaiEmelianov <121934748+NikolaiEmelianov@users.noreply.github.com>
2023-02-06 17:23:43 +0400

    PWN-7021 - swap quote (#1199)
    * PWN-7023 - Jupiter API. /swap request for transaction
    
    * PWN-7021 - swap route request
    
    * PWN-7021 - review fixes
    
    * PWN-7021 - review fixex
    
    * PWN-7021 review fixes
    
    * PWN-7021 review fixes
    
    * PWN-7021 ktlint fix
    
    ---------
    
    Co-authored-by: Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
    Co-authored-by: Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>

--- app/src/main/java/org/p2p/wallet/swap/jupiter/api/SwapJupiterApi.kt
+++ app/src/main/java/org/p2p/wallet/swap/jupiter/api/SwapJupiterApi.kt
@@ -2,12 +2,25 @@ package org.p2p.wallet.swap.jupiter.api
 
 import org.p2p.wallet.swap.jupiter.api.request.CreateSwapTransactionRequest
 import org.p2p.wallet.swap.jupiter.api.response.CreateSwapTransactionResponse
+import org.p2p.wallet.swap.jupiter.api.response.SwapJupiterQuoteResponse
+import org.p2p.wallet.utils.Base58String
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
+import java.math.BigDecimal
 
 interface SwapJupiterApi {
     @POST("v4/swap")
     suspend fun createRouteSwapTransaction(
         @Body body: CreateSwapTransactionRequest
     ): CreateSwapTransactionResponse
+
+    @GET("v4/quote")
+    suspend fun getSwapRoutes(
+        @Query("inputMint") inputMint: Base58String,
+        @Query("outputMint") outputMint: Base58String,
+        @Query("amount") amount: BigDecimal,
+        @Query("userPublicKey") userPublicKey: Base58String,
+    ): SwapJupiterQuoteResponse
 }
