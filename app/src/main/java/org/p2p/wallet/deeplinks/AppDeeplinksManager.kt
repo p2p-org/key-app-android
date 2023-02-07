c89e88934a64c317f857da049bda5936286bf412
kabramovp2p <konstantin.abramov@p2p.org>
2023-02-07 15:44:24 +0200

    PWN-7063 - keyapp deeplinks prepare

--- app/src/main/java/org/p2p/wallet/deeplinks/AppDeeplinksManager.kt
+++ app/src/main/java/org/p2p/wallet/deeplinks/AppDeeplinksManager.kt
@@ -1,10 +1,10 @@
 package org.p2p.wallet.deeplinks
 
-import androidx.core.content.getSystemService
-import androidx.fragment.app.FragmentManager
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
+import androidx.core.content.getSystemService
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.p2p.uikit.components.ScreenTab
 import org.p2p.wallet.R
@@ -49,11 +49,11 @@ class AppDeeplinksManager(
 
     fun handleDeeplinkIntent(intent: Intent) {
         when {
-            isAppOpenFromBackground(intent) -> {
+            isDeeplinkWithUri(intent) -> {
                 val data = intent.data ?: return
                 intercomDeeplinkManager.handleBackgroundDeeplink(data)
             }
-            isAppOpenFromForeground(intent) -> {
+            isDeeplinkWithExtras(intent) -> {
                 val extras = intent.extras ?: return
                 // additional parsing when app been opened with notification from background
                 if (extras.containsKey(NOTIFICATION_TYPE)) {
@@ -69,9 +69,9 @@ class AppDeeplinksManager(
         }
     }
 
-    private fun isAppOpenFromBackground(intent: Intent): Boolean = intent.data != null
+    private fun isDeeplinkWithUri(intent: Intent): Boolean = intent.data != null
 
-    private fun isAppOpenFromForeground(intent: Intent): Boolean = intent.extras != null
+    private fun isDeeplinkWithExtras(intent: Intent): Boolean = intent.extras != null
 
     private fun handleOrSaveDeeplinkIntent(intent: Intent) {
         intent.extras?.apply {
