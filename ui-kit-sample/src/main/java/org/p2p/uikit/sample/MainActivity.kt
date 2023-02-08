23a7d68468f87e4b29daaa362e87d08437e4ee3a
Nikolai Emelianov <nikolai.emelianov@p2p.org>
2023-02-08 19:22:12 +0400

    PWN-7030 - finance block xml, sample app

---
+++ ui-kit-sample/src/main/java/org/p2p/uikit/sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.p2p.uikit.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
