b3b627640b6e4c8865b6b7db27e56ee22e892657
Chingiz <chyngyz.i@p2p.org>
2023-02-09 17:24:08 +0600

    PWN-6722 - Reverted back to last commit

--- bash_scripts/find_jira_key.sh
+++ bash_scripts/find_jira_key.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-git fetch
 
-pr_title=$(echo "${{ github.event.pull_request.title }}")
+commit_message=$(git log -1 --pretty=%B)
 jira_ticket=$(echo "$commit_message" | grep -Eo "PWN-[0-9]+")
-echo "message $commit_message"
 echo "$jira_ticket"
