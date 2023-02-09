bc2efd93119cb0838b6082ceb634cb50faf0b518
Chingiz <chyngyz.i@p2p.org>
2023-02-09 15:47:30 +0600

    PWN-6722 - Fixed the directory 10

--- bash_scripts/find_jira_key.sh
+++ bash_scripts/find_jira_key.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-commit_message=$(git cherry -v develop "$GITHUB_HEAD_REF")
+commit_message=$(git log -1 --pretty=%B)
 jira_ticket=$(echo "$commit_message" | grep -Eo "PWN-[0-9]+")
 echo "$jira_ticket"
