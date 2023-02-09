c10ab8af94fc9cd04bf29c52082895f4272d1850
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2023-02-09 11:24:40 +0200

    PWN-7063 - login with deeplink added (#1222)
    * PWN-7063 - keyapp deeplinks added

--- app/src/main/java/org/p2p/wallet/auth/ui/onboarding/root/OnboardingRootFragment.kt
+++ app/src/main/java/org/p2p/wallet/auth/ui/onboarding/root/OnboardingRootFragment.kt
@@ -1,10 +1,10 @@
 package org.p2p.wallet.auth.ui.onboarding.root
 
-import androidx.fragment.app.Fragment
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 import org.p2p.wallet.R
