cae7fad8caefd64f9abf35e561001fb716db37b6
Chingiz <chyngyz.i@p2p.org>
2023-02-09 17:14:11 +0600

    PWN-6722 - Changed commit message fetching

--- bash_scripts/find_jira_key.sh
+++ bash_scripts/find_jira_key.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-commit_message=$(git log -1 --pretty=%B)
+commit_message=$(git show -s --format=%B "$GITHUB_HEAD_REF")
 jira_ticket=$(echo "$commit_message" | grep -Eo "PWN-[0-9]+")
 echo "message $commit_message"
 echo "$jira_ticket"
