3e8dbf7ffbeb6b19d6f3264ab8cbc8a0d11e579d
Chingiz <chyngyz.i@p2p.org>
2023-02-09 17:27:09 +0600

    PWN-6722 - Reverted back to last commit 2

--- bash_scripts/find_jira_key.sh
+++ bash_scripts/find_jira_key.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-commit_message=$(git log -1 --pretty=%B)
+# this script should run on develop branch, i.e. when PR is merged
+commit_message=$(git log -1 --pretty=format:'%b' develop)
 jira_ticket=$(echo "$commit_message" | grep -Eo "PWN-[0-9]+")
 echo "$jira_ticket"
