6c159b9c9439eda612a0915c1a1db1bc8f781018
kabramovp2p <konstantin.abramov@p2p.org>
2023-02-14 12:53:51 +0200

    PWN-6888 - remove not needed in sell viewHolder and make it simple

--- app/src/main/java/org/p2p/wallet/history/ui/token/adapter/holders/HistorySellTransactionViewHolder.kt
+++ app/src/main/java/org/p2p/wallet/history/ui/token/adapter/holders/HistorySellTransactionViewHolder.kt
@@ -14,24 +14,18 @@ class HistorySellTransactionViewHolder(
 ) : HistoryTransactionViewHolder(binding.root) {
     fun onBind(item: HistoryItem.MoonpayTransactionItem) = with(binding) {
         root.setOnClickListener { onHistoryClicked.invoke(item) }
-        renderStatusIcon(item)
-        renderTitleAndSubtitle(item)
-        renderAmounts(item)
-    }
-
-    private fun ItemHistoryMoonpayTransactionBinding.renderStatusIcon(item: HistoryItem.MoonpayTransactionItem) {
-        imageViewTransactionStatusIcon.setImageResource(item.statusIconRes)
-        imageViewTransactionStatusIcon.setBackgroundResource(item.statusBackgroundRes)
-        imageViewTransactionStatusIcon.imageTintList = ColorStateList.valueOf(getColor(item.statusIconColor))
-    }
-
-    private fun ItemHistoryMoonpayTransactionBinding.renderTitleAndSubtitle(item: HistoryItem.MoonpayTransactionItem) {
-        layoutTransactionDetails.startAmountView.title = item.titleStatus
-        layoutTransactionDetails.startAmountView.subtitle = item.subtitleReceiver
-    }
-
-    private fun renderAmounts(item: HistoryItem.MoonpayTransactionItem) {
-        binding.layoutTransactionDetails.endAmountView.topValue = item.endTopValue
-        binding.layoutTransactionDetails.endAmountView.bottomValue = null // hide SOL amount
+        imageViewTransactionStatusIcon.apply {
+            setImageResource(item.statusIconRes)
+            setBackgroundResource(item.statusBackgroundRes)
+            imageTintList = ColorStateList.valueOf(getColor(item.statusIconColor))
+        }
+        layoutTransactionDetails.startAmountView.apply {
+            title = item.titleStatus
+            subtitle = item.subtitleReceiver
+        }
+        binding.layoutTransactionDetails.endAmountView.apply {
+            topValue = item.endTopValue
+            bottomValue = null // hide SOL amount
+        }
     }
 }
