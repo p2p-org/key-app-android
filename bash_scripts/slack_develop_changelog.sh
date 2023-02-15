dd3fe76dce90525815605e796f8f1ab0d50d6241
Chingiz <chyngyz.i@p2p.org>
2023-02-15 23:05:33 +0600

    PWN-6722 - Changelog fixes 4

--- bash_scripts/slack_develop_changelog.sh
+++ bash_scripts/slack_develop_changelog.sh
@@ -2,16 +2,24 @@
 
 # Set the branch names
 start_branch=develop
-end_branch=origin/$GITHUB_HEAD_REF
+end_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Fetch the latest changes
 git fetch origin
 
-# Get the range of commits between the two branches
-commits=$(git log --pretty=format:"%h - %s (%b) - %an" ${start_branch}..${end_branch})
+# Check if the remote branch exists
+remote_branch_exists=$(git show-ref --quiet refs/remotes/origin/$end_branch)
 
+# Get the range of commits between the two branches
+if [ $remote_branch_exists ]; then
+  commits=$(git log --pretty=format:"%h - %s (%b) - %an" ${start_branch}..origin/$end_branch)
+else
+  commits=""
+fi
+
+# Check if commits are empty
 if [ -z "$commits" ]; then
-  echo "No commits found, skipping slack message."
+  echo "No commits found, skipping"
 else
   changelog_message="*CHANGELOG*
 $commits"
