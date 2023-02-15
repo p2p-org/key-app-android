e2878ba6be1fb36695679682e03a0c40d6a88031
Chingiz <chyngyz.i@p2p.org>
2023-02-15 18:32:39 +0600

    PWN-6722 - Changelog fixes

--- bash_scripts/slack_develop_changelog.sh
+++ bash_scripts/slack_develop_changelog.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-git fetch
 
-printf "$(git log -1 --pretty=format:'%b' develop)" > changelog.txt
+# Set the branch names
+start_branch=develop
+end_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Fetch the latest changes
+git fetch origin
+
+# Get the range of commits between the two branches
+commits=$(git log --pretty=format:"%h - %s (%b) - %an" ${start_branch}..${end_branch})
+
+changelog_message="*CHANGELOG*
+$commits"
 
 curl \
--F token="$1" \
--F channels="$2" \
--F initial_comment="List of changes included in the build" \
--F file=@changelog.txt \
-https://slack.com/api/files.upload
-
+    -F token="$1" \
+    -F channel="$2"\
+    -F text="$changelog_message" \
+    https://slack.com/api/chat.postMessage
