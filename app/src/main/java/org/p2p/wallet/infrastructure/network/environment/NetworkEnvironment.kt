45b16f48d73d15b035448b08778d319729b33bec
Davran331122 <khasanov.davran@p2p.org>
2023-02-16 16:27:59 +0600

    PWN-6884 - Revert adding push service environment

--- app/src/main/java/org/p2p/wallet/infrastructure/network/environment/NetworkEnvironment.kt
+++ app/src/main/java/org/p2p/wallet/infrastructure/network/environment/NetworkEnvironment.kt
@@ -8,6 +8,5 @@ enum class NetworkEnvironment(val endpoint: String) : Parcelable {
     MAINNET("https://api.mainnet-beta.solana.com"),
     SOLANA("https://solana-api.projectserum.com"),
     RPC_POOL("https://p2p.rpcpool.com"),
-    PUSH_SERVICE(" https://push-service.keyapp.org"),
     DEVNET("https://api.devnet.solana.com");
 }
