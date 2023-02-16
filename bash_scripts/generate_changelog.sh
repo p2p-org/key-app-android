47579b681c306dc69bc661361b333df99d1714cf
Chingiz <chyngyz.i@p2p.org>
2023-02-16 17:03:04 +0600

    PWN-6722 - Changelog fixes 17

--- bash_scripts/generate_changelog.sh
+++ bash_scripts/generate_changelog.sh
@@ -5,7 +5,7 @@ last_commit_hash=$(cat last_commit_hash.txt)
 #last_commit_hash="$(git rev-parse origin/develop)"
 
 # Get the current head commit hash
-current_commit_hash="$(git rev-parse HEAD)"
+current_commit_hash="$(git rev-parse "$GITHUB_HEAD_REF")"
 
 # Run the git log command and save the result in CHANGELOG.txt
 printf "$(git log "$last_commit_hash".."$current_commit_hash" --no-merges --pretty=format:\"%h - %s - %an\")" > changelog.txt
