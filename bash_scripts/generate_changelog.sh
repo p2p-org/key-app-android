e1b95c4a34ced03e6e13ff7883e639aa1c7ebdff
Chingiz <chyngyz.i@p2p.org>
2023-02-16 14:34:43 +0600

    PWN-6722 - Changelog fixes 5

--- bash_scripts/generate_changelog.sh
+++ bash_scripts/generate_changelog.sh
@@ -4,7 +4,7 @@
 develop_commit_hash=$(cat last_commit_hash.txt)
 
 # Get the current head commit hash
-current_commit_hash=$(git rev-parse origin/$GITHUB_HEAD_REF)
+current_commit_hash=$(git rev-parse "$GITHUB_HEAD_REF")
 
 # Run the gradle task with the fromCommit and toCommit properties set
 ./gradlew gitChangelogTask -PfromCommit=$develop_commit_hash -PtoCommit=$current_commit_hash
