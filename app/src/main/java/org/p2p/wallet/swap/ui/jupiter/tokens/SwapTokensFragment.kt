7c0b1ad53eff972439906a64f42f3e453790de14
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2023-02-21 16:05:48 +0800

    PWN-7114 - add SearchView

--- app/src/main/java/org/p2p/wallet/swap/ui/jupiter/tokens/SwapTokensFragment.kt
+++ app/src/main/java/org/p2p/wallet/swap/ui/jupiter/tokens/SwapTokensFragment.kt
@@ -1,9 +1,15 @@
 package org.p2p.wallet.swap.ui.jupiter.tokens
 
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import org.p2p.core.utils.hideKeyboard
 import org.p2p.uikit.model.AnyCellItem
 import org.p2p.uikit.utils.attachAdapter
+import org.p2p.uikit.utils.showSoftKeyboard
 import org.p2p.wallet.R
 import org.p2p.wallet.common.mvp.BaseMvpFragment
 import org.p2p.wallet.databinding.FragmentJupiterSwapTokensBinding
@@ -20,7 +26,8 @@ private const val ARG_CHANGE_TOKEN = "ARG_CHANGE_TOKEN"
 
 class SwapTokensFragment :
     BaseMvpFragment<SwapTokensContract.View, SwapTokensContract.Presenter>(R.layout.fragment_jupiter_swap_tokens),
-    SwapTokensContract.View {
+    SwapTokensContract.View,
+    SearchView.OnQueryTextListener {
 
     companion object {
         fun create(tokenToChange: SwapTokensChangeToken): SwapTokensFragment =
@@ -40,8 +47,38 @@ class SwapTokensFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.toolbar.setNavigationOnClickListener { popBackStack() }
+        inflateSearchMenu(binding.toolbar)
 
         binding.recyclerViewTokens.attachAdapter(adapter)
+    }
+
+    // todo: extract SearchView to UiKitToolbar
+    private fun inflateSearchMenu(toolbar: Toolbar) {
+        val search = toolbar.menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as SearchView
+
+        searchView.apply {
+            onActionViewExpanded()
+            setOnQueryTextListener(this@SwapTokensFragment)
+            showSoftKeyboard()
+        }
+
+        val searchEditText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
+        searchEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                hideKeyboard()
+                return@setOnEditorActionListener true
+            }
+
+            return@setOnEditorActionListener false
+        }
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean = false
+
+    override fun onQueryTextChange(searchText: String): Boolean {
+        presenter.onSearchTokenQueryChanged(searchText)
+        return true
     }
 
     override fun setTokenItems(items: List<AnyCellItem>) {
