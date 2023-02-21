p2p-org/key-app-android

commit 6c0c8c657a04aa069aaaf06536dbde48551a1da7

PWN-7063 - Deeplinks moved to onboarding screen (#1268)
* PWN-7063 - Deeplinks moved to onboarding screen
* PWN-7063 - Added default notification deeplink support
* PWN-7063 - Minor renamings

app/src/main/java/org/p2p/wallet/restore/ui/seedphrase/SeedPhrasePresenter.kt
@@ -6,9 +6,9 @@ import org.p2p.wallet.common.mvp.BasePresenter
 import org.p2p.wallet.infrastructure.network.provider.SeedPhraseProvider
 import org.p2p.wallet.infrastructure.network.provider.SeedPhraseSource
 import org.p2p.wallet.restore.interactor.SeedPhraseInteractor
-import org.p2p.wallet.restore.interactor.SeedPhraseInteractor.SeedPhraseVerifyResult
 import kotlin.properties.Delegates.observable
 import kotlinx.coroutines.launch
+import org.p2p.wallet.restore.model.SeedPhraseVerifyResult
 
 private const val SEED_PHRASE_SIZE_SHORT = 12
 private const val SEED_PHRASE_SIZE_LONG = 24
@@ -56,22 +56,20 @@ class SeedPhrasePresenter(
     override fun verifySeedPhrase() {
         launch {
             when (val result = seedPhraseInteractor.verifySeedPhrase(currentSeedPhrase)) {
-                // TODO simplify code
-                is SeedPhraseVerifyResult.VerifiedSeedPhrase -> {
-                    currentSeedPhrase = result.seedPhraseWord
-                    if (currentSeedPhrase.all(SeedPhraseWord::isValid)) {
-                        seedPhraseProvider.setUserSeedPhrase(
-                            words = currentSeedPhrase.map { it.text },
-                            provider = SeedPhraseSource.MANUAL
-                        )
-                        view?.navigateToDerievableAccounts(currentSeedPhrase)
-                    } else {
-                        // warning: updateSeedPhrase causes keyboard to appear, so add a check
-                        view?.updateSeedPhraseView(currentSeedPhrase)
-                        view?.showUiKitSnackBar(messageResId = R.string.seed_phrase_verify_words_failed)
-                    }
+                is SeedPhraseVerifyResult.Verified -> {
+                    currentSeedPhrase = result.seedPhrase
+                    seedPhraseProvider.setUserSeedPhrase(
+                        words = result.getKeys(),
+                        provider = SeedPhraseSource.MANUAL
+                    )
                 }
-                is SeedPhraseVerifyResult.VerifyByChecksumFailed -> {
+                is SeedPhraseVerifyResult.Invalid -> {
+                    // warning: updateSeedPhrase causes keyboard to appear, so add a check
+                    currentSeedPhrase = result.seedPhraseWord
+                    view?.updateSeedPhraseView(currentSeedPhrase)
+                    view?.showUiKitSnackBar(messageResId = R.string.seed_phrase_verify_words_failed)
+                }
+                is SeedPhraseVerifyResult.VerificationFailed -> {
                     view?.showUiKitSnackBar(messageResId = R.string.seed_phrase_verify_checksum_failed)
                 }
             }

app/src/main/java/org/p2p/wallet/auth/ui/onboarding/NewOnboardingFragment.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.View
 import org.koin.android.ext.android.inject
+import java.io.File
 import org.p2p.core.utils.insets.doOnApplyWindowInsets
 import org.p2p.core.utils.insets.systemAndIme
 import org.p2p.uikit.natives.UiKitSnackbarStyle
@@ -35,7 +36,6 @@ import org.p2p.wallet.utils.openFile
 import org.p2p.wallet.utils.popBackStack
 import org.p2p.wallet.utils.replaceFragment
 import org.p2p.wallet.utils.viewbinding.viewBinding
-import java.io.File
 
 class NewOnboardingFragment :
     BaseMvpFragment<NewOnboardingContract.View, NewOnboardingContract.Presenter>(R.layout.fragment_new_onboarding),

app/src/main/java/org/p2p/wallet/auth/ui/onboarding/root/OnboardingRootContract.kt
@@ -1,5 +1,6 @@
 package org.p2p.wallet.auth.ui.onboarding.root
 
+import android.net.Uri
 import org.p2p.wallet.common.mvp.MvpPresenter
 import org.p2p.wallet.common.mvp.MvpView
 
@@ -9,9 +10,11 @@ interface OnboardingRootContract {
         fun navigateToContinueOnboarding()
         fun navigateToRestore()
         fun navigateToCreatePin()
+        fun navigateToMain()
     }
 
     interface Presenter : MvpPresenter<View> {
         fun logNotificationPermissionGranted(isGranted: Boolean)
+        fun validDeeplink(deeplink: Uri)
     }
 }

app/src/main/java/org/p2p/wallet/auth/ui/onboarding/root/OnboardingRootFragment.kt
@@ -1,10 +1,11 @@
 package org.p2p.wallet.auth.ui.onboarding.root
 
+import androidx.core.app.NotificationManagerCompat
+import androidx.fragment.app.Fragment
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import androidx.core.app.NotificationManagerCompat
-import androidx.fragment.app.Fragment
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 import org.p2p.wallet.R
@@ -13,6 +14,7 @@ import org.p2p.wallet.auth.ui.onboarding.continuestep.ContinueOnboardingFragment
 import org.p2p.wallet.auth.ui.pin.newcreate.NewCreatePinFragment
 import org.p2p.wallet.auth.ui.restore.common.CommonRestoreFragment
 import org.p2p.wallet.common.mvp.BaseMvpFragment
+import org.p2p.wallet.home.MainFragment
 import org.p2p.wallet.utils.popAndReplaceFragment
 import org.p2p.wallet.utils.replaceFragment
 
@@ -52,6 +54,10 @@ class OnboardingRootFragment :
         popAndReplaceFragment(NewCreatePinFragment.create(), inclusive = true)
     }
 
+    override fun navigateToMain() {
+        popAndReplaceFragment(target = MainFragment.create(), inclusive = true)
+    }
+
     override fun applyWindowInsets(rootView: View) {
         // do nothing
     }
@@ -62,4 +68,8 @@ class OnboardingRootFragment :
         fragmentManager = childFragmentManager,
         addToBackStack = false
     )
+
+    fun triggerOnboadringDeeplink(deeplink: Uri) {
+        presenter.validDeeplink(deeplink)
+    }
 }

app/src/main/java/org/p2p/wallet/auth/ui/onboarding/root/OnboardingRootPresenter.kt
@@ -1,18 +1,31 @@
 package org.p2p.wallet.auth.ui.onboarding.root
 
+import android.net.Uri
+import kotlinx.coroutines.launch
+import org.p2p.solanaj.crypto.DerivationPath
+import org.p2p.uikit.organisms.seedphrase.SeedPhraseWord
 import org.p2p.wallet.auth.analytics.OnboardingAnalytics
 import org.p2p.wallet.auth.interactor.AuthInteractor
 import org.p2p.wallet.auth.repository.UserSignUpDetailsStorage
 import org.p2p.wallet.common.mvp.BasePresenter
+import org.p2p.wallet.deeplinks.AppDeeplinksManager
+import org.p2p.wallet.deeplinks.DeeplinkQuery
+import org.p2p.wallet.infrastructure.network.provider.SeedPhraseProvider
+import org.p2p.wallet.infrastructure.network.provider.SeedPhraseSource
 import org.p2p.wallet.infrastructure.network.provider.TokenKeyProvider
 import org.p2p.wallet.intercom.IntercomService
+import org.p2p.wallet.restore.interactor.SeedPhraseInteractor
+import org.p2p.wallet.restore.model.SeedPhraseVerifyResult
 import org.p2p.wallet.utils.emptyString
 
 class OnboardingRootPresenter(
     private val userSignUpDetailsStorage: UserSignUpDetailsStorage,
     private val tokenKeyProvider: TokenKeyProvider,
     private val authInteractor: AuthInteractor,
-    private val onboardingAnalytics: OnboardingAnalytics
+    private val onboardingAnalytics: OnboardingAnalytics,
+    private val deeplinksManager: AppDeeplinksManager,
+    private val seedPhraseInteractor: SeedPhraseInteractor,
+    private val seedPhraseProvider: SeedPhraseProvider
 ) : BasePresenter<OnboardingRootContract.View>(), OnboardingRootContract.Presenter {
 
     override fun attach(view: OnboardingRootContract.View) {
@@ -48,5 +61,45 @@ class OnboardingRootPresenter(
 
     override fun logNotificationPermissionGranted(isGranted: Boolean) {
         onboardingAnalytics.setUserGrantedNotificationPermissions(isGranted = isGranted)
+    }
+
+    override fun validDeeplink(deeplink: Uri) {
+        val seedPhrase = deeplink.getQueryParameter(DeeplinkQuery.value) ?: return
+        if (seedPhrase.isBlank()) return
+
+        launch {
+            val keys = seedPhrase.split("-").map { key ->
+                SeedPhraseWord(
+                    text = key,
+                    isValid = true
+                )
+            }
+            verifySeedPhrase(keys, deeplink)
+        }
+    }
+
+    private suspend fun verifySeedPhrase(seedPhrase: List<SeedPhraseWord>, deeplink: Uri) {
+        when (val result = seedPhraseInteractor.verifySeedPhrase(seedPhrase)) {
+            is SeedPhraseVerifyResult.Verified -> {
+                val keys = result.getKeys()
+                seedPhraseProvider.setUserSeedPhrase(
+                    words = keys,
+                    provider = SeedPhraseSource.MANUAL
+                )
+                seedPhraseInteractor.createAndSaveAccount(DerivationPath.BIP44CHANGE, keys)
+                createPin(deeplink)
+            }
+            is SeedPhraseVerifyResult.Invalid,
+            is SeedPhraseVerifyResult.VerificationFailed -> Unit
+        }
+    }
+
+    private fun createPin(deeplink: Uri) {
+        val pinCode = deeplink.getQueryParameter(DeeplinkQuery.pinCode) ?: return
+        if (pinCode.isNotEmpty()) {
+            authInteractor.registerComplete(pinCode, cipher = null)
+            authInteractor.finishSignUp()
+            view?.navigateToMain()
+        }
     }
 }

app/src/main/java/org/p2p/wallet/debug/settings/DebugSettingsAdapter.kt
@@ -149,10 +149,10 @@ class DebugSettingsAdapter(
         private val onSettingsRowSwitchListener: (titleResId: Int, isSelected: Boolean) -> Unit
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        private val title = binding.textViewSettingName
-        private val subtitle = binding.textViewSettingSubtitle
+        private val title = binding.textViewSettingsName
+        private val subtitle = binding.textViewSettingsSubtitle
         private val switcher = binding.switchChangeSettings
-        private val icon = binding.imageViewSettingIcon
+        private val icon = binding.imageViewSettingsIcon
 
         fun bind(item: SettingsRow.Switcher) {
             title.setText(item.titleResId)

app/src/main/java/org/p2p/wallet/deeplinks/AppDeeplinksManager.kt
@@ -1,19 +1,18 @@
 package org.p2p.wallet.deeplinks
 
 import androidx.core.content.getSystemService
-import androidx.fragment.app.FragmentManager
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.p2p.uikit.components.ScreenTab
 import org.p2p.wallet.R
-import org.p2p.wallet.home.MainFragment
 import org.p2p.wallet.intercom.IntercomDeeplinkManager
 import org.p2p.wallet.notification.NotificationType
 import org.p2p.wallet.root.RootActivity
+import org.p2p.wallet.root.RootListener
 import org.p2p.wallet.utils.toStringMap
+
+private const val EXTRA_TAB_SCREEN = "EXTRA_TAB_SCREEN"
 
 class AppDeeplinksManager(
     private val context: Context,
@@ -22,17 +21,64 @@ class AppDeeplinksManager(
 
     companion object {
         const val NOTIFICATION_TYPE = "eventType"
-        const val DEEPLINK_MAIN_SCREEN_EXTRA = "DEEPLINK_SCREEN_EXTRA"
     }
 
-    var mainTabsSwitcher: MainTabsSwitcher? = null
-    var mainFragmentManager: FragmentManager? = null
+    private var mainTabsSwitcher: MainTabsSwitcher? = null
+    private var rootListener: RootListener? = null
 
     private var pendingIntent: Intent? = null
-    var pendingDeeplinkUri: Uri? = null
 
-    val hasPendingDeeplink
-        get() = pendingDeeplinkUri != null
+    fun setTabsSwitcher(mainTabsSwitcher: MainTabsSwitcher) {
+        this.mainTabsSwitcher = mainTabsSwitcher
+    }
+
+    fun setRootListener(rootListener: RootListener) {
+        this.rootListener = rootListener
+    }
+
+    fun clearTabsSwitcher() {
+        mainTabsSwitcher = null
+    }
+
+    fun clearRootListener() {
+        rootListener = null
+    }
+
+    fun handleDeeplinkIntent(intent: Intent) {
+        when {
+            isDeeplinkWithUri(intent) -> {
+                val data = intent.data ?: return
+
+                val isValidScheme = context.getString(R.string.app_scheme) == data.scheme
+                when {
+                    isValidScheme && DeeplinkUtils.isValidOnboardingLink(data) -> {
+                        rootListener?.triggerOnboardingDeeplink(data)
+                    }
+                    intercomDeeplinkManager.handleBackgroundDeeplink(data) -> {
+                        // do nothing
+                    }
+                }
+            }
+            isDeeplinkWithExtras(intent) -> {
+                val extras = intent.extras ?: return
+
+                when {
+                    // additional parsing when app been opened with notification from background
+                    extras.containsKey(NOTIFICATION_TYPE) -> {
+                        val values = extras.toStringMap()
+                        val notificationType = NotificationType.fromValue(
+                            values[NOTIFICATION_TYPE].orEmpty()
+                        )
+                        intent.addDeeplinkDataToIntent(notificationType)
+                    }
+                    extras.containsKey(EXTRA_TAB_SCREEN) -> {
+                        switchToMainTabIfPossible(intent)
+                    }
+                }
+            }
+            else -> Unit
+        }
+    }
 
     fun buildIntent(notificationType: NotificationType): Intent {
         val activityManager = context.getSystemService<ActivityManager>()
@@ -46,78 +92,34 @@ class AppDeeplinksManager(
         }
     }
 
+    fun executeHomePendingDeeplink() {
+        pendingIntent?.let { switchToMainTabIfPossible(it) }
+        pendingIntent = null
+    }
+
     private fun Intent.addDeeplinkDataToIntent(notificationType: NotificationType) {
-        if (notificationType == NotificationType.RECEIVE) {
-            putExtra(DEEPLINK_MAIN_SCREEN_EXTRA, R.id.historyItem)
+        val navigationId = when (notificationType) {
+            NotificationType.RECEIVE -> R.id.historyItem
+            NotificationType.DEFAULT -> R.id.homeItem
         }
+
+        putExtra(EXTRA_TAB_SCREEN, navigationId)
     }
 
-    fun handleDeeplinkIntent(intent: Intent) {
-        when {
-            isDeeplinkWithUri(intent) -> {
-                val data = intent.data ?: return
-                val needStartDeeplink = data.host in DeeplinkHosts.validListToStartDeeplinks
-                if (context.getString(R.string.app_scheme) == data.scheme && needStartDeeplink) {
-                    pendingDeeplinkUri = data
-                } else {
-                    intercomDeeplinkManager.handleBackgroundDeeplink(data)
-                }
-            }
-            isDeeplinkWithExtras(intent) -> {
-                val extras = intent.extras ?: return
-                // additional parsing when app been opened with notification from background
-                if (extras.containsKey(NOTIFICATION_TYPE)) {
-                    val values = extras.toStringMap()
-                    val notificationType = NotificationType.fromValue(
-                        values[NOTIFICATION_TYPE].orEmpty()
-                    )
-                    intent.addDeeplinkDataToIntent(notificationType)
-                }
-                handleOrSaveDeeplinkIntent(intent)
-            }
-            else -> Unit
-        }
-    }
+    private fun switchToMainTabIfPossible(intent: Intent) {
+        val extras = intent.extras ?: return
 
-    private fun isDeeplinkWithUri(intent: Intent): Boolean = intent.data != null
+        rootListener?.popBackStackToMain()
 
-    private fun isDeeplinkWithExtras(intent: Intent): Boolean = intent.extras != null
-
-    private fun handleOrSaveDeeplinkIntent(intent: Intent) {
-        intent.extras?.apply {
-            if (containsKey(DEEPLINK_MAIN_SCREEN_EXTRA)) {
-                popToMainScreen()
-                mainTabsSwitcher?.navigate(
-                    ScreenTab.fromTabId(getInt(DEEPLINK_MAIN_SCREEN_EXTRA))!!
-                )
-                    ?: savePendingIntent(intent)
-            }
-        }
-    }
-
-    private fun popToMainScreen() {
-        mainFragmentManager?.apply {
-            if (backStackEntryCount > 1) {
-                val lastScreen = fragments.lastOrNull()
-                if (lastScreen is BottomSheetDialogFragment) {
-                    lastScreen.dismissAllowingStateLoss()
-                }
-                popBackStackImmediate(
-                    MainFragment::class.java.name,
-                    0
-                )
-            }
-        }
-    }
-
-    fun handleSavedDeeplinkIntent() {
-        pendingIntent?.let {
-            handleOrSaveDeeplinkIntent(it)
-            pendingIntent = null
-        }
+        val clickedTab = ScreenTab.fromTabId(extras.getInt(EXTRA_TAB_SCREEN))!!
+        mainTabsSwitcher?.navigate(clickedTab) ?: savePendingIntent(intent)
     }
 
     private fun savePendingIntent(intent: Intent) {
         pendingIntent = intent
     }
+
+    private fun isDeeplinkWithUri(intent: Intent): Boolean = intent.data != null
+
+    private fun isDeeplinkWithExtras(intent: Intent): Boolean = intent.extras != null
 }

app/src/main/java/org/p2p/wallet/deeplinks/DeeplinkUtils.kt
@@ -1,11 +1,17 @@
 package org.p2p.wallet.deeplinks
 
 import android.net.Uri
+import org.p2p.wallet.deeplinks.DeeplinkHosts.ONBOARDING_DEEPLINK_SEGMENT
+
+object DeeplinkUtils {
+    fun isValidOnboardingLink(uri: Uri): Boolean {
+        val segment = uri.pathSegments?.firstOrNull()
+        return uri.host == ONBOARDING_DEEPLINK_SEGMENT && segment == DeeplinkSegments.onboardingSeedPhrase
+    }
+}
 
 object DeeplinkHosts {
-    private const val ONBOARDING_DEEPLINK_SEGMENT = "onboarding"
-
-    val validListToStartDeeplinks = listOf(ONBOARDING_DEEPLINK_SEGMENT)
+    const val ONBOARDING_DEEPLINK_SEGMENT = "onboarding"
 }
 
 object DeeplinkSegments {
@@ -15,10 +21,4 @@ object DeeplinkSegments {
 object DeeplinkQuery {
     const val pinCode = "pincode"
     const val value = "value"
-}
-
-object DeeplinkUtils {
-    fun hasFastOnboardingDeeplink(uri: Uri?): Boolean {
-        return uri?.pathSegments?.first() == DeeplinkSegments.onboardingSeedPhrase
-    }
 }

commit cb151e8f984dcb3928a83481f01d9db399e45e4e

NO-TICKET - fix login by seed phrase caused by 6c0c8c65

app/src/main/java/org/p2p/wallet/restore/ui/seedphrase/SeedPhrasePresenter.kt
@@ -1,13 +1,13 @@
 package org.p2p.wallet.restore.ui.seedphrase
 
+import kotlin.properties.Delegates.observable
+import kotlinx.coroutines.launch
 import org.p2p.uikit.organisms.seedphrase.SeedPhraseWord
 import org.p2p.wallet.R
 import org.p2p.wallet.common.mvp.BasePresenter
 import org.p2p.wallet.infrastructure.network.provider.SeedPhraseProvider
 import org.p2p.wallet.infrastructure.network.provider.SeedPhraseSource
 import org.p2p.wallet.restore.interactor.SeedPhraseInteractor
-import kotlin.properties.Delegates.observable
-import kotlinx.coroutines.launch
 import org.p2p.wallet.restore.model.SeedPhraseVerifyResult
 
 private const val SEED_PHRASE_SIZE_SHORT = 12
@@ -62,6 +62,7 @@ class SeedPhrasePresenter(
                         words = result.getKeys(),
                         provider = SeedPhraseSource.MANUAL
                     )
+                    view?.navigateToDerievableAccounts(currentSeedPhrase)
                 }
                 is SeedPhraseVerifyResult.Invalid -> {
                     // warning: updateSeedPhrase causes keyboard to appear, so add a check