f7e94f24845f3dd9d842e7b98fb37ed7b90c0db5
NikolaiEmelianov <121934748+NikolaiEmelianov@users.noreply.github.com>
2023-02-24 15:35:05 +0400

    PWN-7169 - debounce amount input (#1309)
    * NO-TICKET - fix outline provider for previous sdk
    
    * PWN-7030 - IconWrapper, LeftSide, IconContainer
    
    * PWN-7030 - ktlin
    
    * PWN-7030 - finance block xml, sample app
    
    * PWN-7030 - sample, fix models
    
    * PWN-7030 - ktlint
    
    * PWN-7030 - inflate listener
    
    * PWN-7030 - fix margin
    
    * PWN-7039 - rename
    
    * PWN-7039 - common view pool, right side view
    
    * PWN-7039 - ktlin fixes
    
    * PWN-7039 - example of actions
    
    * PWN-7039 - preview, examples
    
    * PWN-7039 - preview, examples
    
    * PWN-7039 - fix image
    
    * PWN-7039 - sample, fix badge
    
    * PWN-7039 - rename pkg
    
    * PWN-7039 - add @Deprecated
    
    * PWN-7039 - deprecated old, fix constrain, moved icon wrapper
    
    * PWN-7030 - renaming, AnyCellItem.kt
    
    * PWN-7030 - reuse DrawableContainer
    
    * PWN-7030 - private binding
    
    * PWN-7030 - delete sample app
    
    * PWN-7030 - fix after merge
    
    * PWN-7171 - New Swap. Draft for swap state store
    
    * PWN-7168 - widget view, swap fragment, skeleton
    
    * PWN-7171 - New Swap. Change naming
    
    * PWN-7168 - text skeleton
    
    * PWN-7168 - ktlint
    
    * PWN-7168 - alert, strings, loading widget state
    
    * PWN-7168 - ktlint
    
    * PWN-7168 - rollback main fragment
    
    * PWN-7168 - fix skeleton
    
    * PWN-7171 - New Swap. Fix comments
    
    * PWN-7168 - textview auto size
    
    * PWN-6960 - domain token wrapper model
    
    * PWN-6960 - domain token wrapper model
    
    * PWN-6960 - widget state mapper
    
    * PWN-6960 - button state, mapper, ratio state
    
    * PWN-6960 - amount autoSize, formatting
    
    * PWN-6960 - small fixes
    
    * PWN-6960 - review fixes
    
    * PWN-7169 - add handlers, error state
    
    * PWN-7169 - add handlers, error state
    
    * PWN-7169 - fix after merge
    
    * PWN-7169 - token selector
    
    * PWN-7169 - ktlint
    
    * PWN-7169 - provide different token selector
    
    * PWN-7169 - ktlint
    
    * PWN-7169 - ui fixes
    
    * PWN-7169 - ui fixes
    
    * NO-TICKET - skeleton
    
    * PWN-7169 - fix after merge
    
    * PWN-7169 - fix after merge
    
    * PWN-7169 - koin module, manager holder
    
    * PWN-7169 - review fixes
    
    * PWN-7169 - review fixes
    
    * PWN-7169 - rate loader
    
    * PWN-7169 - fix after merge
    
    * PWN-7169 - fix after merge
    
    * PWN-7169 - rate loader
    
    * PWN-7169 - fix
    
    * PWN-7169 - ktlint
    
    * PWN-7169 - widget click actions
    
    * PWN-7169 - swap click, static skeleton
    
    * PWN-7169 - debounce amount input
    
    * PWN-7169 - ktlint
    
    * PWN-7169 - review fixes
    
    ---------
    
    Co-authored-by: Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>

--- app/src/main/java/org/p2p/wallet/swap/ui/jupiter/main/mapper/SwapWidgetMapper.kt
+++ app/src/main/java/org/p2p/wallet/swap/ui/jupiter/main/mapper/SwapWidgetMapper.kt
@@ -41,7 +41,8 @@ class SwapWidgetMapper {
             is SwapRateLoaderState.Loaded -> widgetModel.copy(
                 fiatAmount = fiatAmount(
                     fiatAmount = tokenAmount.multiply(state.rate)
-                )
+                ),
+                amount = tokenAmount(state.token, tokenAmount)
             )
             SwapRateLoaderState.Loading -> widgetModel.copy(
                 fiatAmount = textCellSkeleton(
@@ -53,10 +54,22 @@ class SwapWidgetMapper {
         }
     }
 
-    fun mapTokenA(
+    fun mapTokenAAndSaveOldFiatAmount(
+        oldWidgetModel: SwapWidgetModel,
         token: SwapTokenModel,
         tokenAmount: BigDecimal? = null,
     ): SwapWidgetModel {
+        var result = mapTokenA(token, tokenAmount)
+        if (oldWidgetModel is SwapWidgetModel.Content && oldWidgetModel.fiatAmount != null) {
+            result = result.copy(fiatAmount = oldWidgetModel.fiatAmount)
+        }
+        return result
+    }
+
+    fun mapTokenA(
+        token: SwapTokenModel,
+        tokenAmount: BigDecimal? = null,
+    ): SwapWidgetModel.Content {
         return SwapWidgetModel.Content(
             isStatic = false,
             widgetTitle = swapWidgetFromTitle(),
@@ -69,7 +82,19 @@ class SwapWidgetMapper {
         )
     }
 
-    fun mapTokenB(token: SwapTokenModel, tokenAmount: BigDecimal?): SwapWidgetModel {
+    fun mapTokenBAndSaveOldFiatAmount(
+        oldWidgetModel: SwapWidgetModel,
+        token: SwapTokenModel,
+        tokenAmount: BigDecimal? = null,
+    ): SwapWidgetModel {
+        var result = mapTokenB(token, tokenAmount)
+        if (oldWidgetModel is SwapWidgetModel.Content && oldWidgetModel.fiatAmount != null) {
+            result = result.copy(fiatAmount = oldWidgetModel.fiatAmount)
+        }
+        return result
+    }
+
+    fun mapTokenB(token: SwapTokenModel, tokenAmount: BigDecimal?): SwapWidgetModel.Content {
         return SwapWidgetModel.Content(
             isStatic = true,
             widgetTitle = swapWidgetToTitle(),
