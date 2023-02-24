773d3ccea176a8f3d480f703098755ea52e823a7
Nikolai Emelianov <nikolai.emelianov@p2p.org>
2023-02-24 15:30:38 +0400

    PWN-7169 - same token error, price impact, amount error

---
+++ app/src/main/java/org/p2p/wallet/swap/jupiter/statemanager/validator/SwapValidator.kt
@@ -0,0 +1,28 @@
+package org.p2p.wallet.swap.jupiter.statemanager.validator
+
+import java.math.BigDecimal
+import org.p2p.core.utils.isMoreThan
+import org.p2p.core.utils.isZero
+import org.p2p.wallet.swap.jupiter.domain.model.SwapTokenModel
+import org.p2p.wallet.swap.jupiter.statemanager.SwapFeatureException
+
+class SwapValidator {
+
+    fun validateInputAmount(
+        tokenA: SwapTokenModel,
+        amountTokenA: BigDecimal
+    ) {
+        when {
+            tokenA is SwapTokenModel.JupiterToken && !amountTokenA.isZero() ||
+                tokenA is SwapTokenModel.UserToken && amountTokenA.isMoreThan(tokenA.tokenAmount)
+            -> throw SwapFeatureException.NotValidTokenA(amountTokenA)
+        }
+    }
+
+    fun validateIsSameTokens(
+        tokenA: SwapTokenModel,
+        tokenB: SwapTokenModel,
+    ) {
+        if (tokenA.mintAddress == tokenB.mintAddress) throw SwapFeatureException.SameTokens
+    }
+}
