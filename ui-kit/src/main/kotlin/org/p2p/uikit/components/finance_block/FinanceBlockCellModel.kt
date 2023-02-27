d94f79a6ddf38e1b35c395bc408e3ca3ecd633f3
Nikolai Emelianov <nikolai.emelianov@p2p.org>
2023-02-27 15:22:18 +0400

    PWN-7169 - select token a decorator

--- ui-kit/src/main/kotlin/org/p2p/uikit/components/finance_block/FinanceBlockCellModel.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/components/finance_block/FinanceBlockCellModel.kt
@@ -1,14 +1,17 @@
 package org.p2p.uikit.components.finance_block
 
+import org.p2p.uikit.R
 import org.p2p.uikit.components.left_side.LeftSideCellModel
 import org.p2p.uikit.components.right_side.RightSideCellModel
 import org.p2p.uikit.model.AnyCellItem
 import org.p2p.uikit.model.CellModelPayload
+import org.p2p.uikit.utils.drawable.DrawableCellModel
 import org.p2p.uikit.utils.viewState.ViewAccessibilityCellModel
 
 data class FinanceBlockCellModel(
     val leftSideCellModel: LeftSideCellModel? = null,
     val rightSideCellModel: RightSideCellModel? = null,
     val accessibility: ViewAccessibilityCellModel = ViewAccessibilityCellModel(),
+    val background: DrawableCellModel? = DrawableCellModel(tint = R.color.snow),
     override val payload: Any? = null,
 ) : CellModelPayload, AnyCellItem
