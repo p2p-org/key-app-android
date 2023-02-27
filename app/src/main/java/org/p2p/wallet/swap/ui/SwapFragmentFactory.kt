308a03bc659cc5fe184fb65beea03f0062e795f4
Nikolai Emelianov <nikolai.emelianov@p2p.org>
2023-02-27 13:51:36 +0400

    PWN-7169 - open from argument

--- app/src/main/java/org/p2p/wallet/swap/ui/SwapFragmentFactory.kt
+++ app/src/main/java/org/p2p/wallet/swap/ui/SwapFragmentFactory.kt
@@ -5,13 +5,13 @@ import org.p2p.core.token.Token
 import org.p2p.wallet.common.feature_toggles.toggles.remote.NewSwapEnabledFeatureToggle
 import org.p2p.wallet.swap.ui.jupiter.main.JupiterSwapFragment
 import org.p2p.wallet.swap.ui.orca.OrcaSwapFragment
-import org.p2p.wallet.swap.ui.orca.OrcaSwapOpenedFrom
+import org.p2p.wallet.swap.ui.orca.SwapOpenedFrom
 
 class SwapFragmentFactory constructor(private val newSwapEnabledFeatureToggle: NewSwapEnabledFeatureToggle) {
 
-    fun swapFragment(token: Token.Active? = null, source: OrcaSwapOpenedFrom = OrcaSwapOpenedFrom.OTHER): Fragment {
+    fun swapFragment(token: Token.Active? = null, source: SwapOpenedFrom = SwapOpenedFrom.OTHER): Fragment {
         return if (newSwapEnabledFeatureToggle.isFeatureEnabled) {
-            JupiterSwapFragment.create(token)
+            JupiterSwapFragment.create(token, source)
         } else {
             token?.let { OrcaSwapFragment.create(it, source) } ?: OrcaSwapFragment.create(source)
         }
