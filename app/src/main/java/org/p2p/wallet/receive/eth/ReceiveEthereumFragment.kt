b64e36bafc2021c18b7b4b2c746042a7332c22e2
Davran331122 <khasanov.davran@p2p.org>
2023-03-13 16:19:00 +0600

    PWN-7382 - Fix PR comments

--- app/src/main/java/org/p2p/wallet/receive/eth/ReceiveEthereumFragment.kt
+++ app/src/main/java/org/p2p/wallet/receive/eth/ReceiveEthereumFragment.kt
@@ -62,7 +62,6 @@ class EthereumReceiveFragment :
                 showUiKitSnackBar(messageResId = R.string.receive_eth_address_copied)
             }
             financeBlockAddress.apply {
-                updateHorizontalPadding(start = 16, end = 8)
                 bind(
                     FinanceBlockCellModel(
                         leftSideCellModel = LeftSideCellModel.IconWithText(
