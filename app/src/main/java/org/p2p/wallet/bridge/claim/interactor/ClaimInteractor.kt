9eaac5a244040c6534a1808029929faba1a799d9
kabramovp2p <konstantin.abramov@p2p.org>
2023-03-17 10:23:47 +0200

    PWN-7378 - comment fix

--- app/src/main/java/org/p2p/wallet/bridge/claim/interactor/ClaimInteractor.kt
+++ app/src/main/java/org/p2p/wallet/bridge/claim/interactor/ClaimInteractor.kt
@@ -23,7 +23,7 @@ class ClaimInteractor(
             recipientAddress = solanaAddress,
             erc20Token = erc20Token,
             amount = amount,
-            slippage = null//DEFAULT_ERC20_TOKEN_SLIPPAGE
+            slippage = null // DEFAULT_ERC20_TOKEN_SLIPPAGE
         )
     }
 
