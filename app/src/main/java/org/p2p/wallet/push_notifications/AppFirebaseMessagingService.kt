8fc84000a299b82119e15fe8551a91404934654a
Nikolai Emelianov <nikolai.emelianov@p2p.org>
2023-03-20 14:08:31 +0300

    PWN-7587 update token each time login

--- app/src/main/java/org/p2p/wallet/push_notifications/AppFirebaseMessagingService.kt
+++ app/src/main/java/org/p2p/wallet/push_notifications/AppFirebaseMessagingService.kt
@@ -11,6 +11,9 @@ import org.p2p.wallet.notification.AppNotificationManager
 import org.p2p.wallet.notification.FcmPushNotificationData
 import org.p2p.wallet.notification.NotificationType
 import timber.log.Timber
+import kotlinx.coroutines.launch
+import org.p2p.wallet.common.di.AppScope
+import org.p2p.wallet.push_notifications.ineractor.PushNotificationsInteractor
 
 private const val TAG = "AppFirebaseMessagingService"
 
@@ -19,10 +22,13 @@ class AppFirebaseMessagingService : FirebaseMessagingService(), KoinComponent {
     private val appNotificationManager: AppNotificationManager by inject()
     private val appsFlyerService: AppsFlyerService by inject()
     private val intercomPushService: IntercomPushService by inject()
+    private val pushNotificationsInteractor: PushNotificationsInteractor by inject()
+    private val appScope: AppScope by inject()
 
     override fun onNewToken(token: String) {
         appsFlyerService.onNewToken(token)
         intercomPushService.registerForPush(token)
+        appScope.launch { pushNotificationsInteractor.updateDeviceToken() }
         super.onNewToken(token)
     }
 
