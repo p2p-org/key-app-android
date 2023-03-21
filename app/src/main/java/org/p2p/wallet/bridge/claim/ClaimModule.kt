8bf6ba9ac591d549a8d0d34802217946be2fd379
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2023-03-21 11:56:53 +0200

    PWN-7623 - show fees only if compensationDeclineReason.isNotEmpty() (#1457)
    * PWN-7623 - show fees only if compensationDeclineReason.isNotEmpty()
    
    * PWN-7623 - include totalFees in fix
    
    * PWN-7623 - switch isFree condition to make ClaimDetails
    
    * PWN-7623 - use resultAmount to show final amount to claim
    
    * PWN-7623 - move all ui stuff to ClaimUiMapper.kt
    
    * PWN-7623 - return real claim status

--- app/src/main/java/org/p2p/wallet/bridge/claim/ClaimModule.kt
+++ app/src/main/java/org/p2p/wallet/bridge/claim/ClaimModule.kt
@@ -9,6 +9,7 @@ import org.p2p.wallet.bridge.claim.repository.EthereumClaimRemoteRepository
 import org.p2p.wallet.bridge.claim.repository.EthereumClaimRepository
 import org.p2p.wallet.bridge.claim.ui.ClaimContract
 import org.p2p.wallet.bridge.claim.ui.ClaimPresenter
+import org.p2p.wallet.bridge.claim.ui.mapper.ClaimUiMapper
 import org.p2p.wallet.common.di.InjectionModule
 
 object ClaimModule : InjectionModule {
@@ -16,6 +17,7 @@ object ClaimModule : InjectionModule {
     override fun create() = module {
         singleOf(::EthereumClaimRemoteRepository) bind EthereumClaimRepository::class
         factoryOf(::ClaimInteractor)
+        factoryOf(::ClaimUiMapper)
         factoryOf(::ClaimPresenter) bind ClaimContract.Presenter::class
     }
 }
