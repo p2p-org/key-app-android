da0abeb4619fc589639cdba844982d42730cc3f1
kabramovp2p <konstantin.abramov@p2p.org>
2023-03-22 17:22:20 +0200

    PWN-7606 - fixes for coingecko use single for TokenPricesRemoteRepository and use common PriceCacheRepository.kt to use same logic under the hood

--- ethereumkit/src/main/java/org/p2p/ethereumkit/external/price/EthereumPriceRepository.kt
+++ ethereumkit/src/main/java/org/p2p/ethereumkit/external/price/EthereumPriceRepository.kt
@@ -1,49 +1,26 @@
 package org.p2p.ethereumkit.external.price
 
 import java.math.BigDecimal
-import java.time.ZonedDateTime
+import org.p2p.core.model.TokenPriceWithMark
+import org.p2p.core.pricecache.PriceCacheRepository
 import org.p2p.ethereumkit.external.api.coingecko.CoinGeckoService
-
-const val DEFAULT_EXPIRATION_TIME_SEC = 60
 
 internal class EthereumPriceRepository(
     private val priceApi: CoinGeckoService
-) : PriceRepository {
-
-    private val cachedPrices: MutableMap<String, TokenPrice> = mutableMapOf()
+) : PriceRepository, PriceCacheRepository() {
 
     override suspend fun getTokenPrices(
         tokenAddresses: List<String>,
         targetCurrency: String
     ): Map<String, BigDecimal> {
-        val filteredTokenAddresses = filterAddressesForExpiredPrices(tokenAddresses)
+        val filteredTokenAddresses = filterKeysForExpiredPrices(tokenAddresses)
         if (filteredTokenAddresses.isNotEmpty()) {
             val response = priceApi.getEthereumTokenPrices(
                 tokenAddresses = filteredTokenAddresses.joinToString(","),
                 targetCurrency = targetCurrency
             )
-            val timestamp = ZonedDateTime.now().toEpochSecond()
-            mergeCache(response.entries.associate { it.key to TokenPrice(it.value.priceInUsd, timestamp) })
+            mergeCache(response.entries.associate { it.key to TokenPriceWithMark(it.value.priceInUsd) })
         }
         return cachedPrices.entries.associate { it.key to it.value.priceInUsd }
-    }
-
-    private fun filterAddressesForExpiredPrices(tokenAddresses: List<String>): List<String> {
-        return tokenAddresses.filterNot { cachedPrices[it]?.isValid() == true }
-    }
-
-    private fun mergeCache(newItems: Map<String, TokenPrice>) {
-        for ((k, v) in newItems) {
-            cachedPrices.merge(k, v) { _, newVal -> newVal }
-        }
-    }
-}
-
-internal class TokenPrice(
-    val priceInUsd: BigDecimal,
-    val timestamp: Long
-) {
-    fun isValid(): Boolean {
-        return ZonedDateTime.now().toEpochSecond() - timestamp < DEFAULT_EXPIRATION_TIME_SEC
     }
 }
