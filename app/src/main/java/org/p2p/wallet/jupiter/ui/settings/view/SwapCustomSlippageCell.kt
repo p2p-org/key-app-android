b9ad0cb0cbcd47483609b1d0cbc1b9565b0fba01
Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>
2023-03-23 16:57:49 +0800

    PWN-7729 PWN-7733 - Swap release fixes (#1480)
    * PWN-7729 - New Swap. Increase slippage input delay
    
    * PWN-7733 - New Swap. Fix rate formatting
    
    * [Release] Bump 2.5.0.7 -> 2.5.0.8

--- app/src/main/java/org/p2p/wallet/jupiter/ui/settings/view/SwapCustomSlippageCell.kt
+++ app/src/main/java/org/p2p/wallet/jupiter/ui/settings/view/SwapCustomSlippageCell.kt
@@ -7,17 +7,18 @@ import org.p2p.core.utils.DecimalFormatter
 import org.p2p.uikit.model.AnyCellItem
 import org.p2p.uikit.utils.getColor
 import org.p2p.uikit.utils.getString
+import org.p2p.uikit.utils.inflateViewBinding
 import org.p2p.wallet.R
 import org.p2p.wallet.databinding.ItemSwapCustomSlippageBinding
-import org.p2p.wallet.swap.model.MAX_ALLOWED_SLIPPAGE
-import org.p2p.wallet.swap.model.PERCENT_DIVIDE_VALUE
 import org.p2p.wallet.swap.model.Slippage
+import org.p2p.wallet.swap.model.Slippage.Companion.MAX_ALLOWED_SLIPPAGE
+import org.p2p.wallet.swap.model.Slippage.Companion.PERCENT_DIVIDE_VALUE
 
 fun swapCustomSlippageDelegate(
     onCustomSlippageChange: (value: Double?) -> Unit,
 ): AdapterDelegate<List<AnyCellItem>> =
     adapterDelegateViewBinding<SwapCustomSlippageCellModel, AnyCellItem, ItemSwapCustomSlippageBinding>(
-        viewBinding = { inflater, parent -> ItemSwapCustomSlippageBinding.inflate(inflater, parent, false) },
+        viewBinding = { _, parent -> parent.inflateViewBinding(attachToRoot = false) },
     ) {
 
         val nightText = binding.getColor(R.color.text_night)
@@ -48,14 +49,14 @@ fun swapCustomSlippageDelegate(
 
         bind {
             val slippage = (item.slippage.doubleValue * PERCENT_DIVIDE_VALUE)
-            val text = DecimalFormatter.format(slippage, 2)
+            val text = DecimalFormatter.format(value = slippage, decimals = 2)
             with(binding) {
                 if (text != textInputEditTextCustomSlippage.text?.toString()) {
                     internalOnAmountChanged = null
                     textInputEditTextCustomSlippage.setText(text)
                     validateSlippage(text)
                     textInputEditTextCustomSlippage.setSelection(text.length)
-                    internalOnAmountChanged = { validateSlippage(it) }
+                    internalOnAmountChanged = ::validateSlippage
                 }
             }
         }
