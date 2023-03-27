8953c5a458927e0abff74977c5784e31a8acb352
kabramovp2p <konstantin.abramov@p2p.org>
2023-03-27 13:00:34 +0300

    PWN-7710 - fixes to show ETH not WETH and total amount in you get field

--- app/src/main/java/org/p2p/wallet/bridge/claim/ui/ClaimPresenter.kt
+++ app/src/main/java/org/p2p/wallet/bridge/claim/ui/ClaimPresenter.kt
@@ -8,11 +8,10 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.p2p.core.common.TextContainer
 import org.p2p.core.token.Token
-import org.p2p.core.token.TokenData
-import org.p2p.core.utils.Constants
 import org.p2p.core.utils.isConnectionError
 import org.p2p.core.utils.toLamports
 import org.p2p.core.wrapper.HexString
+import org.p2p.ethereumkit.external.model.ERC20Tokens
 import org.p2p.ethereumkit.external.repository.EthereumRepository
 import org.p2p.uikit.utils.text.TextViewCellModel
 import org.p2p.wallet.R
@@ -20,6 +19,7 @@ import org.p2p.wallet.bridge.claim.interactor.ClaimInteractor
 import org.p2p.wallet.bridge.claim.model.ClaimDetails
 import org.p2p.wallet.bridge.claim.ui.mapper.ClaimUiMapper
 import org.p2p.wallet.bridge.model.BridgeBundleFees
+import org.p2p.wallet.bridge.model.BridgeFee
 import org.p2p.wallet.bridge.model.BridgeResult
 import org.p2p.wallet.bridge.model.BridgeResult.Error.ContractError
 import org.p2p.wallet.bridge.model.BridgeResult.Error.NotEnoughAmount
@@ -50,12 +50,16 @@ class ClaimPresenter(
     private var latestTransactions: List<HexString> = emptyList()
     private var latestBundleId: String = emptyString()
     private var refreshJobDelayTimeInMillis = DEFAULT_DELAY_IN_MILLIS
-    private var eth: TokenData? = null
+    private var eth: Token.Eth? = null
 
     override fun attach(view: ClaimContract.View) {
         super.attach(view)
         launch {
-            eth = userLocalRepository.findTokenData(Constants.WRAPPED_ETH_MINT)
+            if (eth == null) {
+                eth = ethereumRepository.loadWalletTokens().firstOrNull {
+                    it.mintAddress == ERC20Tokens.ETH.mintAddress
+                }
+            }
         }
         startRefreshJob()
         view.apply {
@@ -85,7 +89,7 @@ class ClaimPresenter(
                     latestBundleId = bundleId
                     refreshJobDelayTimeInMillis = getExpirationDateInMillis() - ZonedDateTime.now().dateMilli()
                     latestTransactions = transactions
-                    showFees(fees, compensationDeclineReason.isEmpty())
+                    showFees(resultAmount, fees, compensationDeclineReason.isEmpty())
                     val finalValue = claimUiMapper.makeResultAmount(resultAmount, tokenToClaim)
                     view?.showClaimButtonValue(finalValue.formattedTokenAmount.orEmpty())
                 }
@@ -105,9 +109,9 @@ class ClaimPresenter(
         }
     }
 
-    private fun showFees(fees: BridgeBundleFees, isFree: Boolean) {
+    private fun showFees(resultAmount: BridgeFee, fees: BridgeBundleFees, isFree: Boolean) {
         view?.showFee(claimUiMapper.mapFeeTextContainer(fees, isFree))
-        claimDetails = claimUiMapper.makeClaimDetails(tokenToClaim, fees, eth)
+        claimDetails = claimUiMapper.makeClaimDetails(tokenToClaim, resultAmount, fees, eth)
         view?.setClaimButtonState(isButtonEnabled = true)
     }
 
