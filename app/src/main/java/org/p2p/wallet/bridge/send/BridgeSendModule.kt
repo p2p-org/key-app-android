108107628c5e2f701128d66e6ba64d315b948e08
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2023-03-28 10:11:11 +0300

    PWN-7828 - EthereumSendInteractor.kt and fixes to make real call (#1492)
    * PWN-7828 - EthereumSendInteractor.kt and fixes to make real call
    
    * PWN-7856 - changes according to comments

--- app/src/main/java/org/p2p/wallet/bridge/send/BridgeSendModule.kt
+++ app/src/main/java/org/p2p/wallet/bridge/send/BridgeSendModule.kt
@@ -3,7 +3,9 @@ package org.p2p.wallet.bridge.send
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import org.p2p.wallet.bridge.send.interactor.EthereumSendInteractor
 import org.p2p.wallet.bridge.send.repository.EthereumSendRemoteRepository
+import org.p2p.wallet.bridge.send.repository.EthereumSendRepository
 import org.p2p.wallet.bridge.send.ui.BridgeSendContract
 import org.p2p.wallet.bridge.send.ui.BridgeSendPresenter
 import org.p2p.wallet.common.di.InjectionModule
@@ -11,8 +13,9 @@ import org.p2p.wallet.common.di.InjectionModule
 object BridgeSendModule : InjectionModule {
     override fun create() = module {
         factoryOf(::SendFragmentFactory)
+        factoryOf(::EthereumSendInteractor)
         factoryOf(::BridgeSendPresenter) bind BridgeSendContract.Presenter::class
-        factoryOf(::EthereumSendRemoteRepository) bind EthereumSendRemoteRepository::class
+        factoryOf(::EthereumSendRemoteRepository) bind EthereumSendRepository::class
         factoryOf(::BridgeSendInteractor)
     }
 }
