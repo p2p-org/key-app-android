31e29b0e4e146b78fb6c8d2796e4a9fb222bf1ea
Nikolai Emelianov <nikolai.emelianov@p2p.org>
2023-03-29 11:29:33 +0300

    PWN-7614 - ktlint

--- app/src/main/java/org/p2p/wallet/bridge/api/response/BridgeSendTransactionResponse.kt
+++ app/src/main/java/org/p2p/wallet/bridge/api/response/BridgeSendTransactionResponse.kt
@@ -1,7 +1,6 @@
 package org.p2p.wallet.bridge.api.response
 
 import com.google.gson.annotations.SerializedName
-import org.p2p.solanaj.utils.crypto.Base64String
 
 data class BridgeSendTransactionResponse(
     @SerializedName("transaction")
