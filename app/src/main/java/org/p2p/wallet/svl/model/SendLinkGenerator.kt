7dafb9268730310db3f782e7523ee84ebc976a60
Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>
2023-03-29 19:06:12 +0800

    PWN-7891 PWN-7894 - SVL. Fix account generation (#1511)

--- app/src/main/java/org/p2p/wallet/svl/model/SendLinkGenerator.kt
+++ app/src/main/java/org/p2p/wallet/svl/model/SendLinkGenerator.kt
@@ -3,6 +3,7 @@ package org.p2p.wallet.svl.model
 import timber.log.Timber
 import kotlin.random.Random
 import org.p2p.solanaj.core.Account
+import org.p2p.solanaj.core.AccountCreationFailed
 import org.p2p.solanaj.crypto.DerivationPath
 import org.p2p.wallet.BuildConfig
 import org.p2p.wallet.newsend.model.TemporaryAccount
@@ -19,16 +20,21 @@ object SendLinkGenerator {
 
         Timber.tag("SendLinkGenerator").d("Generated symbols: $generatedSymbols")
 
-        val account = Account.fromBip44Mnemonic(
-            words = generatedSymbols,
-            walletIndex = 0,
-            derivationPath = DerivationPath.BIP44CHANGE,
-            saltPrefix = BuildConfig.saltPrefix,
-            includeSpaces = false
-        )
+        val account = try {
+            Account.fromBip44Mnemonic(
+                words = generatedSymbols,
+                walletIndex = 0,
+                derivationPath = DerivationPath.BIP44CHANGE,
+                saltPrefix = BuildConfig.saltPrefix,
+                includeSpaces = false
+            )
+        } catch (failed: AccountCreationFailed) {
+            Timber.e(failed, "Failed to create temporary account")
+            throw failed
+        }
 
         return TemporaryAccount(
-            symbols = generatedSymbols.joinToString(emptyString()),
+            symbols = generatedSymbols.joinToString(separator = emptyString()),
             address = account.publicKey.toBase58(),
             keypair = account.getEncodedKeyPair()
         )
@@ -37,8 +43,13 @@ object SendLinkGenerator {
     private fun generateSymbols(): List<String> {
         val regex = Regex(REGEX_LINK_ALLOWED_SYMBOLS)
         val random = Random.Default
-        return (1..SYMBOLS_COUNT)
-            .map { random.nextInt(ASCII_CHARACTERS_COUNT).toString() }
-            .filter { regex.matches(it) }
+        val resultSymbols = mutableListOf<String>()
+        while (resultSymbols.size != SYMBOLS_COUNT) {
+            val nextAsciiSymbol = Char(code = random.nextInt(ASCII_CHARACTERS_COUNT)).toString()
+            if (regex.matches(nextAsciiSymbol)) {
+                resultSymbols += nextAsciiSymbol
+            }
+        }
+        return resultSymbols
     }
 }
