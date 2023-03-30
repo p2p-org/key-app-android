69bc47bf5d253c1047964c1f989b6c78eca25815
Chingiz <chyngyz.i@p2p.org>
2023-03-30 17:00:53 +0600

    Bump version

--- buildSrc/src/main/java/AppVersions.kt
+++ buildSrc/src/main/java/AppVersions.kt
@@ -2,6 +2,6 @@ object AppVersions {
     const val VERSION_MAJOR = 2
     const val VERSION_MINOR = 5
     const val VERSION_HOTFIX = 1
-    const val VERSION_PATCH = 1
+    const val VERSION_PATCH = 2
     const val VERSION_BUILD = 1679029420412
 }
