3055cd34384f334f54976c262b63b75d46cced90
Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>
2023-03-30 18:56:43 +0800

    PWN-7927 - Fix SOL rate in DerivableAccountFragment (#1523)
    * PWN-7927 - Fix rates in DerivableAccountsPresenter
    
    * Bump app version

--- app/src/main/java/org/p2p/wallet/restore/model/DerivableAccount.kt
+++ app/src/main/java/org/p2p/wallet/restore/model/DerivableAccount.kt
@@ -1,12 +1,12 @@
 package org.p2p.wallet.restore.model
 
-import org.p2p.solanaj.crypto.DerivationPath
-import org.p2p.solanaj.core.Account
 import java.math.BigDecimal
+import org.p2p.solanaj.core.Account
+import org.p2p.solanaj.crypto.DerivationPath
 
 data class DerivableAccount(
     val path: DerivationPath,
     val account: Account,
-    val total: BigDecimal,
+    val totalInSol: BigDecimal,
     val totalInUsd: BigDecimal? = null
 )
