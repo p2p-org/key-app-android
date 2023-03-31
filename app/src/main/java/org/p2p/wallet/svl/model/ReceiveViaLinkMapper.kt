da625fb58fc57aee726ec495786b7fdc597ce824
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2023-03-31 12:04:19 +0800

    PWN-7940 - SVL. Remove date subtitle in bottom sheet

--- app/src/main/java/org/p2p/wallet/svl/model/ReceiveViaLinkMapper.kt
+++ app/src/main/java/org/p2p/wallet/svl/model/ReceiveViaLinkMapper.kt
@@ -1,13 +1,11 @@
 package org.p2p.wallet.svl.model
 
-import android.content.Context
 import org.p2p.core.common.DrawableContainer
 import org.p2p.core.common.TextContainer
 import org.p2p.core.token.Token
 import org.p2p.uikit.utils.image.ImageViewCellModel
 import org.p2p.uikit.utils.text.TextViewCellModel
 import org.p2p.wallet.R
-import org.p2p.wallet.utils.DateTimeUtils
 
 class ReceiveViaLinkMapper {
 
@@ -21,26 +19,17 @@ class ReceiveViaLinkMapper {
             text = TextContainer(address)
         )
 
-    fun mapCurrentDate(context: Context): TextViewCellModel {
-        val dateText = DateTimeUtils.getDateFormatted(System.currentTimeMillis(), context)
-        return TextViewCellModel.Raw(
-            text = TextContainer(dateText)
-        )
-    }
-
     fun mapTokenIcon(token: Token.Active): ImageViewCellModel =
         ImageViewCellModel(
             icon = DrawableContainer(token.iconUrl.orEmpty())
         )
 
-    fun mapClaimSuccessMessage(context: Context, token: Token.Active): TextViewCellModel {
-        val text = context.getString(
-            R.string.send_via_link_receive_funds_success_title,
-            token.total,
-            token.tokenSymbol
-        )
+    fun mapClaimSuccessMessage(token: Token.Active): TextViewCellModel {
         return TextViewCellModel.Raw(
-            text = TextContainer(text)
+            text = TextContainer.invoke(
+                R.string.send_via_link_receive_funds_success_title,
+                token.total, token.tokenSymbol
+            )
         )
     }
 }
