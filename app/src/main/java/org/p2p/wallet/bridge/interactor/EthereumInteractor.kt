ea8b2c19a6fef0dcba0eece8d45885788a991731
davrankhasanov <khasanov.davran@p2p.org>
2023-04-10 17:28:25 +0600

    Fix fees in details fragment

--- app/src/main/java/org/p2p/wallet/bridge/interactor/EthereumInteractor.kt
+++ app/src/main/java/org/p2p/wallet/bridge/interactor/EthereumInteractor.kt
@@ -71,4 +71,8 @@ class EthereumInteractor(
     suspend fun sendClaimBundle(signatures: List<Signature>) {
         return claimInteractor.sendEthereumBundle(signatures)
     }
+
+    suspend fun getClaimMinAmountForFreeFee(): BigDecimal {
+        return claimInteractor.getEthereumMinAmountForFreeFee()
+    }
 }
