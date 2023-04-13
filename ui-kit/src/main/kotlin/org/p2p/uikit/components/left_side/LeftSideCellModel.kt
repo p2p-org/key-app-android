e64c25dc6751a3bcfb86f2a55df260c97262f0c3
Eduard Maximovich <eduard.maximovich@p2p.org>
2023-04-13 20:28:42 +0400

    PWN-8075 - added ellipsize option to finance block view
    - `middle` ellipsize for send-via-link list
    - `end` ellipsize for send-via-link details link (bottom sheet)

--- ui-kit/src/main/kotlin/org/p2p/uikit/components/left_side/LeftSideCellModel.kt
+++ ui-kit/src/main/kotlin/org/p2p/uikit/components/left_side/LeftSideCellModel.kt
@@ -1,5 +1,6 @@
 package org.p2p.uikit.components.left_side
 
+import android.text.TextUtils
 import org.p2p.uikit.components.icon_wrapper.IconWrapperCellModel
 import org.p2p.uikit.utils.text.TextViewCellModel
 
@@ -10,5 +11,7 @@ sealed interface LeftSideCellModel {
         val firstLineText: TextViewCellModel? = null,
         val secondLineText: TextViewCellModel? = null,
         val thirdLineText: TextViewCellModel? = null,
+        // styling
+        val firstLineTextEllipsize: TextUtils.TruncateAt? = TextUtils.TruncateAt.MIDDLE,
     ) : LeftSideCellModel
 }
