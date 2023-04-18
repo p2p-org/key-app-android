39c94dc70624fd83ce9903c63b0c5865cdbc313e
Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>
2023-04-18 21:08:01 +0800

    PWN-7948 - SVL. Fix pending transactions (#1589)

--- app/src/main/java/org/p2p/wallet/history/ui/detailsbottomsheet/HistoryTransactionDetailsContract.kt
+++ app/src/main/java/org/p2p/wallet/history/ui/detailsbottomsheet/HistoryTransactionDetailsContract.kt
@@ -2,7 +2,6 @@ package org.p2p.wallet.history.ui.detailsbottomsheet
 
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
-import org.threeten.bp.ZonedDateTime
 import org.p2p.wallet.common.mvp.MvpPresenter
 import org.p2p.wallet.common.mvp.MvpView
 import org.p2p.wallet.history.model.rpc.RpcFee
@@ -13,18 +12,19 @@ interface HistoryTransactionDetailsContract {
     interface View : MvpView {
         fun showTransferView(tokenIconUrl: String?, placeholderIcon: Int)
         fun showSwapView(sourceIconUrl: String?, destinationIconUrl: String?)
-        fun showDate(date: ZonedDateTime)
+        fun showSubtitle(subtitle: String)
         fun showStatus(@StringRes titleResId: Int, @ColorRes colorId: Int)
-        fun showErrorState(errorMessage: String)
+        fun showProgressTransactionErrorState(errorMessage: String)
         fun showTransactionId(signature: String)
         fun showAmount(amountToken: String?, amountUsd: String?)
         fun showFee(fees: List<RpcFee>?)
         fun showLoading(isLoading: Boolean)
         fun showError(@StringRes messageId: Int)
-        fun showSenderAddress(senderAddress: Base58String, senderUsername: String?)
         fun showReceiverAddress(receiverAddress: Base58String, receiverUsername: String?)
         fun showStateTitleValue(title: String, value: String)
         fun hideSendReceiveTitleAndValue()
+        fun showSenderAddress(senderAddress: Base58String, senderUsername: String?, isReceivePending: Boolean)
+        fun showProgressTransactionInProgress()
     }
 
     interface Presenter : MvpPresenter<View> {
