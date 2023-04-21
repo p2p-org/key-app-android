c6a4980aade43a477b1a5e8c3f1377cbc0a73914
Chingiz <chyngyz.i@p2p.org>
2023-04-21 15:03:27 +0600

    Release cut v2.7.0

--- buildSrc/src/main/java/AppVersions.kt
+++ buildSrc/src/main/java/AppVersions.kt
@@ -1,7 +1,7 @@
 object AppVersions {
     const val VERSION_MAJOR = 2
-    const val VERSION_MINOR = 6
+    const val VERSION_MINOR = 7
     const val VERSION_HOTFIX = 0
-    const val VERSION_PATCH = 2
+    const val VERSION_PATCH = 0
     const val VERSION_BUILD = 1682497599933
 }
