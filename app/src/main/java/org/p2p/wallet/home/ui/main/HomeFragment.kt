2e3d4d593e7f239833a6bbf3c9366b3e94e96818
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2023-04-24 20:27:13 +0900

    PWN-7901 - Nameservice bugs

--- app/src/main/java/org/p2p/wallet/home/ui/main/HomeFragment.kt
+++ app/src/main/java/org/p2p/wallet/home/ui/main/HomeFragment.kt
@@ -143,8 +143,8 @@ class HomeFragment :
         }
     }
 
-    override fun showAddressCopied(addressAndUsername: String) {
-        requireContext().copyToClipBoard(addressAndUsername)
+    override fun showAddressCopied(addressOrUsername: String) {
+        requireContext().copyToClipBoard(addressOrUsername)
         showUiKitSnackBar(
             message = getString(R.string.home_address_snackbar_text),
             actionButtonResId = R.string.common_ok,
