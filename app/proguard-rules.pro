711e322da6089eae45201b34685a3ed2ee5ec77e
Davran331122 <122359284+Davran331122@users.noreply.github.com>
2023-04-28 11:49:32 +0600

    PWN-8256 - Fix claim bundles on release builds (#1640)
    * PWN-8256 - Fix claim bundles on release builds
    
    * PWN-8256 - Clean branch
    
    * PWN-8256 - Resolve PR comments
    
    * PWN-8256 - Remove useless spaces

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -78,6 +78,7 @@
 -keep public class * implements org.p2p.wallet.auth.model.RestoreUserResult
 -keep public class * implements org.p2p.wallet.jupiter.statemanager.SwapStateAction
 -keep public class * extends org.p2p.wallet.moonpay.model.MoonpayBuyResult
+-keep public class * implements org.p2p.wallet.bridge.model.BridgeResult
 
 -keep class io.intercom.android.** { *; }
 -keep class com.intercom.** { *; }
--- core/src/main/java/org/p2p/core/rpc/JsonRpc.kt
+++ core/src/main/java/org/p2p/core/rpc/JsonRpc.kt
@@ -3,21 +3,28 @@ package org.p2p.core.rpc
 import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.google.gson.annotations.SerializedName
+import timber.log.Timber
 import java.lang.reflect.Type
 
-abstract class JsonRpc<P,T>(
-        val method: String,
-        val params: P
+abstract class JsonRpc<P, T>(
+    @SerializedName("method")
+    val method: String,
+    @SerializedName("params")
+    val params: P
 ) {
     @SerializedName("jsonrpc")
     val version: String = "2.0"
+
+    @SerializedName("id")
     var id: Int = 1
 
     protected abstract val typeOfResult: Type
 
     fun parseResponse(response: RpcResponse, gson: Gson): T {
         if (response.error != null) {
-            throw ResponseError.RpcError(response.error)
+            val error = ResponseError.RpcError(response.error)
+            Timber.e(error, "RPC error returned")
+            throw error
         }
         return parseResult(response.result, gson)
     }
@@ -30,8 +37,13 @@ abstract class JsonRpc<P,T>(
         }
     }
 
-    sealed class ResponseError : Throwable() {
-        class RpcError(val error: RpcResponse.Error) : ResponseError()
-        class InvalidResult(val result: Any?) : ResponseError()
+    sealed class ResponseError(message: String) : Throwable(message = message) {
+        data class RpcError(val error: RpcResponse.Error) : ResponseError(
+            "Rpc returned error. message=${error.message};code=${error.code}"
+        )
+
+        data class InvalidResult(val result: Any?) : ResponseError(
+            "Rpc returned invalid result type: ${result?.javaClass}"
+        )
     }
 }
