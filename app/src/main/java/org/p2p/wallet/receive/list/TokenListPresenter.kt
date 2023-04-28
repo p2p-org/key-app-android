04d5baa9ceb732f286c1ad19bbff25e021afa450
kabramovp2p <konstantin.abramov@p2p.org>
2023-04-28 11:57:19 +0300

    PWN-4179 - fix formatting code

--- app/src/main/java/org/p2p/wallet/receive/list/TokenListPresenter.kt
+++ app/src/main/java/org/p2p/wallet/receive/list/TokenListPresenter.kt
@@ -1,7 +1,6 @@
 package org.p2p.wallet.receive.list
 
 import kotlinx.coroutines.launch
-import org.p2p.core.utils.Constants
 import org.p2p.wallet.common.analytics.interactor.ScreensAnalyticsInteractor
 import org.p2p.wallet.common.mvp.BasePresenter
 import org.p2p.wallet.home.analytics.BrowseAnalytics
