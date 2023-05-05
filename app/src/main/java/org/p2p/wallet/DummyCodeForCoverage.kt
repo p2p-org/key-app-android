d02b25d4254c0ae5d491df4d9b9d8db60f922486
Eduard Maximovich <eduard.maximovich@p2p.org>
2023-05-05 17:44:57 +0400

    NO-TICKET - dummy code to test coverage

---
+++ app/src/main/java/org/p2p/wallet/DummyCodeForCoverage.kt
@@ -0,0 +1,12 @@
+package org.p2p.wallet
+
+/**
+ * key-app. 2023
+ * @author Eduard Maximovich (edward.vstock@gmail.com)
+ */
+class DummyCodeForCoverage {
+
+    fun dummyA() {}
+    fun dymmyB(): Int = 42
+    fun dummyC(value: Int): String = value.toString()
+}
---
+++ app/src/test/java/org/p2p/wallet/DummyCodeForCoverageTest.kt
@@ -0,0 +1,26 @@
+package org.p2p.wallet
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class DummyCodeForCoverageTest {
+
+    @Test
+    fun testDummyA() {
+        val dummy = DummyCodeForCoverage()
+        dummy.dummyA()
+    }
+
+    @Test
+    fun testDummyB() {
+        val dummy = DummyCodeForCoverage()
+        assertEquals(42, dummy.dymmyB())
+    }
+
+    @Test
+    fun testDummyC() {
+        val dummy = DummyCodeForCoverage()
+        // don't check return value for test
+    }
+}
