641a8caac2ee479e7b8d0fc91c354e4faf57ef15
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2023-05-12 12:40:05 +0300

    PWN-8477 - getClaimMinAmountForFreeFee from ethereumInteractor on ReceiveEthereumFragment.kt (#1707)

--- app/src/main/java/org/p2p/wallet/receive/eth/ReceiveEthereumContract.kt
+++ app/src/main/java/org/p2p/wallet/receive/eth/ReceiveEthereumContract.kt
@@ -1,14 +1,15 @@
 package org.p2p.wallet.receive.eth
 
 import android.graphics.Bitmap
+import java.math.BigDecimal
 import org.p2p.wallet.common.mvp.MvpPresenter
 import org.p2p.wallet.common.mvp.MvpView
 
 interface ReceiveEthereumContract {
     interface View : MvpView {
         fun showQrAndAddress(qrBitmap: Bitmap, addressInHexString: String)
-
         fun showLoading(isLoading: Boolean)
+        fun setMinAmountForFreeFee(minAmountForFreeFee: BigDecimal)
     }
 
     interface Presenter : MvpPresenter<View> {
