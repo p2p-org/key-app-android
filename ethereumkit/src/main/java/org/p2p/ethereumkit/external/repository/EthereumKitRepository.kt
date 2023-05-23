12c5107e60d63f559922a4ce382b3dfe922e7648
kabramovp2p <konstantin.abramov@p2p.org>
2023-05-23 15:43:01 +0300

    PWN-8654 - fix adding metadata everytime in localTokensMetadata

--- ethereumkit/src/main/java/org/p2p/ethereumkit/external/repository/EthereumKitRepository.kt
+++ ethereumkit/src/main/java/org/p2p/ethereumkit/external/repository/EthereumKitRepository.kt
@@ -38,7 +38,6 @@ internal class EthereumKitRepository(
 ) : EthereumRepository {
 
     private var tokenKeyProvider: EthTokenKeyProvider? = null
-    private var localTokensMetadata = mutableListOf<EthTokenMetadata>()
     private var ethereumTokensFlow = MutableStateFlow<List<Token.Eth>>(emptyList())
 
     override fun init(seedPhrase: List<String>) {
@@ -79,13 +78,13 @@ internal class EthereumKitRepository(
     override suspend fun loadWalletTokens(claimingTokens: List<EthereumClaimToken>) {
         val walletTokens = withContext(dispatchers.io) {
             try {
-                localTokensMetadata.addAll(loadTokensMetadata())
-                getPriceForTokens(localTokensMetadata.map { it.contractAddress.hex })
+                val tokensMetadata = loadTokensMetadata()
+                getPriceForTokens(tokensMetadata.map { it.contractAddress.hex })
                     .onEach { (address, price) ->
-                        localTokensMetadata.find { it.contractAddress.hex == address }?.price = price
+                        tokensMetadata.find { it.contractAddress.hex == address }?.price = price
                     }
 
-                (listOf(getEthToken()) + localTokensMetadata).map { metadata ->
+                (listOf(getEthToken()) + tokensMetadata).map { metadata ->
                     var isClaiming = false
                     var latestBundleId: String? = null
                     var tokenAmount: BigDecimal? = null
