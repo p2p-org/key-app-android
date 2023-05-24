d36604e04a2a9e9d0ff338c4f3fadfca3f91174f
Eduard Maximovich <eduard.maximovich@p2p.org>
2023-05-24 11:39:32 +0400

    PWN-8381 - refactor sms input

---
+++ app/src/main/java/org/p2p/wallet/smsinput/striga/StrigaSmsInputPresenter.kt
@@ -0,0 +1,23 @@
+package org.p2p.wallet.smsinput.striga
+
+import org.p2p.wallet.common.mvp.BasePresenter
+import org.p2p.wallet.smsinput.SmsInputContract
+
+class StrigaSmsInputPresenter(
+
+) : BasePresenter<SmsInputContract.View>(), SmsInputContract.Presenter {
+
+    override fun attach(view: SmsInputContract.View) {
+        super.attach(view)
+        view.renderSmsTimerState(SmsInputContract.Presenter.SmsInputTimerState.ResendSmsReady)
+    }
+
+    override fun onSmsInputChanged(smsCode: String) {
+    }
+
+    override fun checkSmsValue(smsCode: String) {
+    }
+
+    override fun resendSms() {
+    }
+}
