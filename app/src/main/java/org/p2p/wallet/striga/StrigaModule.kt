139fe047e26695f1238ff7801946d53ea9b28291
davrankhasanov <khasanov.davran@p2p.org>
2023-05-24 23:42:54 +0600

    PWN-8457 - Finish personal data screen, and add ui kit input widget

---
+++ app/src/main/java/org/p2p/wallet/striga/StrigaModule.kt
@@ -0,0 +1,16 @@
+package org.p2p.wallet.striga
+
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+import org.p2p.wallet.common.di.InjectionModule
+import org.p2p.wallet.striga.ui.personaldata.StrigaPersonalInfoContract
+import org.p2p.wallet.striga.ui.personaldata.StrigaPersonalInfoPresenter
+
+object StrigaModule : InjectionModule {
+
+    override fun create(): Module = module {
+        factoryOf(::StrigaPersonalInfoPresenter) bind StrigaPersonalInfoContract.Presenter::class
+    }
+}
