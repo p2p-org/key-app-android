a2c978ec6baddc83bd45e6e1d15c9d420fbc0f8a
davrankhasanov <khasanov.davran@p2p.org>
2023-05-24 18:12:19 +0600

    PWN-8455 - Clean branch

--- app/src/main/java/org/p2p/wallet/home/ui/main/HomeFragment.kt
+++ app/src/main/java/org/p2p/wallet/home/ui/main/HomeFragment.kt
@@ -51,7 +51,6 @@ import org.p2p.wallet.receive.solana.ReceiveSolanaFragment
 import org.p2p.wallet.root.RootListener
 import org.p2p.wallet.sell.ui.payload.SellPayloadFragment
 import org.p2p.wallet.settings.ui.settings.SettingsFragment
-import org.p2p.wallet.striga.ui.TopUpWalletBottomSheet
 import org.p2p.wallet.transaction.model.NewShowProgress
 import org.p2p.wallet.utils.HomeScreenLayoutManager
 import org.p2p.wallet.utils.copyToClipBoard
@@ -152,8 +151,6 @@ class HomeFragment :
                 AppNotificationManager.createNotificationChannels(requireContext())
             }
         }
-
-        TopUpWalletBottomSheet.show(null,childFragmentManager)
     }
 
     override fun showAddressCopied(addressOrUsername: String) {
