bcc64ddf4a60e4a06260213a4ba4ed5ca7e5af81
Eduard Maximovich <eduard.maximovich@p2p.org>
2023-05-26 11:41:12 +0400

    PWN-8651 - merging conflicts

--- app/src/main/java/org/p2p/wallet/striga/di/StrigaSignupModule.kt
+++ app/src/main/java/org/p2p/wallet/striga/di/StrigaSignupModule.kt
@@ -10,12 +10,19 @@ import org.koin.dsl.module
 import retrofit2.create
 import org.p2p.wallet.R
 import org.p2p.wallet.common.di.InjectionModule
+import org.p2p.wallet.infrastructure.network.NetworkModule.getRetrofit
+import org.p2p.wallet.infrastructure.network.interceptor.StrigaProxyApiInterceptor
+import org.p2p.wallet.striga.StrigaUserIdProvider
 import org.p2p.wallet.striga.onboarding.StrigaOnboardingContract
+import org.p2p.wallet.striga.onboarding.StrigaOnboardingPresenter
+import org.p2p.wallet.striga.onboarding.interactor.StrigaOnboardingInteractor
 import org.p2p.wallet.striga.repository.StrigaPresetDataInMemoryRepository
 import org.p2p.wallet.striga.repository.StrigaPresetDataLocalRepository
 import org.p2p.wallet.striga.signup.repository.StrigaSignupDataDatabaseRepository
 import org.p2p.wallet.striga.signup.repository.StrigaSignupDataLocalRepository
 import org.p2p.wallet.striga.signup.repository.mapper.StrigaSignupDataMapper
+import org.p2p.wallet.striga.ui.personaldata.StrigaPersonalInfoContract
+import org.p2p.wallet.striga.ui.personaldata.StrigaPersonalInfoPresenter
 import org.p2p.wallet.striga.user.api.StrigaApi
 import org.p2p.wallet.striga.user.repository.StrigaUserRemoteRepository
 import org.p2p.wallet.striga.user.repository.StrigaUserRepository
@@ -24,7 +31,10 @@ import org.p2p.wallet.striga.user.repository.StrigaUserRepositoryMapper
 object StrigaSignupModule : InjectionModule {
     override fun create() = module {
         initDataLayer()
-        initUiLayer()
+
+        factoryOf(::StrigaOnboardingInteractor)
+        factoryOf(::StrigaOnboardingPresenter) bind StrigaOnboardingContract.Presenter::class
+        factoryOf(::StrigaPersonalInfoPresenter) bind StrigaPersonalInfoContract.Presenter::class
     }
 
     private fun Module.initDataLayer() {
@@ -45,11 +55,5 @@ object StrigaSignupModule : InjectionModule {
         factoryOf(::StrigaSignupDataMapper)
 
         factoryOf(::StrigaUserIdProvider)
-    }
-
-    private fun Module.initUiLayer() {
-        factoryOf(org.p2p.wallet.striga.onboarding.interactor::StrigaOnboardingInteractor)
-        factoryOf(org.p2p.wallet.striga.onboarding::StrigaOnboardingPresenter) bind org.p2p.wallet.striga.onboarding.StrigaOnboardingContract.Presenter::class
-        factoryOf(org.p2p.wallet.striga.ui.personaldata::StrigaPersonalInfoPresenter) bind org.p2p.wallet.striga.ui.personaldata.StrigaPersonalInfoContract.Presenter::class
     }
 }
