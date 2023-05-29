aff9fe1483186e9922e1cf70c8a7015d94f0899c
Davran331122 <122359284+Davran331122@users.noreply.github.com>
2023-05-29 10:25:48 +0600

    PWN- 8652 - Add Striga second step fragment, and refactor first step (#1768)
    * PWN-8457 - Finish personal data screen, and add ui kit input widget
    
    * PWN-8457 - Fix ktlint
    
    * PWN-8457 - Checkpoint commit
    
    * NO-TICKET - Update logic for add listeners for edit texts
    
    * PWN-8457 - Remove call from home fragment
    
    * PWN-8652 - Add striga sign up second step screen
    
    * PWN-8652 - Add striga sign up second step screen, and refactor first step, also add toolbar
    
    * PWN-8652 - Fix branch
    
    * PWN-8652 - Resolve PR comments
    
    * PWN-8652 - Revert Project.xml file changes
    
    * PWN-8652 - Fix branch after merge with dev
    
    ---------
    
    Co-authored-by: Gleb Levinkov (p2p) <100791035+gslevinkov@users.noreply.github.com>

--- app/src/main/java/org/p2p/wallet/striga/ui/personaldata/StrigaPersonalInfoContract.kt
+++
@@ -1,9 +0,0 @@
-package org.p2p.wallet.striga.ui.personaldata
-
-import org.p2p.wallet.common.mvp.MvpPresenter
-import org.p2p.wallet.common.mvp.MvpView
-
-interface StrigaPersonalInfoContract {
-    interface View : MvpView
-    interface Presenter : MvpPresenter<View>
-}
--- app/src/main/java/org/p2p/wallet/striga/ui/personaldata/StrigaPersonalInfoPresenter.kt
+++
@@ -1,7 +0,0 @@
-package org.p2p.wallet.striga.ui.personaldata
-
-import org.p2p.wallet.common.mvp.BasePresenter
-
-class StrigaPersonalInfoPresenter :
-    BasePresenter<StrigaPersonalInfoContract.View>(),
-    StrigaPersonalInfoContract.Presenter
