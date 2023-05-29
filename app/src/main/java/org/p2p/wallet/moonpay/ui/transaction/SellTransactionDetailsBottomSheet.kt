51bff277139bb05cd2ca1ae36fabfe62b6f9e282
Chingiz <chyngyz.i@p2p.org>
2023-05-29 13:46:22 +0600

    NO-TICKET - Cleaning (#1772)

--- app/src/main/java/org/p2p/wallet/moonpay/ui/transaction/SellTransactionDetailsBottomSheet.kt
+++ app/src/main/java/org/p2p/wallet/moonpay/ui/transaction/SellTransactionDetailsBottomSheet.kt
@@ -16,7 +16,6 @@ import org.p2p.wallet.R
 import org.p2p.wallet.common.mvp.BaseMvpBottomSheet
 import org.p2p.wallet.databinding.DialogSendTransactionDetailsBinding
 import org.p2p.wallet.home.ui.select.bottomsheet.SelectTokenBottomSheet
-import org.p2p.wallet.newsend.model.AddressState
 import org.p2p.wallet.newsend.model.SearchResult
 import org.p2p.wallet.newsend.ui.NewSendFragment
 import org.p2p.wallet.newsend.ui.SendOpenedFrom
@@ -137,7 +136,7 @@ class SellTransactionDetailsBottomSheet :
         sendAmount: BigDecimal,
         receiverAddress: String
     ) {
-        val recipient = SearchResult.AddressFound(AddressState(receiverAddress))
+        val recipient = SearchResult.AddressFound(receiverAddress)
         replaceFragment(
             NewSendFragment.create(
                 recipient = recipient,
