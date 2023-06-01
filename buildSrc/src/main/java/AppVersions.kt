8a52c6202055c94460570bfae4c8ce34b312024d
Chingiz <chyngyz.i@p2p.org>
2023-06-01 13:08:09 +0600

    PWN-8764 - Updated RPC API token

--- buildSrc/src/main/java/AppVersions.kt
+++ buildSrc/src/main/java/AppVersions.kt
@@ -1,7 +1,7 @@
 object AppVersions {
     const val VERSION_MAJOR = 2
     const val VERSION_MINOR = 7
-    const val VERSION_HOTFIX = 0
-    const val VERSION_PATCH = 4
+    const val VERSION_HOTFIX = 1
+    const val VERSION_PATCH = 0
     const val VERSION_BUILD = 1684743430817
 }
