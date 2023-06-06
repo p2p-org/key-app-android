bd6b76c46789918c90695c875ed1aa9236254317
Eduard Maximovich <eduard.maximovich@p2p.org>
2023-06-06 13:03:59 +0400

    PWN-8820 - striga sms error fragment

---
+++ app/src/main/java/org/p2p/wallet/striga/sms/StrigaSmsErrorViewType.kt
@@ -0,0 +1,41 @@
+package org.p2p.wallet.striga.sms
+
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.p2p.wallet.R
+
+@Parcelize
+sealed class StrigaSmsErrorViewType(
+    @DrawableRes val imageRes: Int,
+    @StringRes val titleRes: Int,
+    @StringRes val subtitleRes: Int,
+    @ColorRes val helpButtonColorRes: Int
+) : Parcelable {
+
+    @Parcelize
+    class NumberAlreadyUsed : StrigaSmsErrorViewType(
+        imageRes = R.drawable.ic_hand_with_square,
+        titleRes = R.string.string_sms_error_number_already_used_title,
+        subtitleRes = R.string.string_sms_error_number_already_used_subtitle,
+        helpButtonColorRes = R.color.lime
+    )
+
+    @Parcelize
+    class ExceededResendAttempts : StrigaSmsErrorViewType(
+        imageRes = R.drawable.ic_timer_error_raster,
+        titleRes = R.string.striga_sms_error_exceeded_resend_attempts_title,
+        subtitleRes = R.string.striga_sms_error_exceeded_resend_attempts_subtitle,
+        helpButtonColorRes = R.color.snow
+    )
+
+    @Parcelize
+    class ExceededConfirmationAttempts : StrigaSmsErrorViewType(
+        imageRes = R.drawable.ic_timer_error_raster,
+        titleRes = R.string.striga_sms_error_exceeded_confirmation_attempts_title,
+        subtitleRes = R.string.striga_sms_error_exceeded_confirmation_attempts_subtitle,
+        helpButtonColorRes = R.color.snow
+    )
+}
