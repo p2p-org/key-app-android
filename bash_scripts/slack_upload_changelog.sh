840747eeb14a7874033e9e8bef19d8e0aae822f4
Chingiz <chyngyz.i@p2p.org>
2023-06-12 17:11:16 +0600

    PWN-8350 - Manual feature build

--- bash_scripts/slack_upload_changelog.sh
+++ bash_scripts/slack_upload_changelog.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-curl \
--F token="$1" \
--F channels="$2" \
--F file=@$3 \
-https://slack.com/api/files.upload
+apk_file=$3
+version_name=$(aapt dump badging "$apk_file" | awk -v FS="'" '/versionName=/{print $2}')
 
+slack_ktlint_message="Build $version_name is published!"
+
+curl \
+  -F token="$1" \
+  -F channels="$2" \
+  -F file=@"$apk_file" \
+  -F text="${slack_ktlint_message}" \
+  https://slack.com/api/files.upload
