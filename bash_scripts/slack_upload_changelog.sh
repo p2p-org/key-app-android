2325a8d78771043086645da6efc650749925ef93
Chingiz <chyngyz.i@p2p.org>
2023-06-12 18:53:13 +0600

    PWN-8350 - Try fix slack

--- bash_scripts/slack_upload_changelog.sh
+++ bash_scripts/slack_upload_changelog.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-apk_file=$3
-apk_filename=$(basename "$apk_file")
-version_name=$(echo "$apk_filename" | sed 's/^key-app-\(.*\)$/\1/')
-
-slack_ktlint_message="Build $version_name is published!"
-
 curl \
-  -F token="$1" \
-  -F channels="$2" \
-  -F file=@"$apk_file" \
-  -F text="${slack_ktlint_message}" \
-  https://slack.com/api/files.upload
+-F token="$1" \
+-F channels="$2" \
+-F file=@$3 \
+https://slack.com/api/files.upload
