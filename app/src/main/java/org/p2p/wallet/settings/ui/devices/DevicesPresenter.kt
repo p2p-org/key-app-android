0f063e50122ef4411cbf0907b0f8ce5b472a39e1
kabramovp2p <konstantin.abramov@p2p.org>
2023-06-13 17:05:21 +0300

    PWN-8349 - add logic to show proper data on screen when device is not same as in metadata

--- app/src/main/java/org/p2p/wallet/settings/ui/devices/DevicesPresenter.kt
+++ app/src/main/java/org/p2p/wallet/settings/ui/devices/DevicesPresenter.kt
@@ -1,10 +1,14 @@
 package org.p2p.wallet.settings.ui.devices
 
 import kotlinx.coroutines.launch
+import org.p2p.wallet.auth.gateway.repository.model.GatewayOnboardingMetadata
 import org.p2p.wallet.common.mvp.BasePresenter
+import org.p2p.wallet.infrastructure.security.SecureStorageContract
+import org.p2p.wallet.settings.DeviceInfoHelper
 
 class DevicesPresenter(
-    private val deviceCellMapper: DeviceCellMapper
+    private val secureStorage: SecureStorageContract,
+    private val deviceCellMapper: DeviceCellMapper,
 ) : BasePresenter<DevicesContract.View>(), DevicesContract.Presenter {
 
     override fun attach(view: DevicesContract.View) {
@@ -13,13 +17,19 @@ class DevicesPresenter(
     }
 
     override fun executeDeviceShareChange() {
+        // TODO PWN-8351 add start point of change device share flow
     }
 
     private fun loadInitialData() {
         launch {
-            // todo: get current and old device share and show them
-
-            val items = deviceCellMapper.toCellModels("Xiaomi 13 Lite", "Redmi 8")
+            val oldDeviceShareName = secureStorage.getObject(
+                SecureStorageContract.Key.KEY_ONBOARDING_METADATA,
+                GatewayOnboardingMetadata::class
+            )?.deviceShareDeviceName.orEmpty()
+            val items = deviceCellMapper.toCellModels(
+                DeviceInfoHelper.getCurrentDeviceName(),
+                oldDeviceShareName
+            )
             view?.showCells(items)
         }
     }
