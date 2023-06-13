8cec7418e4cc13a4eca4d6a6c15bbc2554993c97
kabramovp2p <konstantin.abramov@p2p.org>
2023-06-13 11:33:53 +0300

    PWN-8752 - social networks added to settings

--- app/src/main/java/org/p2p/wallet/settings/ui/settings/SettingsFragment.kt
+++ app/src/main/java/org/p2p/wallet/settings/ui/settings/SettingsFragment.kt
@@ -23,6 +23,7 @@ import org.p2p.wallet.utils.BiometricPromptWrapper
 import org.p2p.wallet.utils.replaceFragment
 import org.p2p.wallet.utils.requireParcelable
 import org.p2p.wallet.utils.showInfoDialog
+import org.p2p.wallet.utils.showUrlInCustomTabs
 import org.p2p.wallet.utils.unsafeLazy
 import org.p2p.wallet.utils.viewbinding.viewBinding
 
@@ -102,6 +103,12 @@ class SettingsFragment :
             R.string.settings_item_title_support -> {
                 IntercomService.showMessenger()
             }
+            R.string.settings_item_title_twitter -> {
+                openTwitter()
+            }
+            R.string.settings_item_title_discord -> {
+                openDiscord()
+            }
         }
     }
 
@@ -146,5 +153,14 @@ class SettingsFragment :
 
     override fun openSecurityAndPrivacy() {
         replaceFragment(SecurityAndPrivacyFragment.create())
+    }
+
+    private fun openTwitter() {
+        requireContext().showUrlInCustomTabs("https://twitter.com/KeyApp_")
+    }
+
+    private fun openDiscord() {
+        val discordLink = "https://discord.gg/SpW3GmEYgU"
+        requireContext().showUrlInCustomTabs(discordLink)
     }
 }
