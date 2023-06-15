23133bdcae5ca17d83a703a5d1e5dbc5e4cfcc13
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2023-06-15 14:03:24 +0300

    PWN-8827 - connect otp screen and add some logic to restore wallet (#1832)
    * PWN-8827 - connect otp screen and add some logic to restore wallet
    
    * PWN-8827 - changes according to comments
    
    * PWN-8827 - SmsInputFactory.kt strategy fix
    
    * PWN-8827 - show error screen when there is no account associated with this email

--- app/src/main/java/org/p2p/wallet/settings/ui/mail/SettingsEmailConfirmPresenter.kt
+++ app/src/main/java/org/p2p/wallet/settings/ui/mail/SettingsEmailConfirmPresenter.kt
@@ -1,19 +1,73 @@
 package org.p2p.wallet.settings.ui.mail
 
+import timber.log.Timber
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.p2p.wallet.auth.interactor.MetadataInteractor
+import org.p2p.wallet.auth.interactor.OnboardingInteractor
 import org.p2p.wallet.auth.interactor.restore.RestoreWalletInteractor
+import org.p2p.wallet.auth.model.OnboardingFlow
+import org.p2p.wallet.auth.model.RestoreError
+import org.p2p.wallet.auth.model.RestoreFailureState
+import org.p2p.wallet.auth.model.RestoreSuccessState
+import org.p2p.wallet.auth.repository.RestoreUserResultHandler
 import org.p2p.wallet.common.mvp.BasePresenter
+import org.p2p.wallet.settings.DeviceInfoHelper
+import org.p2p.wallet.utils.DateTimeUtils
 
 class SettingsEmailConfirmPresenter(
+    private val onboardingInteractor: OnboardingInteractor,
     private val restoreWalletInteractor: RestoreWalletInteractor,
+    private val restoreUserResultHandler: RestoreUserResultHandler,
+    private val metadataInteractor: MetadataInteractor,
 ) : BasePresenter<SettingsEmailConfirmContract.View>(), SettingsEmailConfirmContract.Presenter {
 
     override fun setGoogleIdToken(userId: String, idToken: String) {
         launch {
             view?.setLoadingState(isScreenLoading = true)
+            onboardingInteractor.currentFlow = OnboardingFlow.RestoreWallet.SocialPlusCustomShare
             restoreWalletInteractor.obtainTorusKey(userId = userId, idToken = idToken)
-            // TODO update deviceShare
+            restoreUserWithShares(onboardingInteractor.currentFlow as OnboardingFlow.RestoreWallet)
             view?.setLoadingState(isScreenLoading = false)
         }
+    }
+
+    private suspend fun restoreUserWithShares(currentFlow: OnboardingFlow.RestoreWallet) {
+        onboardingInteractor.currentFlow = currentFlow
+        val restoreResult = restoreWalletInteractor.tryRestoreUser(currentFlow)
+        when (val restoreHandledState = restoreUserResultHandler.handleRestoreResult(restoreResult)) {
+            is RestoreSuccessState -> {
+                updateDeviceShare()
+            }
+            is RestoreFailureState.TitleSubtitleError -> {
+                if (restoreHandledState.email != null) {
+                    view?.showIncorrectAccountScreen(restoreHandledState.email)
+                } else {
+                    view?.showUiKitSnackBar(message = restoreHandledState.title)
+                }
+            }
+            is RestoreFailureState.ToastError -> {
+                view?.showUiKitSnackBar(message = restoreHandledState.message)
+            }
+            is RestoreFailureState.LogError -> {
+                Timber.e(RestoreError(restoreHandledState.message))
+            }
+        }
+    }
+
+    private suspend fun updateDeviceShare() {
+        // TODO update with real logic
+        updateMetadata()
+        delay(5000)
+        view?.showSuccessDeviceChange()
+    }
+
+    private suspend fun updateMetadata() {
+        val currentMetadata = metadataInteractor.currentMetadata ?: return
+        val newMetadata = currentMetadata.copy(
+            deviceShareDeviceName = DeviceInfoHelper.getCurrentDeviceName(),
+            deviceNameTimestampSec = DateTimeUtils.getCurrentTimestampInSeconds()
+        )
+        metadataInteractor.updateMetadata(newMetadata)
     }
 }
