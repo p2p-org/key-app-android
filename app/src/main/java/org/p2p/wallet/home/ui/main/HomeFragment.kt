66a0af106c1a8f620e483900ea29c03d66f0b5e2
davrankhasanov <khasanov.davran@p2p.org>
2023-06-15 18:22:17 +0600

    NO-TICKET - Fix crash on topup banner clicked

--- app/src/main/java/org/p2p/wallet/home/ui/main/HomeFragment.kt
+++ app/src/main/java/org/p2p/wallet/home/ui/main/HomeFragment.kt
@@ -289,6 +289,10 @@ class HomeFragment :
         replaceFragment(strigaKycFragmentFactory.kycFragment(status))
     }
 
+    override fun showTopupWalletDialog() {
+        TopUpWalletBottomSheet.show(parentFragmentManager)
+    }
+
     override fun showSendNoTokens(fallbackToken: Token) {
         replaceFragment(SendUnavailableFragment.create(fallbackToken))
     }
