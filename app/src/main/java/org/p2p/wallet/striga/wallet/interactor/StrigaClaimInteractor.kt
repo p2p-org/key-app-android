ad472670eb0a089ca4baa746544bc0f9b94b7c1e
davrankhasanov <khasanov.davran@p2p.org>
2023-07-05 14:05:54 +0600

    PWN-9119 - Merge with develop branch

--- app/src/main/java/org/p2p/wallet/striga/wallet/interactor/StrigaClaimInteractor.kt
+++ app/src/main/java/org/p2p/wallet/striga/wallet/interactor/StrigaClaimInteractor.kt
@@ -95,7 +95,6 @@ class StrigaClaimInteractor(
             .map { it.id }
     }
 
-    private fun getClaimableTokenMetadata(tokenSymbol: String): Token? {
     private suspend fun getClaimableTokenMetadata(tokenSymbol: String): Token? {
         return tokensRepository.findTokenDataBySymbol(tokenSymbol)
             ?.let { tokensRepository.findTokenByMint(it.mintAddress) }
