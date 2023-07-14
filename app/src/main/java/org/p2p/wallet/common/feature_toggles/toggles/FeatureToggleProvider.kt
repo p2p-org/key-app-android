d4ff859cbd9b73098f63bfd7c5698e6de900f073
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2023-07-14 14:50:30 +0300

    PWN-9151 - FeatureToggleProvider.kt added to use in places where we need more then one FT (#1968)
    * PWN-9151 - FeatureToggleProvider.kt added to use in places where we need more then one FT

---
+++ app/src/main/java/org/p2p/wallet/common/feature_toggles/toggles/FeatureToggleProvider.kt
@@ -0,0 +1,32 @@
+package org.p2p.wallet.common.feature_toggles.toggles
+
+import kotlin.reflect.KClass
+import org.p2p.wallet.common.feature_toggles.toggles.remote.RemoteFeatureToggle
+
+/**
+ * Provider made to use one class except of providing every FT in other class
+ * In case if you need provide ONE FT you can inject FT directly, else - inject this class
+ *
+ * Usage example:
+ * ```
+ * val toggle: Toggle = featureToggleProvider.getFeatureToggle(Toggle::class.java)
+ * val toggle: Toggle = featureToggleProvider.getFeatureToggle<Toggle>()
+ * val toggle: Toggle = featureToggleProvider[Toggle::class]
+ * ```
+ */
+class FeatureToggleProvider(private val featureToggles: List<RemoteFeatureToggle<*>>) {
+    @Throws(ClassCastException::class)
+    fun <T : RemoteFeatureToggle<*>> getFeatureToggle(klass: KClass<T>): T {
+        return featureToggles.filterIsInstance(klass.java).first()
+    }
+
+    // usage: val ft = ftProvider[StrigaSignupEnabledFeatureToggle::class]
+    @Throws(ClassCastException::class)
+    operator fun <T : RemoteFeatureToggle<*>> get(klass: KClass<T>): T {
+        return getFeatureToggle(klass)
+    }
+
+    inline fun <reified T : RemoteFeatureToggle<*>> getFeatureToggle(): T {
+        return getFeatureToggle(T::class)
+    }
+}
