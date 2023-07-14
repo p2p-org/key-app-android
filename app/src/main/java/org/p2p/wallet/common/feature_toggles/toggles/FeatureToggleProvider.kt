6a7149a58393faaea565ec62cf501b83771463b4
kabramovp2p <konstantin.abramov@p2p.org>
2023-07-14 12:06:24 +0300

    PWN-9151 - small doc added

--- app/src/main/java/org/p2p/wallet/common/feature_toggles/toggles/FeatureToggleProvider.kt
+++ app/src/main/java/org/p2p/wallet/common/feature_toggles/toggles/FeatureToggleProvider.kt
@@ -2,6 +2,13 @@ package org.p2p.wallet.common.feature_toggles.toggles
 
 import org.p2p.wallet.common.feature_toggles.toggles.remote.RemoteFeatureToggle
 
+/**
+ * Provider made to use one class except of providing every FT in other class
+ * Usage example:
+ * ```
+ * val toggle: Toggle = featureToggleProvider.getFeatureToggle(Toggle::class.java)
+ * ```
+ */
 class FeatureToggleProvider(private val featureToggles: List<RemoteFeatureToggle<*>>) {
     @Throws(ClassCastException::class)
     fun <T : RemoteFeatureToggle<*>> getFeatureToggle(klass: Class<T>): T {
