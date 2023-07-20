2621da646e1e1a7acf9158dc21698c3ed964916b
kabramovp2p <99185280+kabramovp2p@users.noreply.github.com>
2023-07-20 19:42:57 +0300

    PWN-9143 - filter tokens on `isTokenVisibleOnWalletScreen` (#1991)
    * PWN-9143 - filter tokens on `isTokenVisibleOnWalletScreen`
    
    * PWN-9143 - use extension for filterTokensForWalletScreen

--- app/src/main/java/org/p2p/wallet/home/ui/wallet/WalletPresenter.kt
+++ app/src/main/java/org/p2p/wallet/home/ui/wallet/WalletPresenter.kt
@@ -4,6 +4,7 @@ import java.math.BigDecimal
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
+import org.p2p.core.token.filterTokensForWalletScreen
 import org.p2p.wallet.auth.interactor.UsernameInteractor
 import org.p2p.wallet.auth.model.Username
 import org.p2p.wallet.common.mvp.BasePresenter
@@ -12,9 +13,9 @@ import org.p2p.wallet.home.ui.main.striga.StrigaOnRampConfirmedHandler
 import org.p2p.wallet.home.ui.wallet.handlers.StrigaBannerClickHandler
 import org.p2p.wallet.home.ui.wallet.handlers.StrigaOnRampClickHandler
 import org.p2p.wallet.home.ui.wallet.mapper.WalletMapper
+import org.p2p.wallet.home.ui.wallet.mapper.model.StrigaBanner
 import org.p2p.wallet.infrastructure.network.provider.TokenKeyProvider
 import org.p2p.wallet.intercom.IntercomService
-import org.p2p.wallet.home.ui.wallet.mapper.model.StrigaBanner
 import org.p2p.wallet.striga.onramp.interactor.StrigaOnRampInteractor
 import org.p2p.wallet.striga.user.interactor.StrigaUserInteractor
 import org.p2p.wallet.striga.wallet.models.ids.StrigaWithdrawalChallengeId
@@ -111,8 +112,8 @@ class WalletPresenter(
                 )
             }
             is UserTokensState.Loaded -> {
-                val usdc = newState.solTokens.find { it.isUSDC }
-                val balance = usdc?.total ?: BigDecimal.ZERO
+                val filteredTokens = newState.solTokens.filterTokensForWalletScreen()
+                val balance = filteredTokens.sumOf { it.total }
                 view?.showBalance(
                     walletMapper.mapFiatBalance(balance),
                     walletMapper.mapTokenBalance(balance)
