c40ac0796c8bdb2f30972664389adc0047336769
Davran331122 <122359284+Davran331122@users.noreply.github.com>
2023-07-20 20:28:20 +0600

    PWN-9300 update rates after swap send (#1989)
    * PWN-9197 - Add metadata ext, and add saving ext in database
    
    * PWN-9197 - Fix unit tests
    
    * PWN-9197 - Revert changes
    
    * PWN-9197 - Revert changes
    
    * PWN-9197 - Add token extensions configurators for setup token params, also fix bug with fetching ETH tokens, and temporary comment eth wallet
    
    * PWN-9197 - Clean branch
    
    * PWN-9197 - Resolve PR comments
    
    * PWN-9197 - Fix unit tests
    
    * PWN-9197 - Fix ktlint
    
    * PWN-9197 - Fix ktlint
    
    * PWN-9299 - Add worker to fetch tokens and token prices every 5 minutes
    
    * PWN-9300 - Update token rates, after swap and send
    
    * PWN-9300 - Resolve PR comments
    
    * PWN-9300 - Fix unit tests
    
    * PWN-9300 - Fix ktlint

--- app/src/main/java/org/p2p/wallet/home/events/TokenServiceWorkerLoader.kt
+++ app/src/main/java/org/p2p/wallet/home/events/TokenServiceWorkerLoader.kt
@@ -1,7 +1,7 @@
 package org.p2p.wallet.home.events
 
 import timber.log.Timber
-import java.time.Duration
+import kotlin.time.Duration.Companion.minutes
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -10,7 +10,7 @@ import kotlinx.coroutines.launch
 import org.p2p.core.common.di.AppScope
 import org.p2p.wallet.tokenservice.TokenServiceCoordinator
 
-private val DELAY_IN_MINUTES = Duration.ofMinutes(5)
+private val DELAY_IN_MINUTES = 5.minutes
 private const val TAG = "TokenServiceWorkerLoader"
 
 class TokenServiceWorkerLoader(
@@ -29,7 +29,7 @@ class TokenServiceWorkerLoader(
             while (isActive) {
                 try {
                     Timber.tag(TAG).d("Update token rates")
-                    delay(DELAY_IN_MINUTES.toMillis())
+                    delay(DELAY_IN_MINUTES)
                     tokenServiceCoordinator.refresh()
                 } catch (e: Throwable) {
                     Timber.tag(TAG).e(e, "Error while try to refresh token rates")
