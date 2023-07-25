de57bfde140d2ab528921a14c4713daa36d61654
Gleb Levinkov (p2p) <gleb.levinkov@p2p.org>
2023-07-25 13:46:00 +0800

    PWN-9270 - ktlint fixes

--- app/src/main/java/org/p2p/wallet/striga/offramp/StrigaOffRampModule.kt
+++ app/src/main/java/org/p2p/wallet/striga/offramp/StrigaOffRampModule.kt
@@ -3,8 +3,6 @@ package org.p2p.wallet.striga.offramp
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.factoryOf
-import org.koin.dsl.bind
 import org.koin.dsl.module
 import org.p2p.core.common.di.InjectionModule
 import org.p2p.wallet.striga.offramp.withdraw.StrigaWithdrawContract
