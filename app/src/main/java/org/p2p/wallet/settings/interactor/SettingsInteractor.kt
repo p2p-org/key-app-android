9aaadc4f9e378bff3fc956a20cccba53e7d9f0fb
kabramovp2p <konstantin.abramov@p2p.org>
2023-07-26 16:48:48 +0300

    PWN-9386 - use sharedPreferenceChangeListener to observe pref change

--- app/src/main/java/org/p2p/wallet/settings/interactor/SettingsInteractor.kt
+++ app/src/main/java/org/p2p/wallet/settings/interactor/SettingsInteractor.kt
@@ -6,13 +6,15 @@ import org.p2p.wallet.auth.interactor.AuthInteractor
 import org.p2p.wallet.auth.model.BiometricStatus
 import org.p2p.wallet.tokenservice.TokenServiceCoordinator
 
-private const val KEY_HIDDEN_ZERO_BALANCE = "KEY_HIDDEN_ZERO_BALANCE"
-
 class SettingsInteractor(
     private val sharedPreferences: SharedPreferences,
     private val authInteractor: AuthInteractor,
     private val tokenServiceCoordinator: TokenServiceCoordinator,
 ) {
+
+    companion object {
+        const val KEY_HIDDEN_ZERO_BALANCE = "KEY_HIDDEN_ZERO_BALANCE"
+    }
 
     fun setZeroBalanceHidden(isHidden: Boolean) {
         sharedPreferences.edit { putBoolean(KEY_HIDDEN_ZERO_BALANCE, isHidden) }
