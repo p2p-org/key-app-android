f50069d419623efc12d89531657d122b346f68e1
kabramovp2p <konstantin.abramov@p2p.org>
2023-07-26 16:00:56 +0300

    PWN-9386 - refresh tokens when show hide toggle changed

--- app/src/main/java/org/p2p/wallet/settings/interactor/SettingsInteractor.kt
+++ app/src/main/java/org/p2p/wallet/settings/interactor/SettingsInteractor.kt
@@ -4,16 +4,19 @@ import androidx.core.content.edit
 import android.content.SharedPreferences
 import org.p2p.wallet.auth.interactor.AuthInteractor
 import org.p2p.wallet.auth.model.BiometricStatus
+import org.p2p.wallet.tokenservice.TokenServiceCoordinator
 
 private const val KEY_HIDDEN_ZERO_BALANCE = "KEY_HIDDEN_ZERO_BALANCE"
 
 class SettingsInteractor(
     private val sharedPreferences: SharedPreferences,
-    private val authInteractor: AuthInteractor
+    private val authInteractor: AuthInteractor,
+    private val tokenServiceCoordinator: TokenServiceCoordinator,
 ) {
 
     fun setZeroBalanceHidden(isHidden: Boolean) {
         sharedPreferences.edit { putBoolean(KEY_HIDDEN_ZERO_BALANCE, isHidden) }
+        tokenServiceCoordinator.refresh()
     }
 
     fun areZerosHidden(): Boolean = sharedPreferences.getBoolean(KEY_HIDDEN_ZERO_BALANCE, true)
