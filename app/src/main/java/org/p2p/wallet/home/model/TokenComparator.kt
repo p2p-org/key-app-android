256243906aabc0cad9a38e97acee5c03481143c6
kabramovp2p <konstantin.abramov@p2p.org>
2023-07-26 10:35:47 +0300

    PWN-9385 - sorting changed -> now we sort by USD amount mostly

--- app/src/main/java/org/p2p/wallet/home/model/TokenComparator.kt
+++ app/src/main/java/org/p2p/wallet/home/model/TokenComparator.kt
@@ -4,18 +4,13 @@ import org.p2p.core.token.Token
 import org.p2p.core.utils.isMoreThan
 
 /**
- * First element should always be USDC || USDT
- * Further, we sort by total in USD
+ * We sort by total in USD
  * If there is no USD rate for the token, we sort by its total amount
  * Other tokens are not user's, no sorting is applied
  * */
 class TokenComparator : Comparator<Token> {
 
     override fun compare(o1: Token, o2: Token): Int = when {
-        o1.isUSDC -> -1
-        o2.isUSDC -> 1
-        o1.isUSDT && !o2.isUSDC -> -1
-        o2.isUSDT && !o2.isUSDC -> 1
         o1 is Token.Active && o2 is Token.Active -> compareActiveTokens(o1, o2)
         o1 is Token.Active -> -1
         o2 is Token.Active -> 1
