693db2ad9307071b6131a308dbfcc522c3cbbabe
Chingiz <chyngyz.i@p2p.org>
2023-08-02 19:48:07 +0600

    NO-TICKET - Version bump

--- buildSrc/src/main/java/AppVersions.kt
+++ buildSrc/src/main/java/AppVersions.kt
@@ -2,6 +2,6 @@ object AppVersions {
     const val VERSION_MAJOR = 2
     const val VERSION_MINOR = 9
     const val VERSION_HOTFIX = 0
-    const val VERSION_PATCH = 3
+    const val VERSION_PATCH = 4
     const val VERSION_BUILD = 3337
 }
