f348a9303e3bc83d651337279ca622991d66571c
Eduard Maximovich <130352737+eduardmaximovich@users.noreply.github.com>
2023-08-07 13:26:45 +0400

    PWN-9272, PWN-8904, PWN-9512, PWN-9518 - striga. cache related bugfixes (#2081)
    - update striga user wallet every wallet screen open
    - fixed otp timers for striga on/off ramp

--- app/src/main/java/org/p2p/wallet/striga/wallet/repository/impl/StrigaWalletCachedRepository.kt
+++ app/src/main/java/org/p2p/wallet/striga/wallet/repository/impl/StrigaWalletCachedRepository.kt
@@ -23,8 +23,8 @@ internal class StrigaWalletCachedRepository(
 
     private val timber: Timber.Tree = Timber.tag("StrigaWalletCachedRepository")
 
-    override suspend fun getUserWallet(): StrigaDataLayerResult<StrigaUserWallet> {
-        return withCache(cache::userWallet) {
+    override suspend fun getUserWallet(ignoreCache: Boolean): StrigaDataLayerResult<StrigaUserWallet> {
+        return withCache(ignoreCache = ignoreCache, cache = cache::userWallet) {
             remoteRepository.getUserWallet()
         }
     }
@@ -72,11 +72,13 @@ internal class StrigaWalletCachedRepository(
 
     private suspend fun <T> withCache(
         cache: KMutableProperty0<T?>? = null,
+        ignoreCache: Boolean = false,
         remote: suspend () -> T,
     ): StrigaDataLayerResult<T> {
         return try {
-            val result = cache?.invoke()?.also {
-                timber.i("Getting data from cache for ${cache.name}")
+            val cachePropertyRef = if (ignoreCache) null else cache
+            val result = cachePropertyRef?.invoke()?.also {
+                timber.i("Getting data from cache for ${cache?.name}")
             } ?: remote().also {
                 timber.i("Update cache from remote")
                 cache?.set(it)
