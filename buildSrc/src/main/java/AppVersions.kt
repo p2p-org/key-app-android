8a10df49a91a0b581703cdb3e8604a5ba929fe28
Gleb Levinkov <gleb.levinkov@p2p.org>
2024-01-25 14:26:23 +0300

    bump release version to 2.11

--- buildSrc/src/main/java/AppVersions.kt
+++ buildSrc/src/main/java/AppVersions.kt
@@ -1,6 +1,6 @@
 object AppVersions {
     const val VERSION_MAJOR = 2
-    const val VERSION_MINOR = 10
+    const val VERSION_MINOR = 11
     const val VERSION_HOTFIX = 0
     const val VERSION_PATCH = 0
     const val VERSION_BUILD = 3582
