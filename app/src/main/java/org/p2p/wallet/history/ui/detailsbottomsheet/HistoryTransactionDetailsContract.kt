d39ed2e2208c1cc35a008919adf24037647320b7
Eduard Maximovich <130352737+eduardmaximovich@users.noreply.github.com>
2024-02-02 13:29:29 +0400

    ETH-781 - referral in history (#2165)

--- app/src/main/java/org/p2p/wallet/history/ui/detailsbottomsheet/HistoryTransactionDetailsContract.kt
+++ app/src/main/java/org/p2p/wallet/history/ui/detailsbottomsheet/HistoryTransactionDetailsContract.kt
@@ -2,21 +2,29 @@ package org.p2p.wallet.history.ui.detailsbottomsheet
 
 import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
+import org.p2p.core.crypto.Base58String
 import org.p2p.wallet.common.mvp.MvpPresenter
 import org.p2p.wallet.common.mvp.MvpView
 import org.p2p.wallet.history.model.rpc.RpcFee
-import org.p2p.core.crypto.Base58String
 
 interface HistoryTransactionDetailsContract {
 
     interface View : MvpView {
         fun showTransferView(tokenIconUrl: String?, placeholderIcon: Int)
+        fun setSmokeBackground()
+        fun showNewButtons(
+            @StringRes firstButtonTitleRes: Int,
+            @StringRes secondButtonTitleRes: Int,
+        )
+        fun playApplauseAnimation()
         fun showSwapView(sourceIconUrl: String?, destinationIconUrl: String?)
         fun showSubtitle(subtitle: String)
         fun showStatus(@StringRes titleResId: Int, @ColorRes colorId: Int)
+        fun setTitle(@StringRes titleResId: Int)
         fun showProgressTransactionErrorState(errorMessage: String)
         fun showTransactionId(signature: String)
         fun showAmount(amountToken: String?, amountUsd: String?)
+        fun showAmountReferralReward(amountToken: String)
         fun showFee(fees: List<RpcFee>?)
         fun showLoading(isLoading: Boolean)
         fun showError(@StringRes messageId: Int)
@@ -25,9 +33,13 @@ interface HistoryTransactionDetailsContract {
         fun hideSendReceiveTitleAndValue()
         fun showSenderAddress(senderAddress: Base58String, senderUsername: String?, isReceivePending: Boolean)
         fun showProgressTransactionInProgress()
+        fun dismiss()
+        fun navigateToSolscan(txSignature: String)
     }
 
     interface Presenter : MvpPresenter<View> {
         fun load(transactionId: String)
+        fun onPrimaryButtonClick()
+        fun onSecondaryButtonClick()
     }
 }
