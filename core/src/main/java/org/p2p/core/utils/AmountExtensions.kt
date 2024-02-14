0c4d4d34846c564611377aec8cad9b1ee4969897
Eduard Maximovich <eduard.maximovich@p2p.org>
2024-02-14 16:26:29 +0400

    ETH-946 - renamed power value to base because it's a base value, not a power

--- core/src/main/java/org/p2p/core/utils/AmountExtensions.kt
+++ core/src/main/java/org/p2p/core/utils/AmountExtensions.kt
@@ -6,7 +6,7 @@ import java.math.RoundingMode
 import org.p2p.core.token.Token
 import org.p2p.core.utils.Constants.FIAT_FRACTION_LENGTH
 
-private val POWER_VALUE = BigDecimal.TEN
+private val BASE_TEN = BigDecimal.TEN
 const val SOL_DECIMALS = 9
 const val MOONPAY_DECIMAL = 2
 const val STRIGA_FIAT_DECIMALS = 2
@@ -26,7 +26,7 @@ fun String?.toBigIntegerOrZero(): BigInteger {
     return this?.toBigIntegerOrNull() ?: BigInteger.ZERO
 }
 
-fun Int.toPowerValue(): BigDecimal = POWER_VALUE.pow(this)
+fun Int.toPowerValue(): BigDecimal = BASE_TEN.pow(this)
 
 fun BigDecimal.scaleShortOrFirstNotZero(): BigDecimal {
     return if (isZero()) {
@@ -57,7 +57,7 @@ fun BigDecimal.scaleLong(decimals: Int = SCALE_VALUE_LONG): BigDecimal =
 // do not use BigDecimal(double) sometimes it makes the amount less
 // example: pass 0.030, get 0.029
 fun BigInteger.fromLamports(decimals: Int): BigDecimal =
-    (this.toBigDecimal().divide(POWER_VALUE.pow(decimals), 18, RoundingMode.HALF_DOWN))
+    (this.toBigDecimal().divide(BASE_TEN.pow(decimals), 18, RoundingMode.HALF_DOWN))
         .stripTrailingZeros() // removing zeros, case: 0.02000 -> 0.02
         .scaleLong(decimals)
 
