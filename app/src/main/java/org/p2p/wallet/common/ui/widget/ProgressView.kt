d0ab2e11a32755729d12795fd916aef8d7b0b94d
Gleb Levinkov <gleb.levinkov@p2p.org>
2024-02-20 13:41:22 +0300

    PWN-1021 - fix receive sol design

--- app/src/main/java/org/p2p/wallet/common/ui/widget/ProgressView.kt
+++ app/src/main/java/org/p2p/wallet/common/ui/widget/ProgressView.kt
@@ -2,10 +2,10 @@ package org.p2p.wallet.common.ui.widget
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.LayoutInflater
 import android.widget.FrameLayout
 import org.p2p.wallet.R
 import org.p2p.wallet.databinding.WidgetProgressViewBinding
+import org.p2p.wallet.utils.viewbinding.inflateViewBinding
 
 class ProgressView @JvmOverloads constructor(
     context: Context,
@@ -13,9 +13,7 @@ class ProgressView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-    private val binding = WidgetProgressViewBinding.inflate(
-        LayoutInflater.from(context), this
-    )
+    private val binding = inflateViewBinding<WidgetProgressViewBinding>()
 
     init {
         binding.root.setBackgroundColor(context.getColor(R.color.colorProgressBackground))
