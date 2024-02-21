c47b287e55f01361fe2b42152193cf9e9df04860
Gleb Levinkov <gleb.levinkov@p2p.org>
2024-02-21 19:12:40 +0300

    PWN-1052 - create a delay to show snackbar

--- app/src/main/java/org/p2p/wallet/moonpay/clientsideapi/response/MoonpayCurrency.kt
+++ app/src/main/java/org/p2p/wallet/moonpay/clientsideapi/response/MoonpayCurrency.kt
@@ -1,18 +1,18 @@
 package org.p2p.wallet.moonpay.clientsideapi.response
 
-import org.p2p.core.crypto.Base58String
 import org.p2p.core.utils.Constants
 
 sealed class MoonpayCurrency {
     abstract val currencyId: String
     abstract val amounts: MoonpayCurrencyAmounts
 
-    fun isSol() = this is CryptoToken && tokenMint == Constants.WRAPPED_SOL_MINT_B58
+    // it's okay to keep equals by symbol here
+    // because we only have non-scam wrapped SOL in/out Moonpay
+    fun isSol() = this is CryptoToken && tokenSymbol == Constants.SOL_SYMBOL
 
     data class CryptoToken(
         val tokenSymbol: String,
         val tokenName: String,
-        val tokenMint: Base58String,
         override val currencyId: String,
         override val amounts: MoonpayCurrencyAmounts
     ) : MoonpayCurrency()
