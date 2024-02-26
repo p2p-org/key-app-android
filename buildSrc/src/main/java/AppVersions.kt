aa60e77b1179073796cbea6dda11187c87133de5
Eduard Maximovich <eduard.maximovich@p2p.org>
2024-02-26 19:22:41 +0400

    bump app build version

--- buildSrc/src/main/java/AppVersions.kt
+++ buildSrc/src/main/java/AppVersions.kt
@@ -3,5 +3,5 @@ object AppVersions {
     const val VERSION_MINOR = 12
     const val VERSION_HOTFIX = 0
     const val VERSION_PATCH = 0
-    const val VERSION_BUILD = 3585
+    const val VERSION_BUILD = 3586
 }
